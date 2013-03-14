fee946470984066802de752042cc06523444ff8e
Seid Muhie Yimam <seidymam@gmail.com>
2013-03-14 20:02:14 +0000

    #322 - Support CoNLL NE writer/Reader

--- de.tudarmstadt.ukp.clarin.webanno.conll/src/main/java/de/tudarmstadt/ukp/clarin/webanno/conll/ConllReader.java
+++ de.tudarmstadt.ukp.clarin.webanno.conll/src/main/java/de/tudarmstadt/ukp/clarin/webanno/conll/ConllReader.java
@@ -28,6 +28,7 @@ import java.util.StringTokenizer;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.util.Level;
@@ -37,6 +38,7 @@ import com.ibm.icu.text.CharsetDetector;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
@@ -44,11 +46,11 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 
 /**
- * Reads a specific Conll File (9 TAB separated) annotation and change it to CAS object. Example of Input Files: 1
- * Heutzutage heutzutage ADV _ _ 2 ADV _ _ First column: token Number, in a sentence second Column:
- * the token third column: the lemma forth column: the POS fifth/sixth xolumn: Not Yet known seventh
- * column: the target token for a dependency parsing eighth column: the function of the dependency
- * parsing ninth and tenth column: Not Yet Known
+ * Reads a specific Conll File (9 TAB separated) annotation and change it to CAS object. Example of
+ * Input Files: 1 Heutzutage heutzutage ADV _ _ 2 ADV _ _ First column: token Number, in a sentence
+ * second Column: the token third column: the lemma forth column: the POS fifth/sixth xolumn: Not
+ * Yet known seventh column: the target token for a dependency parsing eighth column: the function
+ * of the dependency parsing ninth and tenth column: Not Yet Known
  *
  * Sentences are separated by a blank new line
  *
@@ -65,11 +67,12 @@ public class ConllReader
     {
         StringBuilder text = new StringBuilder();
         int tokenNumber = 0;
-
+        boolean noDependency = false;
         Map<Integer, String> tokens = new HashMap<Integer, String>();
         Map<Integer, String> pos = new HashMap<Integer, String>();
         Map<Integer, String> lemma = new HashMap<Integer, String>();
-        Map<Integer, String> namedEntity = new HashMap<Integer, String>();
+        Map<Integer, String> namedEntity1 = new HashMap<Integer, String>();
+        Map<Integer, String> namedEntity2 = new HashMap<Integer, String>();
         Map<Integer, String> dependencyFunction = new HashMap<Integer, String>();
         Map<Integer, Integer> dependencyDependent = new HashMap<Integer, Integer>();
 
@@ -108,11 +111,20 @@ public class ConllReader
                 tokens.put(tokenNumber, token);
                 lemma.put(tokenNumber, lineTk.nextToken());
                 pos.put(tokenNumber, lineTk.nextToken());
-                namedEntity.put(tokenNumber, lineTk.nextToken());
-                lineTk.nextToken();
-                int dependent = Integer.parseInt(lineTk.nextToken());
-                dependencyDependent.put(tokenNumber, dependent == 0 ? 0 : base + dependent);
-                dependencyFunction.put(tokenNumber, lineTk.nextToken());
+                String ne1 = lineTk.nextToken();
+                String ne2 = lineTk.nextToken();
+                namedEntity1.put(tokenNumber, ne1.equals("_") ? "O" : ne1);
+                namedEntity2.put(tokenNumber, ne2.equals("_") ? "O" : ne2);
+                String dependentValue = lineTk.nextToken();
+                if (NumberUtils.isDigits(dependentValue)) {
+                    int dependent = Integer.parseInt(dependentValue);
+                    dependencyDependent.put(tokenNumber, dependent == 0 ? 0 : base + dependent);
+                    dependencyFunction.put(tokenNumber, lineTk.nextToken());
+                }
+                else{
+                    lineTk.nextToken();
+                    noDependency = true;
+                }
                 lineTk.nextToken();
                 lineTk.nextToken();
             }
@@ -133,32 +145,43 @@ public class ConllReader
             tokenBeginPosition = tokenEndPosition;
             outToken.addToIndexes();
 
+            // Add pos to CAS if exist
+            if(!pos.get(i).equals("_")){
             POS outPos = new POS(aJCas, outToken.getBegin(), outToken.getEnd());
             outPos.setPosValue(pos.get(i));
             outPos.addToIndexes();
             outToken.setPos(outPos);
+            }
 
+            // Add lemma if exist
+            if(!lemma.get(i).equals("_")){
             Lemma outLemma = new Lemma(aJCas, outToken.getBegin(), outToken.getEnd());
             outLemma.setValue(lemma.get(i));
             outLemma.addToIndexes();
             outToken.setLemma(outLemma);
-
+            }
             tokensStored.put("t_" + i, outToken);
         }
 
-        for (int i = 1; i <= tokens.size(); i++) {
-            Dependency outDependency = new Dependency(aJCas);
-            outDependency.setDependencyType(dependencyFunction.get(i));
-            outDependency.setBegin(tokensStored.get("t_" + i).getBegin());
-            outDependency.setEnd(tokensStored.get("t_" + i).getEnd());
-            outDependency.setGovernor(tokensStored.get("t_" + i));
-            if (dependencyDependent.get(i) == 0) {
-                outDependency.setDependent(tokensStored.get("t_" + i));
+        createNamedEntity(namedEntity1, aJCas, tokens, tokensStored);
+        // For Nested Named Entity
+        createNamedEntity(namedEntity2, aJCas, tokens, tokensStored);
+        // add Dependency parsing to CAS, if exist
+        if(!noDependency) {
+            for (int i = 1; i <= tokens.size(); i++) {
+                Dependency outDependency = new Dependency(aJCas);
+                outDependency.setDependencyType(dependencyFunction.get(i));
+                outDependency.setBegin(tokensStored.get("t_" + i).getBegin());
+                outDependency.setEnd(tokensStored.get("t_" + i).getEnd());
+                outDependency.setGovernor(tokensStored.get("t_" + i));
+                if (dependencyDependent.get(i) == 0) {
+                    outDependency.setDependent(tokensStored.get("t_" + i));
+                }
+                else {
+                    outDependency.setDependent(tokensStored.get("t_" + dependencyDependent.get(i)));
+                }
+                outDependency.addToIndexes();
             }
-            else {
-                outDependency.setDependent(tokensStored.get("t_" + dependencyDependent.get(i)));
-            }
-            outDependency.addToIndexes();
         }
 
         for (int i = 0; i < firstTokenInSentence.size(); i++) {
@@ -214,5 +237,71 @@ public class ConllReader
             closeQuietly(is);
         }
 
+    }
+
+    /**
+     * Creates Named Entities from CoNLL BIO format to CAS format
+     */
+    private void createNamedEntity(Map<Integer, String> aNamedEntityMap, JCas aJCas,
+            Map<Integer, String> aTokensMap, Map<String, Token> aJcasTokens)
+    {
+        String previousNamedEntity = "O";
+        int namedEntityBegin = -1;
+        int namedEntityEnd = -1;
+
+        for (int i = 1; i <= aTokensMap.size(); i++) {
+            if (previousNamedEntity.equals("O") && aNamedEntityMap.get(i).equals("O")) {
+                continue;
+            }
+
+            if (!aNamedEntityMap.get(i).equals("O") && namedEntityBegin == -1) {
+                // First Named Entity
+                namedEntityBegin = aJcasTokens.get("t_" + i).getBegin();
+                namedEntityEnd = aJcasTokens.get("t_" + i).getEnd();
+                previousNamedEntity = aNamedEntityMap.get(i);
+            }
+            else if (!previousNamedEntity.equals("O")) {
+                // Named Entity continues
+                if (aNamedEntityMap.get(i).startsWith("I_")) {
+                    namedEntityEnd = aJcasTokens.get("t_" + i).getEnd();
+                }
+                else if (aNamedEntityMap.get(i).equals("O")) {
+
+                    NamedEntity outNamedEntity = new NamedEntity(aJCas, namedEntityBegin,
+                            namedEntityEnd);
+                    outNamedEntity.setValue(previousNamedEntity.substring(2));
+                    outNamedEntity.addToIndexes();
+
+                    previousNamedEntity = "O";
+                }
+                // Different named entity
+                else if (aNamedEntityMap.get(i).startsWith("B_")) {
+
+                    NamedEntity outNamedEntity = new NamedEntity(aJCas, namedEntityBegin,
+                            namedEntityEnd);
+                    outNamedEntity.setValue(previousNamedEntity.substring(2));
+                    outNamedEntity.addToIndexes();
+
+                    namedEntityBegin = aJcasTokens.get("t_" + i).getBegin();
+                    namedEntityEnd = aJcasTokens.get("t_" + i).getEnd();
+                    previousNamedEntity = aNamedEntityMap.get(i);
+                }
+            }
+            else if (!aNamedEntityMap.get(i).equals("O")) {
+                // First Named Entity
+                namedEntityBegin = aJcasTokens.get("t_" + i).getBegin();
+                namedEntityEnd = aJcasTokens.get("t_" + i).getEnd();
+                previousNamedEntity = aNamedEntityMap.get(i);
+            }
+        }
+        // If the last token have a named Entity with Multiple span, add it
+        int lastTokenIndex = aTokensMap.size();
+        String lastNamedEntity = aNamedEntityMap.get(lastTokenIndex);
+        if (lastNamedEntity.startsWith("I_")){
+            NamedEntity outNamedEntity = new NamedEntity(aJCas, namedEntityBegin,
+                    namedEntityEnd);
+            outNamedEntity.setValue(previousNamedEntity.substring(2));
+            outNamedEntity.addToIndexes();
+        }
     }
 }
--- de.tudarmstadt.ukp.clarin.webanno.conll/src/main/java/de/tudarmstadt/ukp/clarin/webanno/conll/ConllWriter.java
+++ de.tudarmstadt.ukp.clarin.webanno.conll/src/main/java/de/tudarmstadt/ukp/clarin/webanno/conll/ConllWriter.java
@@ -29,17 +29,18 @@ import org.apache.uima.jcas.JCas;
 import org.uimafit.descriptor.ConfigurationParameter;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
+import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 
 /**
- * Writes a specific Conll File (9 TAB separated) annotation from the CAS object.  Example of output file: 1
- * Heutzutage heutzutage ADV _ _ 2 ADV _ _ First column: token Number, in a sentence second Column:
- * the token third column: the lemma forth column: the POS fifth/sixth xolumn: Not Yet known seventh
- * column: the target token for a dependency parsing eighth column: the function of the dependency
- * parsing ninth and tenth column: Not Yet Known
+ * Writes a specific Conll File (9 TAB separated) annotation from the CAS object. Example of output
+ * file: 1 Heutzutage heutzutage ADV _ _ 2 ADV _ _ First column: token Number, in a sentence second
+ * Column: the token third column: the lemma forth column: the POS fifth/sixth xolumn: Not Yet known
+ * seventh column: the target token for a dependency parsing eighth column: the function of the
+ * dependency parsing ninth and tenth column: Not Yet Known
  *
  * Sentences are separated by a blank new line
  *
@@ -110,11 +111,67 @@ public class ConllWriter
             }
 
             int j = 1;
+            // Add named Entity to a token
+            Map<String, String> tokenNamedEntityMap = new HashMap<String, String>();
+            for (NamedEntity namedEntity : selectCovered(NamedEntity.class, sentence)) {
+                boolean sameChain = false; // maintain multiple span chains in BIO1 or BIO2
+            for (Token token : selectCovered(Token.class, sentence)) {
+                    if(namedEntity.getBegin()<=token.getBegin() && namedEntity.getEnd()>=token.getEnd()) {
+                        if (tokenNamedEntityMap.get("first-" + token.getAddress()) == null &!sameChain ) {
+                            tokenNamedEntityMap.put("first-" + token.getAddress(),
+                                    namedEntity.getValue());
+                        }
+                        else  if (tokenNamedEntityMap.get("second-" + token.getAddress()) == null) {
+                            tokenNamedEntityMap.put("second-" + token.getAddress(),
+                                    namedEntity.getValue());
+                            sameChain = true;
+                        }
+                    }
+                }
+            }
+            String previopusNamedEntity1 = "O";
+            String previopusNamedEntity2 = "O";
             for (Token token : selectCovered(Token.class, sentence)) {
 
                 String lemma = token.getLemma() == null ? "_" : token.getLemma().getValue();
                 String pos = token.getPos() == null ? "_" : token.getPos().getPosValue();
                 String dependent = "_";
+
+                String firstNamedEntity =  tokenNamedEntityMap.get("first-" + token.getAddress());
+                if (firstNamedEntity==null){
+                    firstNamedEntity="O";
+                    previopusNamedEntity1 = "O";
+                }
+                else if (previopusNamedEntity1.equals("O")){
+                    firstNamedEntity = "B_"+firstNamedEntity;
+                    previopusNamedEntity1 = firstNamedEntity;
+                }
+                else if(previopusNamedEntity1.substring(2).equals(firstNamedEntity)){
+                    firstNamedEntity = "I_"+firstNamedEntity;
+                    previopusNamedEntity1 = firstNamedEntity;
+                }
+                else{
+                    firstNamedEntity = "B_"+firstNamedEntity;
+                    previopusNamedEntity1 = firstNamedEntity;
+                }
+                // for Nested Named Entity
+                String secondNamedEntity =  tokenNamedEntityMap.get("second-" + token.getAddress());
+                if (secondNamedEntity==null){
+                    secondNamedEntity="O";
+                    previopusNamedEntity2 = "O";
+                }
+                else if (previopusNamedEntity2.equals("O")){
+                    secondNamedEntity = "B_"+secondNamedEntity;
+                    previopusNamedEntity2 = secondNamedEntity;
+                }
+                else if(previopusNamedEntity2.substring(2).equals(secondNamedEntity)){
+                    secondNamedEntity = "I_"+secondNamedEntity;
+                    previopusNamedEntity2 = secondNamedEntity;
+                }
+                else{
+                    secondNamedEntity = "B_"+secondNamedEntity;
+                    previopusNamedEntity2 = secondNamedEntity;
+                }
 
                 if (dependentMap.get(token.getAddress()) != null) {
                     if (dependencyMap.get(dependentMap.get(token.getAddress())) != null) {
@@ -128,11 +185,11 @@ public class ConllWriter
                         && dependencyMap.get(dependentMap.get(token.getAddress())) != null
                         && j == dependencyMap.get(dependentMap.get(token.getAddress()))) {
                     conllSb.append(j + "\t" + token.getCoveredText() + "\t" + lemma + "\t" + pos
-                            + "\t" + "_\t_\t" + 0 + "\t" + type + "\t_\t_\n");
+                            + "\t" + firstNamedEntity+"\t"+secondNamedEntity+"\t" + 0 + "\t" + type + "\t_\t_\n");
                 }
                 else {
                     conllSb.append(j + "\t" + token.getCoveredText() + "\t" + lemma + "\t" + pos
-                            + "\t" + "_\t_\t" + dependent + "\t" + type + "\t_\t_\n");
+                            + "\t" + firstNamedEntity+"\t"+secondNamedEntity+"\t" + dependent + "\t" + type + "\t_\t_\n");
                 }
                 j++;
             }
