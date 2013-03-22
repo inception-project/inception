9ff7218a4395c56a47229a5e64e37286ec5dec40
Seid Muhie Yimam <seidymam@gmail.com>
2013-03-22 17:16:35 +0000

    #279 - CasToTcfWriterTest does not use UIMA interface - instead of the contents, check if the number of layers and the number of annotations in each layers are eqaul. comparing String output fails at Jenkins as the library generates outputs suffixes differently in Jenkins than in the Local machine

--- de.tudarmstadt.ukp.clarin.webanno.tcf/src/test/java/de/tudarmstadt/ukp/clarin/webanno/tcf/TcfReaderWriterTest.java
+++ de.tudarmstadt.ukp.clarin.webanno.tcf/src/test/java/de/tudarmstadt/ukp/clarin/webanno/tcf/TcfReaderWriterTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2012
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,18 +21,22 @@ import static org.uimafit.factory.CollectionReaderFactory.createCollectionReader
 import static org.uimafit.pipeline.SimplePipeline.runPipeline;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReader;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.uimafit.component.xwriter.CASDumpWriter;
+
+import eu.clarin.weblicht.wlfxb.io.WLDObjector;
+import eu.clarin.weblicht.wlfxb.tc.xb.TextCorpusStored;
+import eu.clarin.weblicht.wlfxb.xb.WLData;
 
 public class TcfReaderWriterTest
 {
     @Test
-    @Ignore("The TCF library generates different xml namespaces and assertEquals fails on Jenkins ")
+    // @Ignore("The TCF library generates different xml namespaces and assertEquals fails on Jenkins ")
     public void test()
         throws Exception
     {
@@ -41,18 +45,38 @@ public class TcfReaderWriterTest
                 new String[] { "[+]tcf-after.xml" });
 
         AnalysisEngineDescription writer = createPrimitiveDescription(TcfWriter.class,
-                TcfWriter.PARAM_PATH, "target/test-output", TcfWriter.PARAM_STRIP_EXTENSION,
-                true);
+                TcfWriter.PARAM_PATH, "target/test-output", TcfWriter.PARAM_STRIP_EXTENSION, true);
 
         AnalysisEngineDescription dumper = createPrimitiveDescription(CASDumpWriter.class,
                 CASDumpWriter.PARAM_OUTPUT_FILE, "target/test-output/dump.txt");
 
         runPipeline(reader, writer, dumper);
 
-        String reference = FileUtils.readFileToString(new File(
-                "src/test/resources/tcf-after-expected.xml"), "UTF-8");
-        String actual = FileUtils.readFileToString(
-                new File("target/test-output/tcf-after.tcf"), "UTF-8");
-        assertEquals(reference, actual);
+        InputStream isReference = new FileInputStream(new File(
+                "src/test/resources/tcf-after-expected.xml"));
+
+        InputStream isActual = new FileInputStream(new File("target/test-output/tcf-after.tcf"));
+
+        WLData wLDataReference = WLDObjector.read(isReference);
+        TextCorpusStored aCorpusDataReference = wLDataReference.getTextCorpus();
+
+        WLData wLDataActual = WLDObjector.read(isActual);
+        TextCorpusStored aCorpusDataActual = wLDataActual.getTextCorpus();
+
+        // check if layers maintained
+        assertEquals(aCorpusDataReference.getLayers().size(), aCorpusDataActual.getLayers().size());
+
+        // Check if every layers have the same number of annotations
+        for (int i = 0; i < aCorpusDataReference.getLayers().size(); i++) {
+            assertEquals(aCorpusDataReference.getLayers().get(i).size(), aCorpusDataActual
+                    .getLayers().get(i).size());
+        }
+
+        //
+        // String reference = FileUtils.readFileToString(new File(
+        // "src/test/resources/tcf-after-expected.xml"), "UTF-8");
+        // String actual = FileUtils.readFileToString(
+        // new File("target/test-output/tcf-after.tcf"), "UTF-8");
+        // assertEquals(reference, actual);
     }
 }
