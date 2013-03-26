2e6b4f1593c5021d237ae0aa717d55718006a3be
Seid Muhie Yimam <seidymam@gmail.com>
2013-03-26 10:50:07 +0000

    NO ISSUE - added license headers - added javadoc

--- de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/page/monitoring/DocumentColumnMetaData.java
+++ de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/page/monitoring/DocumentColumnMetaData.java
@@ -1,7 +1,23 @@
+/*******************************************************************************
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.clarin.webanno.brat.page.monitoring;
 
 import java.util.List;
 
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.DataGridView;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
 import org.apache.wicket.markup.html.basic.Label;
@@ -9,7 +25,14 @@ import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 
-public class DocumentColumnMetaData extends AbstractColumn<List<String>> {
+/**
+ * Build dynamic columns for the user's annotation documents status {@link DataGridView}
+ * @author Seid Muhie Yimam
+ *
+ */
+public class DocumentColumnMetaData
+    extends AbstractColumn<List<String>>
+{
 
     /**
      *
@@ -17,22 +40,26 @@ public class DocumentColumnMetaData extends AbstractColumn<List<String>> {
     private static final long serialVersionUID = 1L;
     int columnNumber;
 
-    public DocumentColumnMetaData(final UserAnnotatedDocumentProvider prov,final int colNumber) {
-        super(new AbstractReadOnlyModel<String>() {
+    public DocumentColumnMetaData(final UserAnnotatedDocumentProvider prov, final int colNumber)
+    {
+        super(new AbstractReadOnlyModel<String>()
+        {
             private static final long serialVersionUID = 1L;
 
             @Override
-            public String getObject() {
-                    return prov.getColNames().get(colNumber);
+            public String getObject()
+            {
+                return prov.getColNames().get(colNumber);
 
             }
         });
         columnNumber = colNumber;
     }
 
-    public void populateItem(Item<ICellPopulator<List<String>>> cellItem,
-            String componentId, IModel<List<String>> rowModel) {
-        cellItem.add(new Label(componentId,rowModel.getObject().get(columnNumber)));
+    public void populateItem(Item<ICellPopulator<List<String>>> cellItem, String componentId,
+            IModel<List<String>> rowModel)
+    {
+        cellItem.add(new Label(componentId, rowModel.getObject().get(columnNumber)));
     }
 
 }
--- de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/page/monitoring/UserAnnotatedDocumentProvider.java
+++ de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/page/monitoring/UserAnnotatedDocumentProvider.java
@@ -1,14 +1,35 @@
+/*******************************************************************************
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.clarin.webanno.brat.page.monitoring;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.DataGridView;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
 
+/**
+ * Data provider for the user's annotation documents status {@link DataGridView}
+ * @author Seid Muhie Yimam
+ *
+ */
 public class UserAnnotatedDocumentProvider
     extends SortableDataProvider<List<? extends String>>
 {
