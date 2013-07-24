90ec8f1a29e7cc5dfeed0e14375e69c007d4e2c9
Seid Muhie Yimam <seidymam@gmail.com>
2013-07-24 16:18:02 +0000

    NO ISSUES - add missing licenses

--- de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/page/annotation/component/FinishLink.java
+++ de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/page/annotation/component/FinishLink.java
@@ -1,6 +1,20 @@
-/**
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
  *
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.clarin.webanno.webapp.page.annotation.component;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
@@ -19,9 +33,8 @@ import de.tudarmstadt.ukp.clarin.webanno.model.User;
 import de.tudarmstadt.ukp.clarin.webanno.webapp.dialog.YesNoModalPanel;
 
 /**
+ * A link to close/finish annotation/correction/curation project
  * @author Seid Muhie Yimam
- * @author Richard Eckart de Castilho
- *
  */
 public class FinishLink
     extends Panel
