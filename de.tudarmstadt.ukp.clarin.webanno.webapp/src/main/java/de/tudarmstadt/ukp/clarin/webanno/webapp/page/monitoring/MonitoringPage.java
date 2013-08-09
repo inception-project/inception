18539c6573ce841fbadda47c5fe3b9936fef2a68
Seid Muhie Yimam <seidymam@gmail.com>
2013-08-09 10:38:19 +0000

    #597 - "Last accessed" row shows "__"

--- de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/page/monitoring/MonitoringPage.java
+++ de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/page/monitoring/MonitoringPage.java
@@ -309,8 +309,8 @@ public class MonitoringPage
                         // Add a timestamp row for every user.
                         List<String> projectTimeStamp = new ArrayList<String>();
                         projectTimeStamp.add(LAST_ACCESS + LAST_ACCESS_ROW); // first column
-                        projectTimeStamp.add(LAST_ACCESS + "__");// Source document column
-                        projectTimeStamp.add(LAST_ACCESS + "__");// curation column, not yet added
+                        projectTimeStamp.add(LAST_ACCESS + "");// Source document column
+                        projectTimeStamp.add(LAST_ACCESS + "");// curation column, not yet added
                                                                  // TODO
                         for (User user : usersWithPermissions) {
                             if (projectRepository.existsProjectTimeStamp(project,
