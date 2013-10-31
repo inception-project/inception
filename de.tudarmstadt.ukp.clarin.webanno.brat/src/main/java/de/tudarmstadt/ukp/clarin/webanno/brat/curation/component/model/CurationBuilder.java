5da97180f49f3f30250d59cfefffbc8ee3bf7bf5
Seid Muhie Yimam <seidymam@gmail.com>
2013-10-31 10:31:51 +0000

    NO ISSUE 	- disable coref from curation/correction view

--- de.tudarmstadt.ukp.clarin.webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/curation/component/model/CurationBuilder.java
+++ de.tudarmstadt.ukp.clarin.webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/curation/component/model/CurationBuilder.java
@@ -40,6 +40,7 @@ import de.tudarmstadt.ukp.clarin.webanno.api.RepositoryService;
 import de.tudarmstadt.ukp.clarin.webanno.brat.annotation.BratAnnotatorModel;
 import de.tudarmstadt.ukp.clarin.webanno.brat.controller.BratAjaxCasController;
 import de.tudarmstadt.ukp.clarin.webanno.brat.controller.BratAjaxCasUtil;
+import de.tudarmstadt.ukp.clarin.webanno.brat.controller.TypeUtil;
 import de.tudarmstadt.ukp.clarin.webanno.brat.curation.AnnotationOption;
 import de.tudarmstadt.ukp.clarin.webanno.brat.curation.AnnotationSelection;
 import de.tudarmstadt.ukp.clarin.webanno.brat.curation.CasDiff;
@@ -285,6 +286,9 @@ public class CurationBuilder
         List<Type> entryTypes = new LinkedList<Type>();
 
         for (TagSet tagSet : aBratAnnotatorModel.getAnnotationLayers()) {
+            if(tagSet.getType().getName().equals(TypeUtil.COREFERENCE) ||
+                    tagSet.getType().getName().equals(TypeUtil.COREFRELTYPE) )
+                continue;
             entryTypes.add(getAdapter(tagSet.getType().getName()).getAnnotationType(mergeJCas.getCas()));
         }
         return entryTypes;
