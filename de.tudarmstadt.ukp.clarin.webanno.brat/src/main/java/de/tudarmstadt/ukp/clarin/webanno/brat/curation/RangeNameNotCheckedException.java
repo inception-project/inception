e36224b4e13b35c6ab167321b76e670ded12d491
Seid Muhie Yimam <seidymam@gmail.com>
2013-11-06 14:04:44 +0000

    #670 - Refactor codes for 1.0.0 release 	- adding missed licence header and java doc

--- de.tudarmstadt.ukp.clarin.webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/curation/RangeNameNotCheckedException.java
+++ de.tudarmstadt.ukp.clarin.webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/curation/RangeNameNotCheckedException.java
@@ -1,11 +1,32 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.clarin.webanno.brat.curation;
 
 import de.tudarmstadt.ukp.clarin.webanno.brat.controller.BratAnnotationException;
 
+/**
+ * An exception when {@link org.apache.uima.cas.Feature#getRange()#getName} is not either
+ *  uima.cas.Integer or uima.cas.String
+ * @author Seid Muhie Yimam
+ *
+ */
 public class RangeNameNotCheckedException extends BratAnnotationException{
-    /**
-     *
-     */
+
     private static final long serialVersionUID = 9006025257536279474L;
 
     public RangeNameNotCheckedException(String message)
