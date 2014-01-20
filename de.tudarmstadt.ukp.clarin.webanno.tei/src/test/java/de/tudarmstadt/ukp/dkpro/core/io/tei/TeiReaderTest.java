72fb245ab2dbfd8700a266afef49861a127ecd42
Seid Muhie Yimam <seidymam@gmail.com>
2014-01-20 12:29:34 +0000

    NO ISSUEs - no test case file to opensource

--- de.tudarmstadt.ukp.clarin.webanno.tei/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TeiReaderTest.java
+++ de.tudarmstadt.ukp.clarin.webanno.tei/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TeiReaderTest.java
@@ -24,6 +24,7 @@ import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.pipeline.JCasIterable;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
@@ -37,12 +38,13 @@ public class TeiReaderTest
 {
 
     @Test
+    @Ignore("No TEI yet to opensource ")
     public void testTeiReader()
         throws Exception
     {
         CollectionReaderDescription reader = createReaderDescription(TeiReader.class,
-                TeiReader.PARAM_LANGUAGE, "en", TeiReader.PARAM_SOURCE_LOCATION,
-                "classpath:/tei/", TeiReader.PARAM_PATTERNS, new String[] { "[+]*.xml" });
+                TeiReader.PARAM_LANGUAGE, "en", TeiReader.PARAM_SOURCE_LOCATION, "classpath:/tei/",
+                TeiReader.PARAM_PATTERNS, new String[] { "[+]*.xml" });
 
         String firstSentence = "\"På Facebook handler det egentlig bare om at blive set. "
                 + "I informationssamfundet er den største knaphedsressource andres opmærksomhed."
