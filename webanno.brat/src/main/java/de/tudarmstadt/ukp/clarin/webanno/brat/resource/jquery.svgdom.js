9df339bf4d0d24a957b63c5eb1771ea486420fe7
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-11-05 21:23:21 +0000

    #1076 - Add correct license information to brat JS files

--- webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/resource/jquery.svgdom.js
+++ webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/resource/jquery.svgdom.js
@@ -1,4 +1,24 @@
-﻿/* http://keith-wood.name/svg.html
+﻿/*
+ * Copyright 2007 - 2014 Keith Wood
+ * 
+ * ================================
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/* http://keith-wood.name/svg.html
    jQuery DOM compatibility for jQuery SVG v1.5.0.
    Written by Keith Wood (kbwood{at}iinet.com.au) April 2009.
    Available under the MIT (http://keith-wood.name/licence.html) license. 
