527ba6f34d2000f82ca55c1314641e17bb423874
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-11-11 11:50:22 +0000

    #1082 - Add JQuery-based AJAX indicator

--- webanno.webapp.home/src/main/resources/de/tudarmstadt/ukp/clarin/webanno/webapp/home/page/webanno.js
+++ webanno.webapp.home/src/main/resources/de/tudarmstadt/ukp/clarin/webanno/webapp/home/page/webanno.js
@@ -38,3 +38,36 @@ $(document).ready(function() {
     adjustSticker();
   })
 });
+
+$(document).ready(function() {
+  function hideBusysign() {
+	document.getElementById('spinner').style.display ='none';
+  }
+		 
+  function showBusysign() {
+    document.getElementById('spinner').style.display ='inline';
+  }
+		 
+  function clickFunc(eventData) {
+    var clickedElement = (window.event) ? event.srcElement : eventData.target;
+	if ((clickedElement.tagName.toUpperCase() == 'BUTTON' || 
+			clickedElement.tagName.toUpperCase() == 'A' || 
+			clickedElement.parentNode.tagName.toUpperCase() == 'A' || 
+		(clickedElement.tagName.toUpperCase() == 'INPUT' && 
+				(clickedElement.type.toUpperCase() == 'BUTTON' || 
+						clickedElement.type.toUpperCase() == 'SUBMIT')))
+		     && clickedElement.parentNode.id.toUpperCase() != 'NOBUSY' ) {
+		     showBusysign();
+    }
+  }
+  
+  document.getElementsByTagName('body')[0].onclick = clickFunc;
+  hideBusysign();
+  Wicket.Event.subscribe('/ajax/call/beforeSend', function( attributes, jqXHR, settings ) {
+    showBusysign()
+  });
+  Wicket.Event.subscribe('/ajax/call/complete', function( attributes, jqXHR, textStatus) {
+    hideBusysign()
+  });
+});
+	 
