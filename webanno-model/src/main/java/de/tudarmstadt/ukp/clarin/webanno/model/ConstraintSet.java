f5da0da98752d8f1fb21ef452ae0b15ff3721220
Richard Eckart de Castilho <richard.eckart@gmail.com>
2015-03-19 16:59:32 +0000

    #1278 - Add database and import/export model for constraints - Added to database model

---
+++ webanno-model/src/main/java/de/tudarmstadt/ukp/clarin/webanno/model/ConstraintSet.java
@@ -0,0 +1,143 @@
+/*******************************************************************************
+ * Copyright 2015
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.clarin.webanno.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * Set of constraints
+ */
+@Entity
+@Table(name = "constraint_set", uniqueConstraints = { @UniqueConstraint(columnNames = { "name",
+        "project" }) })
+public class ConstraintSet
+    implements Serializable
+{
+    private static final long serialVersionUID = 8496087166198616020L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "project")
+    private Project project;
+
+    @Lob
+    private String description;
+
+    @Lob
+    private String rules;
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long aId)
+    {
+        id = aId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String aName)
+    {
+        name = aName;
+    }
+
+    public Project getProject()
+    {
+        return project;
+    }
+
+    public void setProject(Project aProject)
+    {
+        project = aProject;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String aDescription)
+    {
+        description = aDescription;
+    }
+
+    public String getRules()
+    {
+        return rules;
+    }
+
+    public void setRules(String aRules)
+    {
+        rules = aRules;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((project == null) ? 0 : project.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ConstraintSet other = (ConstraintSet) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        }
+        else if (!name.equals(other.name))
+            return false;
+        if (project == null) {
+            if (other.project != null)
+                return false;
+        }
+        else if (!project.equals(other.project))
+            return false;
+        return true;
+    }
+}
