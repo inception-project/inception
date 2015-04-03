0ed4b35b4a48d5ba5043afacac1f52b5b9a3f1ca
Seid Muhie Yimam <seidymam@gmail.com>
2015-04-03 22:15:09 +0000

    #1283 - Annotation details should not scroll

--- webanno-webapp-home/src/main/resources/de/tudarmstadt/ukp/clarin/webanno/webapp/home/page/webanno.js
+++ webanno-webapp-home/src/main/resources/de/tudarmstadt/ukp/clarin/webanno/webapp/home/page/webanno.js
@@ -15,59 +15,88 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-$(document).ready(function() {
-  $(".top-sticker").each(function(index, sticker) {
-    var shadow = $('<div></div>').insertBefore($(sticker));
-    
-    var initialPos = shadow.position();
-    $(sticker).css("position", "fixed");
-    
-    function adjustSticker(e) {
-        shadow.css("height", sticker.offsetHeight + "px");
-        
-        var pos = shadow.position();
-        if (pos.top <= initialPos.top) {
-            sticker.style.top = Math.max(0, pos.top - $(window).scrollTop()) + "px";
-        }
-        sticker.style.left = Math.max(0,pos.left-$(window).scrollLeft()) + "px"
-    }
-    
-    $(document).on("scroll", adjustSticker);
-    $(document).on("resize", adjustSticker);
-    $(document).on("load", adjustSticker);
-    adjustSticker();
-  })
-});
+$(document).ready(
+		function() {
+			$(".top-sticker").each(
+					function(index, sticker) {
+						var shadow = $('<div></div>').insertBefore($(sticker));
+
+						var initialPos = shadow.position();
+						$(sticker).css("position", "fixed");
+
+						function adjustSticker(e) {
+							shadow.css("height", sticker.offsetHeight + "px");
+
+							var pos = shadow.position();
+							if (pos.top <= initialPos.top) {
+								sticker.style.top = Math.max(0, pos.top
+										- $(window).scrollTop())
+										+ "px";
+							}
+							sticker.style.left = Math.max(0, pos.left
+									- $(window).scrollLeft())
+									+ "px"
+						}
+
+						$(document).on("scroll", adjustSticker);
+						$(document).on("resize", adjustSticker);
+						$(document).on("load", adjustSticker);
+						adjustSticker();
+					})
+		});
 
 $(document).ready(function() {
-  function hideBusysign() {
-	document.getElementById('spinner').style.display ='none';
-  }
-		 
-  function showBusysign() {
-    document.getElementById('spinner').style.display ='inline';
-  }
-		 
-  function clickFunc(eventData) {
-    var clickedElement = (window.event) ? event.srcElement : eventData.target;
-	if ((clickedElement.tagName.toUpperCase() == 'BUTTON' || 
-			clickedElement.tagName.toUpperCase() == 'A' || 
-			clickedElement.parentNode.tagName.toUpperCase() == 'A' || 
-		(clickedElement.tagName.toUpperCase() == 'INPUT' && 
-				(clickedElement.type.toUpperCase() == 'BUTTON' || 
-						clickedElement.type.toUpperCase() == 'SUBMIT')))
-		     && clickedElement.parentNode.id.toUpperCase() != 'NOBUSY' ) {
-		     showBusysign();
-    }
-  }
-  
-  document.getElementsByTagName('body')[0].onclick = clickFunc;
-  hideBusysign();
-  Wicket.Event.subscribe('/ajax/call/beforeSend', function( attributes, jqXHR, settings ) {
-    showBusysign()
-  });
-  Wicket.Event.subscribe('/ajax/call/complete', function( attributes, jqXHR, textStatus) {
-    hideBusysign()
-  });
+	function fixAnnoPanel() {
+		var $annoPanel = $('#annotationDetailEditorPanel');
+		if ($(window).scrollTop() > 100)
+			$annoPanel.css({
+				'position' : 'fixed',
+				'top' : '100px'
+			});
+		else
+			$annoPanel.css({
+				'position' : 'relative',
+				'top' : 'auto'
+			});
+	}
+	$(window).scroll(fixAnnoPanel);
+	fixAnnoPanel();
 });
-	 
+
+$(document)
+		.ready(
+				function() {
+					function hideBusysign() {
+						document.getElementById('spinner').style.display = 'none';
+					}
+
+					function showBusysign() {
+						document.getElementById('spinner').style.display = 'inline';
+					}
+
+					function clickFunc(eventData) {
+						var clickedElement = (window.event) ? event.srcElement
+								: eventData.target;
+						if ((clickedElement.tagName.toUpperCase() == 'BUTTON'
+								|| clickedElement.tagName.toUpperCase() == 'A'
+								|| clickedElement.parentNode.tagName
+										.toUpperCase() == 'A' || (clickedElement.tagName
+								.toUpperCase() == 'INPUT' && (clickedElement.type
+								.toUpperCase() == 'BUTTON' || clickedElement.type
+								.toUpperCase() == 'SUBMIT')))
+								&& clickedElement.parentNode.id.toUpperCase() != 'NOBUSY') {
+							showBusysign();
+						}
+					}
+
+					document.getElementsByTagName('body')[0].onclick = clickFunc;
+					hideBusysign();
+					Wicket.Event.subscribe('/ajax/call/beforeSend', function(
+							attributes, jqXHR, settings) {
+						showBusysign()
+					});
+					Wicket.Event.subscribe('/ajax/call/complete', function(
+							attributes, jqXHR, textStatus) {
+						hideBusysign()
+					});
+				});
