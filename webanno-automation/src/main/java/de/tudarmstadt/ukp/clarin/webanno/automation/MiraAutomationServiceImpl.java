2ee694ad896e68d0b4868dfc13a363c55d0ec105
Richard Eckart de Castilho <richard.eckart@gmail.com>
2015-04-22 09:43:21 +0000

    #1388 - Automation not working in Experimental - Added @Transational annotations for two DB-related methods that didn't have them yet. One of them the "createAutomationStatus" mentioned in the stack trace of the issue.

--- webanno-automation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/automation/MiraAutomationServiceImpl.java
+++ webanno-automation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/automation/MiraAutomationServiceImpl.java
@@ -77,6 +77,7 @@ public class MiraAutomationServiceImpl
     }
 
     @Override
+    @Transactional
     public List<MiraTemplate> listMiraTemplates(Project aProject)
     {
         List<MiraTemplate> allTenplates = entityManager.createQuery(
@@ -157,6 +158,7 @@ public class MiraAutomationServiceImpl
     }
 
     @Override
+    @Transactional
     public AutomationStatus getAutomationStatus(MiraTemplate aTemplate)
     {
         return entityManager
