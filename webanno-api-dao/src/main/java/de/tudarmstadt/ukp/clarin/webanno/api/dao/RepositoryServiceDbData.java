00a6ea2ce7affd48efcaa33c85e213a6cffb6c88
Richard Eckart de Castilho <richard.eckart@gmail.com>
2015-04-23 23:08:37 +0000

    #1294 - Streamline reading/writing of CASes - Fix exception accessing JCas classes after reading serialized CAS.

--- webanno-api-dao/src/main/java/de/tudarmstadt/ukp/clarin/webanno/api/dao/RepositoryServiceDbData.java
+++ webanno-api-dao/src/main/java/de/tudarmstadt/ukp/clarin/webanno/api/dao/RepositoryServiceDbData.java
@@ -78,6 +78,7 @@ import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.cas.Type;
@@ -2465,6 +2466,11 @@ public class RepositoryServiceDbData
         try (ObjectInputStream is = new ObjectInputStream(new FileInputStream(aFile))) {
             CASCompleteSerializer serializer = (CASCompleteSerializer) is.readObject();
             deserializeCASComplete(serializer, aJCas.getCasImpl());
+            // Initialize the JCas sub-system which is the most often used API in DKPro Core components
+            aJCas.getCas().getJCas();
+        }
+        catch (CASException e) {
+            throw new IOException(e);
         }
         catch (ClassNotFoundException e) {
             throw new IOException(e);
