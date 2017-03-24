ddb0d0188c91ad2fcc2d7c08c3bdb061b86344ee
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-03-24 10:15:12 +0100

    #430 - Refactoring project structure (3.2.0) - Removing old unused "tests" - Moving TestCasDiff to curation module

--- webanno-ui-curation/src/test/java/de/tudarmstadt/ukp/clarin/webanno/ui/curation/service/TestCasDiff.java
+++ webanno-ui-curation/src/test/java/de/tudarmstadt/ukp/clarin/webanno/ui/curation/service/TestCasDiff.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.clarin.webanno.brat.page.curation;
+package de.tudarmstadt.ukp.clarin.webanno.ui.curation.service;
 
 import junit.framework.TestCase;
 
