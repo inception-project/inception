94abbab942524291661742337c37058f1d670c25
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-03-26 21:18:56 +0200

    #430 - Refactoring project structure (3.2.0) - Fix NPE in standalone mode when accessing favicon - Remove duplicate dependency - Move WebAnnoApplicationContextInitializer to different package

--- webanno-webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/WebAnnoApplicationContextInitializer.java
+++ webanno-webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/WebAnnoApplicationContextInitializer.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.clarin.webanno.webapp.security.preauth;
+package de.tudarmstadt.ukp.clarin.webanno.webapp;
 
 import java.io.File;
 import java.io.IOException;
