02d803445e8657f4be4ee74cb0eb499776d8c960
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-06-07 11:10:28 +0200

    #516 - Allow adding custom image links to the page header - Implemented feature

---
+++ webanno-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/wicket/ImageLink.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.clarin.webanno.support.wicket;
+
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.resource.ResourceReference;
+
+public class ImageLink
+    extends Panel
+{
+    private static final long serialVersionUID = 1384767661278796054L;
+
+    public ImageLink(String aId, ResourceReference aImageRes, IModel<String> aUrl)
+    {
+        super(aId, aUrl);
+        ExternalLink link = new ExternalLink("link", aUrl);
+        link.add(new Image("image", aImageRes));
+        add(link);
+    }
+}
