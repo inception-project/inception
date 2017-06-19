9f120b72bf3f0707b2ba5dff2564ecd2685b9904
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-06-19 17:20:20 +0200

    #520 - Perform javascript action on click - Use CAS-API-based FSUtil.getFeature() instead of fetching feature values via Java reflection API (which does not work for custom types for which there are no JCas cover classes)

--- webanno-brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/annotation/OnClickActionParser.java
+++ webanno-brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/annotation/OnClickActionParser.java
@@ -17,10 +17,6 @@
  */
 package de.tudarmstadt.ukp.clarin.webanno.brat.annotation;
 
-import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +24,7 @@ import java.util.Map;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.fit.util.FSUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,120 +36,69 @@ import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationLayer;
 import de.tudarmstadt.ukp.clarin.webanno.model.Project;
 import de.tudarmstadt.ukp.clarin.webanno.model.SourceDocument;
 
-/**
- * 
- *
- */
-public class OnClickActionParser implements Serializable {
-	
-	private static final long serialVersionUID = 3008662322929838450L;
-	
-	private final static Logger LOG = LoggerFactory.getLogger(OnClickActionParser.class); 
+public final class OnClickActionParser
+{
+    private final static Logger LOG = LoggerFactory.getLogger(OnClickActionParser.class);
 
-	/**
-	 * 
-	 * @param jstemplate in the form of e.g. "alert('${PID} ${DID} ${DNAME} ${ID}')"
-	 * @param project
-	 * @param document
-	 * @param anno
-	 * @return String with substituted variables
-	 */
-	public static Map<String, String> parse(AnnotationLayer anno_layer, List<AnnotationFeature> anno_layer_features, Project project, SourceDocument document, AnnotationFS anno){
-		Map<String, String> valuesMap = new HashMap<>();
-		// add some defaults
-		valuesMap.put("PID", String.valueOf(project.getId()));
-		valuesMap.put("PNAME", project.getName());
-		valuesMap.put("DOCID", String.valueOf(document.getId()));
-		valuesMap.put("DOCNAME", document.getName());
-		valuesMap.put("LAYERNAME", anno_layer.getUiName());
+    private OnClickActionParser()
+    {
+        // No instances
+    }
+    
+    /**
+     * @return String with substituted variables
+     */
+    public static Map<String, String> parse(AnnotationLayer aLayer,
+            List<AnnotationFeature> aFeatures, Project aProject, SourceDocument aDocument,
+            AnnotationFS aAnnotation)
+    {
+        Map<String, String> valuesMap = new HashMap<>();
 
-		// collect the methods of the annotation
-		Method[] methods = anno.getClass().getDeclaredMethods(); 
-		Arrays.stream(methods)
-			.filter(m -> m.getParameterCount() < 1 && m.getReturnType() != Void.TYPE) // only select getter methods with no parameters and a return type
-			.forEach(m -> {
-				if(m.getName().startsWith("get")){
-					String value = getStringValue(m, anno);
-					if(value != null){
-						String subst = m.getName().substring(3); // just cut-off 'get'
-						valuesMap.put(subst, value);
-					}
-					return;
-				}
-				if(m.getName().startsWith("is")){
-					String value = getStringValue(m, anno);
-					if(value != null){
-						String subst = m.getName(); // don't cut-off 'is'
-						valuesMap.put(subst, value);
-					}
-					return;
-				}
-				// else nothing to add
-			});
-		
-		// add fields from the annotation layer features and use the values from before
-		anno_layer_features.stream().forEach(feat -> {
-			String val = valuesMap.get(feat.getName());
-			if(val != null)
-				valuesMap.put(feat.getUiName(), val);
-		});
-		
-		return valuesMap;
-	}
-	
-	
-	/**
-	 * as JSON object
-	 * @param valueMap
-	 * @return map as JSON object string
-	 */
-	public static String asJSONObject(final Map<String, String> valueMap){
-		if(valueMap == null)
-			return "{ }";
-		try {
-			return new ObjectMapper().writeValueAsString(valueMap);
-		} catch (JsonProcessingException e) {
-			LOG.warn("Could not encode map to json object: '{}'.", StringUtils.abbreviate(valueMap.toString(), 100), e);
-			return String.format("{ \"%s\": \"%s\" }", e.getClass().getSimpleName(), e.getMessage());
-		}
-	}
-	
+        // add some defaults
+        valuesMap.put("PID", String.valueOf(aProject.getId()));
+        valuesMap.put("PNAME", aProject.getName());
+        valuesMap.put("DOCID", String.valueOf(aDocument.getId()));
+        valuesMap.put("DOCNAME", aDocument.getName());
+        valuesMap.put("LAYERNAME", aLayer.getUiName());
 
-	/**
-	 * Escapes values in the map
-	 * 
-	 * @param unescapedValues
-	 */
-	public static void escapeJavascript(final Map<String, String> unescapedValues){
-		unescapedValues
-			.entrySet()
-			.forEach(e -> e.setValue(escapeJavascript(e.getValue())));
-	}
-	
-	/**
-	 * @param unescaped string
-	 * @return javscript escaped string
-	 */
-	public static String escapeJavascript(String unescaped){
-		return StringEscapeUtils.escapeJava(unescaped);
-	}
-	
-	/**
-	 * Evaluate method on obj and return its return value as string or null 
-	 * @param method
-	 * @param obj
-	 * @return return value as string or null
-	 */
-	private static String getStringValue(Method method, Object obj){
-		try {
-			Object result = method.invoke(obj);
-			if(result != null)
-				return String.valueOf(result);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			LOG.debug(String.format("Some %s error happened, but probably not an important one: '%s'.", e.getClass(), e.getMessage()), e);
-			return null;
-		}
-		return null;
-	}
+        // add fields from the annotation layer features and use the values from before
+        aFeatures.stream().forEach(feat -> {
+            Object val = FSUtil.getFeature(aAnnotation, feat.getName(), Object.class);
+            if (val != null) {
+                valuesMap.put(feat.getUiName(), String.valueOf(val));
+            }
+        });
 
+        return valuesMap;
+    }
+
+    /**
+     * as JSON object
+     * 
+     * @return map as JSON object string
+     */
+    public static String asJSONObject(final Map<String, String> aValueMap)
+    {
+        if (aValueMap == null) {
+            return "{ }";
+        }
+        try {
+            return new ObjectMapper().writeValueAsString(aValueMap);
+        }
+        catch (JsonProcessingException e) {
+            LOG.warn("Could not encode map to json object: '{}'.",
+                    StringUtils.abbreviate(aValueMap.toString(), 100), e);
+            return String.format("{ \"%s\": \"%s\" }", e.getClass().getSimpleName(),
+                    e.getMessage());
+        }
+    }
+
+    /**
+     * Escapes values in the map
+     */
+    public static void escapeJavascript(final Map<String, String> aUnescapedValues)
+    {
+        aUnescapedValues.entrySet()
+                .forEach(e -> e.setValue(StringEscapeUtils.escapeJava(e.getValue())));
+    }
 }
