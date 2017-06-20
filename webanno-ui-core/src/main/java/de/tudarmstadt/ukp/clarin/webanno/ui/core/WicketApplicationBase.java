8428f2cafeba5ad810726d7cdd834aa394a3249a
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-06-20 22:54:53 +0200

    #514 - Missing entries in i18n string bundles cause exception - Added workaround: catch Exception and return null

--- webanno-ui-core/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/core/WicketApplicationBase.java
+++ webanno-ui-core/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/core/WicketApplicationBase.java
@@ -20,10 +20,13 @@ package de.tudarmstadt.ukp.clarin.webanno.ui.core;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.Component;
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.core.request.mapper.HomePageMapper;
@@ -171,7 +174,36 @@ public abstract class WicketApplicationBase
         List<IStringResourceLoader> loaders = new ArrayList<>(
                 getResourceSettings().getStringResourceLoaders());
         NestedStringResourceLoader nestedLoader = new NestedStringResourceLoader(loaders,
-                Pattern.compile("#\\(([^ ]*?)\\)"));
+                Pattern.compile("#\\(([^ ]*?)\\)")) {
+            
+            // Workaround for https://issues.apache.org/jira/browse/WICKET-6393
+            // Can be remove with Wicket 7.8.0 or 8.0.0-M7
+            @Override
+            public String loadStringResource(Class<?> aClazz, String aKey, Locale aLocale,
+                    String aStyle, String aVariation)
+            {
+                try {
+                    return super.loadStringResource(aClazz, aKey, aLocale, aStyle, aVariation);
+                }
+                catch (MissingResourceException e) {
+                    return null;
+                }
+            }
+            
+            // Workaround for https://issues.apache.org/jira/browse/WICKET-6393
+            // Can be remove with Wicket 7.8.0 or 8.0.0-M7
+            @Override
+            public String loadStringResource(Component aComponent, String aKey,
+                    Locale aLocale, String aStyle, String aVariation)
+            {
+                try {
+                    return super.loadStringResource(aComponent, aKey, aLocale, aStyle, aVariation);
+                }
+                catch (MissingResourceException e) {
+                    return null;
+                }
+            }
+        };
         getResourceSettings().getStringResourceLoaders().clear();
         getResourceSettings().getStringResourceLoaders().add(nestedLoader);
     }
