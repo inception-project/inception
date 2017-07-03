24dba80e7d4625ab027a795935d6048856f679a7
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-07-03 08:33:30 +0200

    #486 - Refactoring project structure (3.3.0)
    - Base class for overview lists to avoid always having to override the default choice

---
+++ webanno-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/wicket/OverviewListChoice.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package de.tudarmstadt.ukp.clarin.webanno.support.wicket;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.form.ListChoice;
+import org.apache.wicket.model.IModel;
+
+public class OverviewListChoice<T>
+    extends ListChoice<T>
+{
+    private static final long serialVersionUID = 1L;
+
+    public OverviewListChoice(String aId, IModel<? extends List<? extends T>> aChoices,
+            IChoiceRenderer<? super T> aRenderer)
+    {
+        super(aId, aChoices, aRenderer);
+    }
+
+    public OverviewListChoice(String aId, IModel<? extends List<? extends T>> aChoices)
+    {
+        super(aId, aChoices);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel,
+            IModel<? extends List<? extends T>> aChoices, IChoiceRenderer<? super T> aRenderer,
+            int aMaxRows)
+    {
+        super(aId, aModel, aChoices, aRenderer, aMaxRows);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel,
+            IModel<? extends List<? extends T>> aChoices, IChoiceRenderer<? super T> aRenderer)
+    {
+        super(aId, aModel, aChoices, aRenderer);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel,
+            IModel<? extends List<? extends T>> aChoices)
+    {
+        super(aId, aModel, aChoices);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel, List<? extends T> aChoices,
+            IChoiceRenderer<? super T> aRenderer, int aMaxRows)
+    {
+        super(aId, aModel, aChoices, aRenderer, aMaxRows);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel, List<? extends T> aChoices,
+            IChoiceRenderer<? super T> aRenderer)
+    {
+        super(aId, aModel, aChoices, aRenderer);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel, List<? extends T> aChoices,
+            int aMaxRows)
+    {
+        super(aId, aModel, aChoices, aMaxRows);
+    }
+
+    public OverviewListChoice(String aId, IModel<T> aModel, List<? extends T> aChoices)
+    {
+        super(aId, aModel, aChoices);
+    }
+
+    public OverviewListChoice(String aId, List<? extends T> aChoices,
+            IChoiceRenderer<? super T> aRenderer)
+    {
+        super(aId, aChoices, aRenderer);
+    }
+
+    public OverviewListChoice(String aId, List<? extends T> aChoices)
+    {
+        super(aId, aChoices);
+    }
+
+    public OverviewListChoice(String aId)
+    {
+        super(aId);
+    }
+    
+    @Override
+    protected CharSequence getDefaultChoice(String aSelectedValue)
+    {
+        return "";
+    }
+}
