5b127d6d07765b4178aa0e6750764dfd1085407f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-10-20 15:56:33 +0200

    #676 - Improved TSV3 reader and writer
    - Added a new reader, a new writer, and corresponding unit tests.

---
+++ webanno-io-tsv/src/test/java/de/tudarmstadt/ukp/clarin/webanno/tsv/WebAnnoTsv3XWriterTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.clarin.webanno.tsv;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+
+public class WebAnnoTsv3XWriterTest
+    extends WebAnnoTsv3WriterTestBase
+{
+    @Override
+    protected AnalysisEngineDescription makeWriter() throws ResourceInitializationException
+    {
+        return createEngineDescription(WebannoTsv3XWriter.class);
+    }
+
+    @Override
+    protected String getSuiteName() throws ResourceInitializationException
+    {
+        return "tsv3-suite";
+    }
+
+    @Override
+    protected boolean isKnownToFail(String aMethodName)
+    {
+        return false;
+    }
+}
