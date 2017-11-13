792eb548909b66ae3bc82a609819f0fbcece92f1
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-11-13 19:38:57 +0100

    #703 - Improve developer documentation (3.3.0)

--- webanno-api-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/api/annotation/adapter/TypeAdapter.java
+++ webanno-api-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/api/annotation/adapter/TypeAdapter.java
@@ -24,28 +24,38 @@ import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.cas.Type;
 import org.apache.uima.jcas.JCas;
 
+import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.VID;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationLayer;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 /**
- * Type Adapters for span, arc, and chain annotations
+ * A type adapter encapsulates a specific kind of annotation layer, e.g. spans, relations or chains.
+ * A type adapter can be obtained via {@link AnnotationSchemaService#getAdapter} and is always
+ * created for one concrete layer (e.g. POS, NamedEntity). It allows interacting with annotations on
+ * the layer, e.g. creating annotations, deleting annotations, setting and getting feature values.
+ * Most actions are defined by implementations of this interface such as {@link SpanAdapter},
+ * {@link ArcAdapter} or {@link ChainAdapter}.
  */
 public interface TypeAdapter
 {
+    /**
+     * String used to separate multiple values in the annotation label displayed in the UI.
+     */
     String FEATURE_SEPARATOR = " | ";
 
     /**
-     * The ID of the type.
-     *
-     * @return the ID.
+     * @return the database ID of the type.
      */
     long getTypeId();
 
     /**
      * Get the CAS type of the this {@link TypeAdapter}
      *
-     * @param cas the CAS.
+     * @param cas
+     *            the CAS.
      * @return the type.
      */
     Type getAnnotationType(CAS cas);
@@ -58,18 +68,22 @@ public interface TypeAdapter
     String getAnnotationTypeName();
 
     /**
-     * determine the type of Span annotation to be used to have arc annotations (as Origin and
-     * target)
+     * If the underlying layer is attached to another layer, get the name of the feature in the
+     * other layer which is used to refer to annotations on the underlying layer. For example, every
+     * annotation on the {@link POS} layer must correspond to an annotation on the {@link Token}
+     * layer and the token feature {@link Token#getPos()} must point the POS annotation. Thus, for
+     * the POS layer, this method would return {@code pos} and the method
+     * {@link #getAttachTypeName()} would return
+     * {@code de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token}.
      *
      * @return the attach feature name.
      */
     String getAttachFeatureName();
 
     /**
-     * determine the type of Span annotation to be used to have arc annotations (as Origin and
-     * target)
-     *
-     * @return the attach type name.
+     * @return the name of the layer the current layer attaches to (if any)
+     * 
+     * @see #getAttachFeatureName()
      */
     String getAttachTypeName();
 
@@ -90,12 +104,21 @@ public interface TypeAdapter
      */
     void delete(JCas aJCas, VID aVid);
 
+    /**
+     * @return the layer for which this adapter has been created.
+     */
     AnnotationLayer getLayer();
-    
+
+    /**
+     * @return the features defined for this layer.
+     * 
+     * @see AnnotationSchemaService#listAnnotationFeature(AnnotationLayer)
+     */
     Collection<AnnotationFeature> listFeatures();
 
     /**
-     * Update this feature with a new value.
+     * Set the value of the given feature.
+     * 
      * @param aFeature
      *            the feature.
      * @param aJcas
@@ -107,5 +130,14 @@ public interface TypeAdapter
      */
     void setFeatureValue(AnnotationFeature aFeature, JCas aJcas, int aAddress, Object aValue);
 
+    /**
+     * Get the value of the given feature.
+     * 
+     * @param aFeature
+     *            the feature.
+     * @param aFs
+     *            the annotation.
+     * @return the feature value.
+     */
     <T> T getFeatureValue(AnnotationFeature aFeature, FeatureStructure aFs);
 }
