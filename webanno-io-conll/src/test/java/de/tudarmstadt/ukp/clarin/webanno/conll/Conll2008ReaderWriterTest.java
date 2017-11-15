d9f6524c3a8aa162153605fe300a839bd8637670
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-11-15 11:07:54 +0100

    #705 - SRL annotation with CoNLL 2009 format
    - Backported CoNLL 2008 and CoNLL 2009 formats from DKPro Core 1.9.0.

---
+++ webanno-io-conll/src/test/java/de/tudarmstadt/ukp/clarin/webanno/conll/Conll2008ReaderWriterTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.clarin.webanno.conll;
+
+import static de.tudarmstadt.ukp.dkpro.core.testing.IOTestRunner.testOneWay;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.testing.DkproTestContext;
+
+public class Conll2008ReaderWriterTest
+{
+    @Test
+    public void test()
+        throws Exception
+    {
+        testOneWay(Conll2008Reader.class, Conll2008Writer.class, "conll/2008/en-ref.conll",
+                "conll/2008/en-orig.conll");
+    }
+
+    @Rule
+    public DkproTestContext testContext = new DkproTestContext();
+}
