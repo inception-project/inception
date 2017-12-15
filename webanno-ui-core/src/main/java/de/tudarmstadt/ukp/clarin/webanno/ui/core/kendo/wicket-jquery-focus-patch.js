333b302de6ee33f6470f5eed0be83519bbe54102
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-12-15 11:16:14 +0100

    #772 - ComboBox feature editor not focussed on annotation selection
    - Correct license header and mention in NOTICE.txt

--- webanno-ui-core/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/core/kendo/wicket-jquery-focus-patch.js
+++ webanno-ui-core/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/core/kendo/wicket-jquery-focus-patch.js
@@ -1,13 +1,12 @@
 /*
- * Copyright 2017
- * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
- * Technische Universität Darmstadt
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +14,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*global DOMParser: true, ActiveXObject: true, console: true */
+
+/*
+ * Wicket Ajax Support
+ *
+ * WEBANNO: This file reproduces some JS code from wicket-ajax-jquery.js and introduces 
+ *          fixes for handling the focus on specific Kendo components.
+ *
+ * @author Igor Vaynberg
+ * @author Matej Knopp
+ */
+
 
 Wicket.Focus.requestFocus = function() {
 	// if the focused component is replaced by the ajax response, a re-focus might be needed
