ab177fcc4f31b3275389d3ec717adce364b996a6
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-02-01 22:23:37 +0100

    #828 - Simplify registries
    - Formatting

--- webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/sidebar/AnnotationSidebarRegistryImpl.java
+++ webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/sidebar/AnnotationSidebarRegistryImpl.java
@@ -20,6 +20,7 @@ package de.tudarmstadt.ukp.clarin.webanno.ui.annotation.sidebar;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import org.apache.commons.lang3.ClassUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
