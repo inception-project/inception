4022919bad7965a269adb9cd4ca5eec315f47eb9
Wei Ding <ding.wei.cs@gmail.com>
2018-02-11 15:23:00 +0100

    #12 - Mention feature type for fact linking
    - Added fact support class
    - Added 3 editor skeletons

---
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/FaceFeaturePredicateEditor.java
@@ -0,0 +1,21 @@
+package de.tudarmstadt.ukp.inception.kb.feature;
+
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.FeatureEditor;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.FeatureState;
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.model.IModel;
+
+/**
+ *
+ */
+public class FaceFeaturePredicateEditor extends FeatureEditor {
+    public FaceFeaturePredicateEditor(String aId, MarkupContainer aOwner, IModel<FeatureState> aModel) {
+        super(aId, aOwner, aModel);
+    }
+
+    @Override
+    public Component getFocusComponent() {
+        return null;
+    }
+}
