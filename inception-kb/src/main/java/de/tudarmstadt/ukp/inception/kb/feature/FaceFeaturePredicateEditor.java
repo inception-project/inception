2c041190b3d718b9b8437c41b10699406c9fdb49
Wei Ding <ding.wei.cs@gmail.com>
2018-02-18 19:55:04 +0100

    #12 - Mention feature type for fact linking
    - Simple predicate UI for selection

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/FaceFeaturePredicateEditor.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/FaceFeaturePredicateEditor.java
@@ -1,7 +1,9 @@
 package de.tudarmstadt.ukp.inception.kb.feature;
 
 import com.googlecode.wicket.kendo.ui.form.dropdown.DropDownList;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.action.AnnotationActionHandler;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.FeatureEditor;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.FeatureState;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
 import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaModel;
@@ -12,6 +14,7 @@ import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -23,34 +26,22 @@ import java.util.List;
  *
  */
 public class FaceFeaturePredicateEditor extends FeatureEditor {
-    private Label feature;
-    private Component featureKey;
-    private DropDownList<KBHandle>  featureValue;
 
-    private @SpringBean
-    KnowledgeBaseService kbService;
+    private Component focusComponent;
 
-    public FaceFeaturePredicateEditor(String aId, MarkupContainer aOwner, IModel<FeatureState> aModel) {
-        super(aId, aOwner, aModel);
-        updateName();
-        updateKey();
-        updateValue();
+    private @SpringBean KnowledgeBaseService kbService;
+
+    public FaceFeaturePredicateEditor (String aId, MarkupContainer aOwner, AnnotationActionHandler aHandler,
+                                       IModel<AnnotatorState> aStateModel, IModel<FeatureState> aFeatureStateModel)
+    {
+        super(aId, aOwner, new CompoundPropertyModel<>(aFeatureStateModel));
+        add(new Label("feature", getModelObject().feature.getUiName()));
+        add(focusComponent = createFieldComboBox());
     }
 
-
-    public void updateName() {
-        feature = new Label("feature", getModelObject().feature.getUiName());
-        add(feature);
-
-    }
-
-    public void updateKey() {
-        featureKey = new Label("featureKey", "<Select to fill>");
-        add(featureKey);
-    }
-
-    public void updateValue() {
-        featureValue = new DropDownList<>("featureValue", LambdaModel.of(() -> {
+    private DropDownList<KBHandle> createFieldComboBox()
+    {
+        DropDownList<KBHandle> field = new DropDownList<>("value", LambdaModel.of(() -> {
             AnnotationFeature feat = getModelObject().feature;
             List<KBHandle> handles = new LinkedList<>();
             for (KnowledgeBase kb : kbService.getKnowledgeBases(feat.getProject())) {
@@ -58,12 +49,21 @@ public class FaceFeaturePredicateEditor extends FeatureEditor {
             }
             return new ArrayList<>(handles);
         }), new ChoiceRenderer<>("uiLabel"));
-        featureValue.setOutputMarkupId(true);
-        featureValue.setMarkupId(ID_PREFIX + getModelObject().feature.getId());
-        add(featureValue);
+        field.setOutputMarkupId(true);
+        field.setMarkupId(ID_PREFIX + getModelObject().feature.getId());
+        return field;
     }
+
     @Override
-    public Component getFocusComponent() {
-        return featureValue;
+    protected void onInitialize()
+    {
+        super.onInitialize();
+    }
+
+    @Override
+    public Component getFocusComponent()
+    {
+        return focusComponent;
     }
 }
+
