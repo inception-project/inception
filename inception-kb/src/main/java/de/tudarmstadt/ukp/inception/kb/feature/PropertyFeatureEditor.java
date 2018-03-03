076cd78599884bb415d61de63878d5ae297d7c0d
Wei Ding <ding.wei.cs@gmail.com>
2018-03-03 20:26:30 +0100

    #12 - Mention feature type for fact linking
    - Separated featureSupport for predicate and subject/object
    - subject and object share the same feature editor

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/PropertyFeatureEditor.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/PropertyFeatureEditor.java
@@ -1,15 +1,26 @@
+/*
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.tudarmstadt.ukp.inception.kb.feature;
 
-import com.googlecode.wicket.kendo.ui.form.dropdown.DropDownList;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.action.AnnotationActionHandler;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.FeatureEditor;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.FeatureState;
-import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
-import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaModel;
-import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
-import de.tudarmstadt.ukp.inception.kb.graph.KBHandle;
-import de.tudarmstadt.ukp.inception.kb.model.KnowledgeBase;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -18,22 +29,25 @@ import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import com.googlecode.wicket.kendo.ui.form.dropdown.DropDownList;
 
-/**
- *
- */
-public class FaceFeaturePredicateEditor extends FeatureEditor {
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.FeatureEditor;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.FeatureState;
+import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
+import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaModel;
+import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
+import de.tudarmstadt.ukp.inception.kb.graph.KBHandle;
+import de.tudarmstadt.ukp.inception.kb.model.KnowledgeBase;
+
+public class PropertyFeatureEditor extends FeatureEditor {
 
     private static final long serialVersionUID = -4649541419448384970L;
     private Component focusComponent;
 
     private @SpringBean KnowledgeBaseService kbService;
 
-    public FaceFeaturePredicateEditor (String aId, MarkupContainer aOwner, AnnotationActionHandler aHandler,
-                                       IModel<AnnotatorState> aStateModel, IModel<FeatureState> aFeatureStateModel)
+    public PropertyFeatureEditor(String aId, MarkupContainer aOwner,
+                                 IModel<FeatureState> aFeatureStateModel)
     {
         super(aId, aOwner, new CompoundPropertyModel<>(aFeatureStateModel));
         add(new Label("feature", getModelObject().feature.getUiName()));
