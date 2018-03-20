ac114aad76145b6d448684a0f69fddb6e4b718a8
Wei Ding <ding.wei.cs@gmail.com>
2018-03-21 00:37:22 +0100

    #12 - Mention feature type for fact linking
    - Fix the license
    - Formatting
    - use ArrayList and return directly
    - .orElseThrow(NoSuchElementException::new)
    - remove final
    - collect duplicate codes into functions and give good names
    - use fall through
    - change "magic string"s to constants

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/PropertyFeatureSupport.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/PropertyFeatureSupport.java
@@ -21,7 +21,6 @@ import static de.tudarmstadt.ukp.clarin.webanno.api.annotation.util.WebAnnoCasUt
 import static de.tudarmstadt.ukp.clarin.webanno.api.annotation.util.WebAnnoCasUtil.setFeature;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Stream;
@@ -49,24 +48,26 @@ import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
 import de.tudarmstadt.ukp.inception.kb.graph.KBHandle;
 import de.tudarmstadt.ukp.inception.kb.graph.KBProperty;
 
-
 @Component
-public class PropertyFeatureSupport implements FeatureSupport {
+public class PropertyFeatureSupport
+    implements FeatureSupport
+{
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Resource private KnowledgeBaseService kbService;
     private static final String PREDICATE_KEY = "Property: Predicate";
 
     @Override
-    public List<FeatureType> getSupportedFeatureTypes(AnnotationLayer aAnnotationLayer) {
-        List<FeatureType> types = new LinkedList<>();
+    public List<FeatureType> getSupportedFeatureTypes(AnnotationLayer aAnnotationLayer)
+    {
+        List<FeatureType> types = new ArrayList<>();
         types.add(new FeatureType(PREDICATE_KEY, PREDICATE_KEY));
-
-        return new ArrayList<>(types);
+        return types;
     }
 
     @Override
-    public boolean accepts(AnnotationFeature aFeature) {
+    public boolean accepts(AnnotationFeature aFeature)
+    {
         switch (aFeature.getMultiValueMode()) {
         case NONE:
             switch (aFeature.getType()) {
@@ -83,7 +84,8 @@ public class PropertyFeatureSupport implements FeatureSupport {
 
     @Override
     public String renderFeatureValue(AnnotationFeature aFeature, AnnotationFS aFS,
-                                     Feature aLabelFeature) {
+        Feature aLabelFeature)
+    {
         try {
             String value = aFS.getFeatureValueAsString(aLabelFeature);
             String renderValue = null;
@@ -91,50 +93,49 @@ public class PropertyFeatureSupport implements FeatureSupport {
                 // FIXME Since this might be called very often during rendering, it *might* be
                 // worth to set up an LRU cache instead of relying on the performance of the
                 // underlying KB store.
-                renderValue = kbService.getKnowledgeBases(aFeature.getProject())
-                    .stream()
+                renderValue = kbService.getKnowledgeBases(aFeature.getProject()).stream()
                     .map(k -> kbService.readProperty(k, value))
                     .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
-                    .map(KBProperty::getUiLabel)
-                    .findAny()
-                    .orElseThrow(NoSuchElementException::new);
+                    .map(KBProperty::getUiLabel).findAny().orElseThrow(NoSuchElementException::new);
             }
             return renderValue;
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             log.error("Unable to render feature value", e);
             return "ERROR";
         }
     }
 
     @Override
-    public void setFeatureValue(JCas aJcas, AnnotationFeature aFeature, int aAddress, Object
-        aValue) {
+    public void setFeatureValue(JCas aJcas, AnnotationFeature aFeature, int aAddress,
+        Object aValue)
+    {
         KBHandle kbProp = (KBHandle) aValue;
         FeatureStructure fs = selectByAddr(aJcas, FeatureStructure.class, aAddress);
         setFeature(fs, aFeature, kbProp != null ? kbProp.getIdentifier() : null);
     }
 
     @Override
-    public KBHandle getFeatureValue(AnnotationFeature aFeature, FeatureStructure aFs) {
+    public KBHandle getFeatureValue(AnnotationFeature aFeature, FeatureStructure aFs)
+    {
         String value = (String) FeatureSupport.super.getFeatureValue(aFeature, aFs);
         if (value != null) {
-            KBProperty prop = kbService.getKnowledgeBases(aFeature.getProject())
-                .stream()
+            KBProperty prop = kbService.getKnowledgeBases(aFeature.getProject()).stream()
                 .map(k -> kbService.readProperty(k, value))
-                .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
-                .findAny()
-                .orElseThrow(() -> new NoSuchElementException());
+                .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty)).findAny()
+                .orElseThrow(NoSuchElementException::new);
             return new KBHandle(prop.getIdentifier(), prop.getName());
-        } else {
+        }
+        else {
             return null;
         }
     }
 
     @Override
     public FeatureEditor createEditor(String aId, MarkupContainer aOwner,
-                                      AnnotationActionHandler aHandler, final
-                                      IModel<AnnotatorState> aStateModel,
-                                      final IModel<FeatureState> aFeatureStateModel) {
+        AnnotationActionHandler aHandler, final IModel<AnnotatorState> aStateModel,
+        final IModel<FeatureState> aFeatureStateModel)
+    {
         FeatureState featureState = aFeatureStateModel.getObject();
         final FeatureEditor editor;
 
@@ -142,7 +143,8 @@ public class PropertyFeatureSupport implements FeatureSupport {
         case NONE:
             if (featureState.feature.getType().startsWith("Property")) {
                 editor = new PropertyFeatureEditor(aId, aOwner, aFeatureStateModel);
-            } else {
+            }
+            else {
                 throw unsupportedMultiValueModeException(featureState);
             }
             break;
