ae0f08d88465d5c9863bcae3b407200a2b923f9b
Wei Ding <ding.wei.cs@gmail.com>
2018-04-06 20:17:39 +0200

    #23 - FeatureEditor and FeatureSupport for Modifiers of a fact
    - UI of selecting subject: role, label and dropdown

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/FactFeatureSubjectEditor.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/feature/FactFeatureSubjectEditor.java
@@ -1,19 +1,9 @@
 package de.tudarmstadt.ukp.inception.kb.feature;
 
-import com.googlecode.wicket.kendo.ui.form.TextField;
-import com.googlecode.wicket.kendo.ui.form.dropdown.DropDownList;
-import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.action.AnnotationActionHandler;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.FeatureEditor;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.LinkFeatureEditor;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.FeatureState;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.LinkWithRoleModel;
-import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
-import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaModel;
-import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
-import de.tudarmstadt.ukp.inception.kb.graph.KBHandle;
-import de.tudarmstadt.ukp.inception.kb.model.KnowledgeBase;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxEventBehavior;
@@ -21,22 +11,26 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.AbstractTextComponent;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.RefreshingView;
-import org.apache.wicket.markup.repeater.util.ModelIteratorAdapter;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import com.googlecode.wicket.kendo.ui.form.dropdown.DropDownList;
+import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.action.AnnotationActionHandler;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.editor.FeatureEditor;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.FeatureState;
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.LinkWithRoleModel;
+import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
+import de.tudarmstadt.ukp.clarin.webanno.model.LinkMode;
+import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaModel;
+import de.tudarmstadt.ukp.dkpro.core.api.semantics.type.SemArgLink;
+import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
+import de.tudarmstadt.ukp.inception.kb.graph.KBHandle;
+import de.tudarmstadt.ukp.inception.kb.model.KnowledgeBase;
 
 /**
  *
@@ -48,15 +42,15 @@ public class FactFeatureSubjectEditor extends FeatureEditor {
     private WebMarkupContainer content;
 
     @SuppressWarnings("rawtypes")
-    private AbstractTextComponent field;
+    private Component focusComponent;
     private boolean hideUnconstraintFeature;
 
     private AnnotationActionHandler actionHandler;
     private IModel<AnnotatorState> stateModel;
 
-    // Wicket component is bound to this property
     @SuppressWarnings("unused")
     private String newRole = "subj";
+    private LinkWithRoleModel subjectModel;
 
     private @SpringBean KnowledgeBaseService kbService;
 
@@ -74,94 +68,90 @@ public class FactFeatureSubjectEditor extends FeatureEditor {
         content.setOutputMarkupId(true);
         add(content);
 
+        FactFeatureSubjectEditor.this.getModelObject().feature.setLinkMode(LinkMode.WITH_ROLE);
+        FactFeatureSubjectEditor.this.getModelObject().feature.setLinkTypeName(SemArgLink.class.getName());
+        FactFeatureSubjectEditor.this.getModelObject().feature.setLinkTypeRoleFeatureName("role");
+        FactFeatureSubjectEditor.this.getModelObject().feature.setLinkTypeTargetFeatureName("target");
+
         List<LinkWithRoleModel> links = (List<LinkWithRoleModel>) FactFeatureSubjectEditor.this
             .getModelObject().value;
+
         AnnotatorState state = FactFeatureSubjectEditor.this.stateModel.getObject();
 
-        LinkWithRoleModel m = new LinkWithRoleModel();
-        m.role = newRole;
-        links.add(m);
+        subjectModel = new LinkWithRoleModel();
+        subjectModel.role = newRole;
+        links.add(subjectModel);
         state.setArmedSlot(FactFeatureSubjectEditor.this.getModelObject().feature, links.size() - 1);
 
-
-        content.add(new RefreshingView<LinkWithRoleModel>("slots",
-            PropertyModel.of(getModel(), "value"))
-        {
-            private static final long serialVersionUID = 5475284956525780698L;
-
-            @Override
-            protected Iterator<IModel<LinkWithRoleModel>> getItemModels()
-            {
-                return new ModelIteratorAdapter<LinkWithRoleModel>(
-                    (List<LinkWithRoleModel>) FactFeatureSubjectEditor.this.getModelObject().value)
-                {
-                    @Override
-                    protected IModel<LinkWithRoleModel> model(LinkWithRoleModel aObject)
-                    {
-                        return Model.of(aObject);
-                    }
-                };
-            }
-
-            @Override
-            protected void populateItem(final Item<LinkWithRoleModel> aItem)
-            {
-                AnnotatorState state = stateModel.getObject();
-
-                aItem.setModel(new CompoundPropertyModel<>(aItem.getModelObject()));
-                Label role = new Label("role");
-                aItem.add(role);
-
-                final Label label;
-                if (aItem.getModelObject().targetAddr == -1
-                    && state.isArmedSlot(getModelObject().feature, aItem.getIndex())) {
-                    label = new Label("label", "<Select to fill>");
-                }
-                else {
-                    label = new Label("label");
-                }
-                label.add(new AjaxEventBehavior("click")
-                {
-                    private static final long serialVersionUID = 7633309278417475424L;
-
-                    @Override
-                    protected void onEvent(AjaxRequestTarget aTarget)
-                    {
-                        actionToggleArmedState(aTarget, aItem);
-                    }
-                });
-                label.add(new AttributeAppender("style", new Model<String>()
-                {
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    public String getObject()
-                    {
-                        if (state.isArmedSlot(getModelObject().feature, aItem.getIndex())) {
-                            return "; background: orange";
-                        }
-                        else {
-                            return "";
-                        }
-                    }
-                }));
-                aItem.add(label);
-            }
-        });
-
-
+        content.add(new Label("role", subjectModel.role));
+        content.add(createSubjectLabel());
+        content.add(focusComponent = createFieldComboBox());
     }
 
-    private void actionToggleArmedState(AjaxRequestTarget aTarget, Item<LinkWithRoleModel> aItem)
+    private Label createSubjectLabel() {
+        AnnotatorState state = stateModel.getObject();
+        final Label label;
+        if (subjectModel.targetAddr == -1
+            && state.isArmedSlot(getModelObject().feature, 0)) {
+            label = new Label("label", "<Select to fill>");
+        }
+        else {
+            label = new Label("label");
+        }
+        label.add(new AjaxEventBehavior("click")
+        {
+            private static final long serialVersionUID = 7633309278417475424L;
+
+            @Override
+            protected void onEvent(AjaxRequestTarget aTarget)
+            {
+                actionToggleArmedState(aTarget);
+            }
+        });
+        label.add(new AttributeAppender("style", new Model<String>()
+        {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public String getObject()
+            {
+                if (state.isArmedSlot(getModelObject().feature, 0)) {
+                    return "; background: orange";
+                }
+                else {
+                    return "";
+                }
+            }
+        }));
+        return  label;
+    }
+
+    private DropDownList<KBHandle> createFieldComboBox()
+    {
+        DropDownList<KBHandle> field = new DropDownList<>("value", LambdaModel.of(() -> {
+            AnnotationFeature feat = getModelObject().feature;
+            List<KBHandle> handles = new LinkedList<>();
+            for (KnowledgeBase kb : kbService.getKnowledgeBases(feat.getProject())) {
+                handles.addAll(kbService.listConcepts(kb, true));
+            }
+            return new ArrayList<>(handles);
+        }), new ChoiceRenderer<>("uiLabel"));
+        field.setOutputMarkupId(true);
+        field.setMarkupId(ID_PREFIX + getModelObject().feature.getId());
+        return field;
+    }
+
+
+    private void actionToggleArmedState(AjaxRequestTarget aTarget)
     {
         AnnotatorState state = FactFeatureSubjectEditor.this.stateModel.getObject();
 
-        if (state.isArmedSlot(getModelObject().feature, aItem.getIndex())) {
+        if (state.isArmedSlot(getModelObject().feature, 0)) {
             state.clearArmedSlot();
             aTarget.add(content);
         }
         else {
-            state.setArmedSlot(getModelObject().feature, aItem.getIndex());
+            state.setArmedSlot(getModelObject().feature, 0);
             // Need to re-render the whole form because a slot in another
             // link editor might get unarmed
             aTarget.add(getOwner());
@@ -170,6 +160,6 @@ public class FactFeatureSubjectEditor extends FeatureEditor {
 
     @Override
     public Component getFocusComponent() {
-        return field;
+        return focusComponent;
     }
 }
