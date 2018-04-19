cf7ace286f39d7a40dad917b1a98aebe7cc6de52
Wei Ding <ding.wei.cs@gmail.com>
2018-04-19 02:06:30 +0200

    #915 - Add Ajax Event in AnnotationDetailEditorPanel
    * Add a new Ajax Event after Annotation updates

---
+++ webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/event/AjaxAfterAnnotationUpdateEvent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.clarin.webanno.ui.annotation.event;
+
+import java.io.Serializable;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+
+import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
+
+public class AjaxAfterAnnotationUpdateEvent
+{
+    protected AjaxRequestTarget target;
+    private AnnotatorState annotatorState;
+    private Serializable value;
+
+    public AjaxAfterAnnotationUpdateEvent(AjaxRequestTarget aTarget, AnnotatorState aState,
+            Serializable aValue)
+    {
+        target = aTarget;
+        annotatorState = aState;
+        value = aValue;
+    }
+
+    public AjaxRequestTarget getTarget()
+    {
+        return target;
+    }
+
+    public AnnotatorState getAnnotatorState()
+    {
+        return annotatorState;
+    }
+
+    public Serializable getValue()
+    {
+        return value;
+    }
+}
