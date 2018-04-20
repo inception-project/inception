22a5c8ae9795e7fdc51eaad911227c541f75a33a
Peter Jiang <peter.jiang@mailbox.org>
2018-04-20 21:47:29 +0200

    Revert "#77 - Improve Candidate Selection Step by refining SPARQL queries"
    This reverts commit 51bb182bd9d927e41760dd389f7036b8f81579b1.

--- inception-concept-linking/src/main/java/de/tudarmstadt/ukp/inception/conceptlinking/util/QueryUtil.java
+++ inception-concept-linking/src/main/java/de/tudarmstadt/ukp/inception/conceptlinking/util/QueryUtil.java
@@ -91,7 +91,6 @@ public class QueryUtil
             "        ?e2 ?labelpredicate ?altLabel.",
             "        ?altLabel bif:contains '?entityLabel'. ",
             "        ?e2 schema:description ?description.",
-            "        FILTER ( lang(?description) = \"en\" )",
             "      }",
             "    }",
             "  }",
