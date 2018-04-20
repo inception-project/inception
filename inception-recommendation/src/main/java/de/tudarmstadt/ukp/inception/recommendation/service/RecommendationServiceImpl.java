c68afcd1ee65da0c0e3883882e0f2863ed120b18
Peter Jiang <peter.jiang@mailbox.org>
2018-04-20 15:03:59 +0200

    No issue. Removed unused imports.

--- inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/service/RecommendationServiceImpl.java
+++ inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/service/RecommendationServiceImpl.java
@@ -18,8 +18,6 @@
 package de.tudarmstadt.ukp.inception.recommendation.service;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
