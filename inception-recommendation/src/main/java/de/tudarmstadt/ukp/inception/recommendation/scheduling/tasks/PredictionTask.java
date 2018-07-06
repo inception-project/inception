29342a884235c5b8ac393ff03cecc45ba17d191d
Peter Jiang <peter.jiang@mailbox.org>
2018-07-06 12:58:14 +0200

    #369 - Active learning may suggest annotations that are not visible on screen
    - Implemented CAS-based filtering of predictions in PredictionTask
    - Refactor setting visibility to own method because it is called in multiple places
    - Make the methods static so the Renderer may call them

--- inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/scheduling/tasks/PredictionTask.java
+++ inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/scheduling/tasks/PredictionTask.java
@@ -20,8 +20,9 @@ package de.tudarmstadt.ukp.inception.recommendation.scheduling.tasks;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
-import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.Type;
 import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.uima.fit.util.CasUtil;
@@ -32,7 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
 import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.util.WebAnnoCasUtil;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationDocument;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationLayer;
 import de.tudarmstadt.ukp.clarin.webanno.model.Project;
@@ -44,7 +44,6 @@ import de.tudarmstadt.ukp.inception.recommendation.api.RecommendationService;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.AnnotationObject;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.LearningRecord;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.LearningRecordUserAction;
-import de.tudarmstadt.ukp.inception.recommendation.api.model.Offset;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.Predictions;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.Recommender;
 import de.tudarmstadt.ukp.inception.recommendation.imls.util.ListUtil;
@@ -112,10 +111,10 @@ public class PredictionTask
                 for (AnnotationDocument doc : docs) {
                     try {
                         JCas jcas = documentService.readAnnotationCas(doc);
-                        List<AnnotationObject> documentPredictions = filterPredictions(jcas, doc,
-                            layer, classifier);
-
-                        predictions.addAll(documentPredictions);
+                        List<List<AnnotationObject>> documentPredictions = setVisibility(
+                            learningRecordService, jcas, getUser().getUsername(), doc, layer,
+                            Predictions.getPredictions(classifier.predict(jcas, layer)));
+                        documentPredictions.forEach(predictions::addAll);
                     }
                     catch (IOException e) {
                         log.error("Cannot read annotation CAS.", e);
@@ -141,59 +140,87 @@ public class PredictionTask
         recommendationService.putIncomingPredictions(getUser(), getProject(), model);
     }
 
-    private List<AnnotationObject> filterPredictions(JCas aJcas, AnnotationDocument aDoc,
-        AnnotationLayer aLayer, Classifier aClassifier)
+    /**
+     * Goes through all AnnotationObjects and determines the visibility of each one
+     */
+    public static List<List<AnnotationObject>> setVisibility(
+        LearningRecordService aLearningRecordService, JCas aJcas, String aUser,
+        AnnotationDocument aDoc, AnnotationLayer aLayer,
+        List<List<AnnotationObject>> aRecommendations)
     {
-        List<List<AnnotationObject>> recommendations = Predictions.getPredictions(
-            aClassifier.predict(aJcas, aLayer));
+        List<LearningRecord> recordedAnnotations = aLearningRecordService
+            .getAllRecordsByDocumentAndUserAndLayer(aDoc.getDocument(), aUser, aLayer);
 
-        List<LearningRecord> recordedAnnotations = learningRecordService
-            .getAllRecordsByDocumentAndUserAndLayer(aDoc.getDocument(), getUser().getUsername(),
-                aLayer);
+        // Recommendations sorted by begin offset
+        NavigableSet<AnnotationObject> remainingRecommendations = new TreeSet<>(
+            ListUtil.flattenList(aRecommendations));
 
-        for (List<AnnotationObject> token : recommendations) {
-            for (AnnotationObject ao : token) {
-                boolean hasNoAnnotation = ao.getLabel() == null;
-                boolean isOverlappingForFeature = isOverlappingForFeature(ao.getOffset(),
-                    ao.getFeature(), aJcas, aLayer);
-                boolean isRejected = isRejected(recordedAnnotations, ao);
+        // Remove the predictions for which an annotation with the same feature already exists
+        // Also check whether it has an annotation and is not rejected
+        Type type = CasUtil.getType(aJcas.getCas(), aLayer.getName());
+        for (AnnotationFS fs : CasUtil.select(aJcas.getCas(), type)) {
+            if (!remainingRecommendations.isEmpty()) {
+                AnnotationObject ao;
 
-                if (hasNoAnnotation || isOverlappingForFeature || isRejected) {
-                    ao.setVisible(false);
+                // Go to the next token for which an annotation exists
+                do {
+                    ao = remainingRecommendations.pollFirst();
+                    setVisibility(recordedAnnotations, ao);
                 }
-                else {
-                    ao.setVisible(true);
+                while (ao.getOffset().getBeginCharacter() != fs.getBegin()
+                    && !remainingRecommendations.isEmpty());
+
+                boolean isOverlappingForFeature =
+                    fs.getBegin() == ao.getOffset().getBeginCharacter();
+                if (isOverlappingForFeature) {
+                    ao.setVisible(false);
+                } else {
+                    setVisibility(recordedAnnotations, ao);
                 }
             }
         }
 
-        return ListUtil.flattenList(recommendations);
+        // Check for the remaining AnnotationObjects whether they have an annotation
+        // and are not rejected
+        for (AnnotationObject ao: remainingRecommendations) {
+            setVisibility(recordedAnnotations, ao);
+        }
+
+        return aRecommendations;
     }
 
     /**
-     * Check if there is already an existing annotation overlapping the prediction
-     *
+     * Determines whether this recommendation has been rejected
      */
-    private boolean isOverlappingForFeature(Offset aOffset, String aFeatureName, JCas aJcas,
-        AnnotationLayer aLayer)
+    private static boolean isRejected(List<LearningRecord> aRecordedRecommendations,
+        AnnotationObject aAo)
     {
-        Type type = CasUtil.getType(aJcas.getCas(), aLayer.getName());
-        AnnotationFS annoFS = WebAnnoCasUtil.selectSingleFsAt(aJcas,
-            type, aOffset.getBeginCharacter(), aOffset.getEndCharacter());
-        Feature feature = type.getFeatureByBaseName(aFeatureName);
-        return annoFS.getFeatureValue(feature) != null;
-    }
-
-    private boolean isRejected(List<LearningRecord> recordedRecommendations, AnnotationObject ao)
-    {
-        for (LearningRecord record : recordedRecommendations) {
-            if (record.getOffsetCharacterBegin() == ao.getOffset().getBeginCharacter()
-                && record.getOffsetCharacterEnd() == ao.getOffset().getEndCharacter()
-                && record.getAnnotation().equals(ao.getLabel())
+        for (LearningRecord record : aRecordedRecommendations) {
+            if (record.getOffsetCharacterBegin() == aAo.getOffset().getBeginCharacter()
+                && record.getOffsetCharacterEnd() == aAo.getOffset().getEndCharacter()
+                && record.getAnnotation().equals(aAo.getLabel())
                 && record.getUserAction().equals(LearningRecordUserAction.REJECTED)) {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * Sets visibility of an AnnotationObject based on label and Learning Record
+     */
+    private static boolean setVisibility(List<LearningRecord> aRecordedRecommendations,
+        AnnotationObject aAo)
+    {
+        boolean hasNoAnnotation = aAo.getLabel() == null;
+        boolean isRejected = isRejected(aRecordedRecommendations, aAo);
+        if (hasNoAnnotation || isRejected) {
+            aAo.setVisible(false);
+            return false;
+        }
+        else {
+            aAo.setVisible(true);
+            return true;
+        }
     }
 }
