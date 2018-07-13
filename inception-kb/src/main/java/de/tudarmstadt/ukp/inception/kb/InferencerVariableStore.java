2dd1d34fd8b6d0e246a56174cf31c70f9d6fb7fa
kumar <nksaxena2507@gmail.com>
2018-07-13 13:35:36 +0200

    #322 Refractor SPARQL Queries
    - Requested changes

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/InferencerVariableStore.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/InferencerVariableStore.java
@@ -2,16 +2,9 @@ package de.tudarmstadt.ukp.inception.kb;
 
 public class InferencerVariableStore
 {
-    public static String LINE_SEPARATOR = "\n";
-    public static String OWL_PREFIX = "";
-    public static String RDF_PREFIX = "";
-    public static String RDFS_PREFIX = "";
-    public static String OBJECT_PROPERTY = "";
-    public static String DATATYPE_PROPERTY = "";
-    public static String ANNOTATION_PROPERTY = "";
-    
-    public static String PREFIX_RDFS = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>";
-    public static String PREFIX_RDF = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
-    public static String PREFIX_OWL = "PREFIX owl: <http://www.w3.org/2002/07/owl#>";
+    public static final String LINE_SEPARATOR = "\n";
+    public static final String PREFIX_RDFS = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>";
+    public static final String PREFIX_RDF = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
+    public static final String PREFIX_OWL = "PREFIX owl: <http://www.w3.org/2002/07/owl#>";
 
 }
