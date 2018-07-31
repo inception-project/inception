fb14fe832a1c0cf7de97a2b2e59fe548d122db9b
kumar <nksaxena2507@gmail.com>
2018-07-31 09:15:20 +0200

    #428 SPARQL query does not define required prefixe
    - Add prefixes to query from SPARQLQueryStore.java

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/SPARQLQueryStore.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/SPARQLQueryStore.java
@@ -17,17 +17,22 @@
  */
 package de.tudarmstadt.ukp.inception.kb;
 
-import org.eclipse.rdf4j.model.vocabulary.OWL;
-import org.eclipse.rdf4j.model.vocabulary.RDFS;
-
 public class SPARQLQueryStore
 {   
     public static String aLimit = "1000";
     
+    public static final String SPARQL_PREFIX = String.join("\n",
+            "PREFIX e:<http://www.wikidata.org/entity/>",
+            "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>",
+            "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+            "PREFIX owl:<http://www.w3.org/2002/07/owl#>",
+            "PREFIX skos:<http://www.w3.org/2004/02/skos/core#>",
+            "PREFIX base:<http://www.wikidata.org/ontology#>",
+            "PREFIX schema: <http://schema.org/>");
+    
     // Query to list properties from KnowledgeBase
     public static String PROPERTYLIST_QUERY = String.join("\n"
-            , "PREFIX rdfs: <" + RDFS.NAMESPACE + ">"
-            , "PREFIX owl: <" + OWL.NAMESPACE + ">"
+            , SPARQL_PREFIX
             , "SELECT DISTINCT ?s ?l WHERE {"
             , "  { ?s ?pTYPE ?oPROPERTY .}"
             , "  UNION "
