4a2045a34ef985c1758346045ce2973a55af3f10
Peter Jiang <peter.jiang@mailbox.org>
2018-08-27 15:06:27 +0200

    #398 - Recommender use CAS for input and output
    - Implemented NamedEntityLinker in new API
    - Deleted old classes

--- inception-concept-linking/src/main/java/de/tudarmstadt/ukp/inception/conceptlinking/recommender/NamedEntityLinkerFactory.java
+++ inception-concept-linking/src/main/java/de/tudarmstadt/ukp/inception/conceptlinking/recommender/NamedEntityLinkerFactory.java
@@ -22,36 +22,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
-import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.FeatureSupportRegistry;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationFeature;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationLayer;
 import de.tudarmstadt.ukp.inception.conceptlinking.service.ConceptLinkingService;
 import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
-import de.tudarmstadt.ukp.inception.recommendation.api.ClassificationTool;
-import de.tudarmstadt.ukp.inception.recommendation.api.ClassificationToolFactory;
+import de.tudarmstadt.ukp.inception.recommendation.api.model.Recommender;
+import de.tudarmstadt.ukp.inception.recommendation.api.v2.RecommendationEngine;
+import de.tudarmstadt.ukp.inception.recommendation.api.v2.RecommendationEngineFactory;
 
 @Component
-public class NamedEntityLinkerClassificationToolFactory
-    implements ClassificationToolFactory<Object, Void>
+public class NamedEntityLinkerFactory
+    implements RecommendationEngineFactory
 {
-    public static final String PREFIX = "kb:";
-
-    private @Autowired KnowledgeBaseService kbService;
-    private @Autowired ConceptLinkingService clService;
-    private @Autowired DocumentService docService;
-    private @Autowired AnnotationSchemaService annoService;
-    private @Autowired FeatureSupportRegistry fsRegistry;
-
     // This is a string literal so we can rename/refactor the class without it changing its ID
     // and without the database starting to refer to non-existing recommendation tools.
     public static final String ID = "de.tudarmstadt.ukp.inception.conceptlinking.recommender"
         + ".NamedEntityLinkerClassificationTool";
 
+    private static final String PREFIX = "kb:";
+
+    private @Autowired KnowledgeBaseService kbService;
+    private @Autowired ConceptLinkingService clService;
+    private @Autowired AnnotationSchemaService annoService;
+    private @Autowired FeatureSupportRegistry fsRegistry;
+
+
     @Override
     public String getId()
     {
         return ID;
+    }
+
+    @Override
+    public RecommendationEngine build(Recommender aRecommender)
+    {
+        return new NamedEntityLinker(aRecommender, kbService, clService, annoService, fsRegistry);
     }
 
     @Override
@@ -61,20 +67,12 @@ public class NamedEntityLinkerClassificationToolFactory
     }
 
     @Override
-    public ClassificationTool<Object> createTool(long aRecommenderId, String aFeature,
-        AnnotationLayer aLayer, int aMaxPredictions)
-    {
-        return new NamedEntityLinkerClassificationTool(aRecommenderId, aFeature, aLayer, kbService,
-            clService, docService, annoService, fsRegistry);
-    }
-
-    @Override
     public boolean accepts(AnnotationLayer aLayer, AnnotationFeature aFeature)
     {
         if (aLayer == null || aFeature == null) {
             return false;
         }
-        
+
         return (aLayer.isLockToTokenOffset() || aLayer.isMultipleTokens())
             && !aLayer.isCrossSentence() && "span".equals(aLayer.getType())
             && aFeature.getType().startsWith(PREFIX);
