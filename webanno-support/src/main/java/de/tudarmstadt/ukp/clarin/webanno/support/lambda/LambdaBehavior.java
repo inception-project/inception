7f26b02a744f1f95a32db3555b3dd0665fa0e03d
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-10-11 16:12:37 +0100

    #1066 - LambdaBehavior improvements
    - Added onRemove, onException and onComponentTag
    - Fixed serial IDs

--- webanno-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/lambda/LambdaBehavior.java
+++ webanno-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/lambda/LambdaBehavior.java
@@ -19,6 +19,7 @@ package de.tudarmstadt.ukp.clarin.webanno.support.lambda;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.ComponentTag;
 import org.danekja.java.util.function.serializable.SerializableBooleanSupplier;
 
 public class LambdaBehavior
@@ -37,11 +38,53 @@ public class LambdaBehavior
         };
     }
     
+    public static Behavior onRemove(SerializableConsumer<Component> aAction)
+    {
+        return new Behavior()
+        {
+            private static final long serialVersionUID = -8323963975999230350L;
+
+            @Override
+            public void onRemove(Component aComponent)
+            {
+                aAction.accept(aComponent);
+            }
+        };
+    }
+    
+    public static Behavior onException(SerializableBiConsumer<Component, RuntimeException> aAction)
+    {
+        return new Behavior()
+        {
+            private static final long serialVersionUID = 1927758103651261506L;
+
+            @Override
+            public void onException(Component aComponent, RuntimeException aException)
+            {
+                aAction.accept(aComponent, aException);
+            }
+        };
+    }
+    
+    public static Behavior onComponentTag(SerializableBiConsumer<Component, ComponentTag> aAction)
+    {
+        return new Behavior()
+        {
+            private static final long serialVersionUID = 1191220285070986757L;
+
+            @Override
+            public void onComponentTag(Component aComponent, ComponentTag aTag)
+            {
+                aAction.accept(aComponent, aTag);
+            }
+        };
+    }
+    
     public static Behavior visibleWhen(SerializableBooleanSupplier aPredicate)
     {
         return new Behavior()
         {
-            private static final long serialVersionUID = -4689671763746799691L;
+            private static final long serialVersionUID = -7550330528381560032L;
 
             @Override
             public void onConfigure(Component aComponent)
@@ -55,7 +98,7 @@ public class LambdaBehavior
     {
         return new Behavior()
         {
-            private static final long serialVersionUID = -4689671763746799691L;
+            private static final long serialVersionUID = -1435780281900876366L;
 
             @Override
             public void onConfigure(Component aComponent)
