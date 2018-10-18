18f15a1990c0409983eac1f034f18f2f49a8af0b
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-10-18 22:17:43 +0200

    #398 - Recommender use CAS for input and output
    - Fixed recommender sidebar
    - Added option to show all (even accepted / rejected) recommendations (mainly for debugging purposes)

--- inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/adapter/RecommendationSpanRenderer.java
+++ inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/adapter/RecommendationSpanRenderer.java
@@ -125,8 +125,8 @@ public class RecommendationSpanRenderer
                     vspansWithoutRecommendations, ao.getOffset(), windowBegin, ao.getFeature());
                 boolean isRejected = isRejected(recordedAnnotations, ao);
 
-                if (hasNoAnnotation || (!pref.isShowAllPredictions() && isOverlappingForFeature)
-                        || isRejected) {
+                if (hasNoAnnotation || (!pref.isShowAllPredictions()
+                        && (isOverlappingForFeature || isRejected))) {
                     continue;
                 }
 
