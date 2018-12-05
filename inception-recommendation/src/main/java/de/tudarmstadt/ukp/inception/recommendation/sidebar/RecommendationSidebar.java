e9222f06dc619da1dbc8e90e63cac766fa8886ec
Sibgha <sibgha360@gmail.com>
2018-12-05 22:48:22 +0100

    #649 - Learning curve diagram
    - Graph shows multiple recommenders
    
    - Changed to step graph
    
    - JS and CSS reference through webjars
    
    - latest scores are on the right of the graph
    
    - x-axis updated to a sequence of numbers from 0-50. 0 being the oldest evaluation

--- inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/sidebar/RecommendationSidebar.java
+++ inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/sidebar/RecommendationSidebar.java
@@ -22,7 +22,14 @@ import static de.tudarmstadt.ukp.clarin.webanno.support.JSONUtil.toJsonString;
 import static org.apache.commons.lang3.StringUtils.substring;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.IntStream;
 
 import org.apache.wicket.feedback.IFeedback;
 import org.apache.wicket.markup.head.CssHeaderItem;
@@ -40,6 +47,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wicketstuff.event.annotation.OnEvent;
 
+import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
+import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
 import de.tudarmstadt.ukp.clarin.webanno.api.JCasProvider;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.action.AnnotationActionHandler;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
@@ -53,127 +62,196 @@ import de.tudarmstadt.ukp.inception.log.model.LoggedEvent;
 import de.tudarmstadt.ukp.inception.recommendation.api.RecommendationService;
 import de.tudarmstadt.ukp.inception.recommendation.api.model.Preferences;
 import de.tudarmstadt.ukp.inception.recommendation.log.RecommenderEvaluationResultEventAdapter.Details;
-import de.tudarmstadt.ukp.inception.recommendation.sidebar.resource.C3CssReference;
-import de.tudarmstadt.ukp.inception.recommendation.sidebar.resource.C3JsReference;
-import de.tudarmstadt.ukp.inception.recommendation.sidebar.resource.D3JsReference;
 
-public class RecommendationSidebar
-    extends AnnotationSidebar_ImplBase
-{
-    private static final long serialVersionUID = 4306746527837380863L;
+public class RecommendationSidebar extends AnnotationSidebar_ImplBase {
+	private static final long serialVersionUID = 4306746527837380863L;
 
-    private Logger log = LoggerFactory.getLogger(getClass());
-    private WebComponent chartContainer;
+	private Logger log = LoggerFactory.getLogger(getClass());
+	private WebComponent chartContainer;
 
-    private @SpringBean RecommendationService recommendationService;
-    private @SpringBean EventRepository eventRepo;
+	private @SpringBean RecommendationService recommendationService;
+	private @SpringBean EventRepository eventRepo;
 
-    IModel<AnnotatorState> aModel;
+	IModel<AnnotatorState> aModel;
 
-    public RecommendationSidebar(String aId, IModel<AnnotatorState> aModel,
-            AnnotationActionHandler aActionHandler, JCasProvider aJCasProvider,
-            AnnotationPage aAnnotationPage)
-    {
-        super(aId, aModel, aActionHandler, aJCasProvider, aAnnotationPage);
-        this.aModel = aModel;
-        IModel<Preferences> model = LambdaModelAdapter.of(
-            () -> recommendationService.getPreferences(aModel.getObject().getUser(),
-                    aModel.getObject().getProject()),
-            (v) -> recommendationService.setPreferences(aModel.getObject().getUser(),
-                    aModel.getObject().getProject(), v));
+	int maxPointsToPlot = 50;
 
-        Form<Preferences> form = new Form<>("form", CompoundPropertyModel.of(model));
+	public RecommendationSidebar(String aId, IModel<AnnotatorState> aModel, AnnotationActionHandler aActionHandler,
+			JCasProvider aJCasProvider, AnnotationPage aAnnotationPage) {
+		super(aId, aModel, aActionHandler, aJCasProvider, aAnnotationPage);
+		this.aModel = aModel;
+		IModel<Preferences> model = LambdaModelAdapter.of(
+				() -> recommendationService.getPreferences(aModel.getObject().getUser(),
+						aModel.getObject().getProject()),
+				(v) -> recommendationService.setPreferences(aModel.getObject().getUser(),
+						aModel.getObject().getProject(), v));
 
-        form.add(new NumberTextField<Integer>("maxPredictions", Integer.class).setMinimum(1)
-                .setMaximum(10).setStep(1));
+		Form<Preferences> form = new Form<>("form", CompoundPropertyModel.of(model));
 
-        form.add(new CheckBox("showAllPredictions"));
+		form.add(new NumberTextField<Integer>("maxPredictions", Integer.class).setMinimum(1).setMaximum(10).setStep(1));
 
-        form.add(new LambdaAjaxButton<>("save", (_target, _form) -> 
-                aAnnotationPage.actionRefreshDocument(_target)));
+		form.add(new CheckBox("showAllPredictions"));
 
-        add(form);
+		form.add(new LambdaAjaxButton<>("save", (_target, _form) -> aAnnotationPage.actionRefreshDocument(_target)));
 
-        chartContainer = new Label("chart-container");
-        chartContainer.setOutputMarkupId(true);
-        add(chartContainer);
-    }
+		add(form);
 
-    @Override
-    public void renderHead(IHeaderResponse aResponse)
-    {
-        super.renderHead(aResponse);
+		chartContainer = new Label("chart-container");
+		chartContainer.setOutputMarkupId(true);
+		add(chartContainer);
+	}
 
-        // import Css
-        aResponse.render(JavaScriptHeaderItem.forReference(C3JsReference.get()));
-        aResponse.render(JavaScriptHeaderItem.forReference(D3JsReference.get()));
+	@Override
+	public void renderHead(IHeaderResponse aResponse) {
+		super.renderHead(aResponse);
 
-        // import Css
-        aResponse.render(CssHeaderItem.forReference(C3CssReference.get()));
-    }
+		// import Js
+		aResponse.render(JavaScriptHeaderItem.forReference(new WebjarsJavaScriptResourceReference("c3/current/c3.js")));
+		aResponse.render(
+				JavaScriptHeaderItem.forReference(new WebjarsJavaScriptResourceReference("d3js/current/d3.js")));
 
-    @OnEvent
-    public void onRenderAnnotations(RenderAnnotationsEvent aEvent)
-    {
-        try {
-            log.info("rendered annotation event");
-    
-            double[] listScores = getLatestScores(aEvent);
-            String data = substring(toJsonString(listScores), 1, -1);
-    
-            // bind data to chart container
-            String javascript = "var chart=c3.generate({bindto:'#" + chartContainer.getMarkupId()
-                    + "',data:{columns:[[\"Iteration\"," + data + "]]}});;";
-            log.info(javascript);
-    
-            aEvent.getRequestHandler().prependJavaScript(javascript);
-        }
-        catch (IOException e) {
-            log.error("Unable to render chart", e);
+		// import Css
+		aResponse.render(CssHeaderItem.forReference(new WebjarsCssResourceReference("c3/current/c3.css")));
+	}
 
-            error("Unable to render chart: " + e.getMessage());
-            
-            aEvent.getRequestHandler().addChildren(getPage(), IFeedback.class);
-        }
-    }
+	@OnEvent
+	public void onRenderAnnotations(RenderAnnotationsEvent aEvent) {
+		try {
+			log.info("rendered annotation event");
 
-    /**
-     * Fetches a number of latest evaluation scores from the database
-     */
-    private double[] getLatestScores(RenderAnnotationsEvent aEvent)
-    {
-        // we want to plot RecommenderEvaluationResultEvent for the learning curve. The value of the
-        // event
-        String eventType = "RecommenderEvaluationResultEvent";
-        int maximumRecordsToPlot = 50;
+			StringBuilder chartType = new StringBuilder();
+			
+			HashMap<String, List<Double>> recommenderScoreMap = getLatestScores(aEvent);
 
-        List<LoggedEvent> loggedEvents = eventRepo.listLoggedEvents(aModel.getObject().getProject(),
-                aModel.getObject().getUser().getUsername(), eventType, maximumRecordsToPlot);
+			// iterate over the recommender score map to create data arrays to feed to the
+			// c3 graph
+			StringBuilder dataColumns = new StringBuilder();
+			Iterator<Entry<String, List<Double>>> it = recommenderScoreMap.entrySet().iterator();
+			while (it.hasNext()) {
+				Entry<String, List<Double>> pair = it.next();
 
-        // iterate over the logged events to extract the scores.
-        double[] listDetailJson = new double[loggedEvents.size()];
+				String jsonString = toJsonString(pair);
+				jsonString = jsonString.replaceAll("\".*?\"", "");
+				jsonString = jsonString.replaceAll("'.*?'", "");
+				jsonString = jsonString.replaceAll("`.*?`", "");
 
-        int index = 0;
-        for (LoggedEvent loggedEvent : loggedEvents) {
-            String detailJson = loggedEvent.getDetails();
+				String data = substring(jsonString, 3, -2);
 
-            try {
-                Details detail = fromJsonString(Details.class, detailJson);
+				// append recommender name
+				dataColumns.append("['");
+				String[] recommenderClass = pair.getKey().toString().split("\\.");
+				String recommenderName = recommenderClass[recommenderClass.length - 1];
+				
+				//define chart type for the recommender
+				chartType.append(recommenderName);
+				chartType.append(": 'step', ");
+				
+				dataColumns.append(recommenderName);
 
-                listDetailJson[index] = detail.score;
-                index++;
+				// append data columns
+				dataColumns.append("', ");
+				dataColumns.append(data);
+				dataColumns.append("]");
+				dataColumns.append(",");
 
-            }
-            catch (IOException e) {
-                log.error("Invalid logged Event detail. Skipping record with logged event id: "
-                        + loggedEvent.getId(), e);
+				// avoids a ConcurrentModificationException
+				it.remove();
+			}
 
-                error("Invalid logged Event detail. Skipping record with logged event id: "
-                        + loggedEvent.getId());
-                
-                aEvent.getRequestHandler().addChildren(getPage(), IFeedback.class);
-            }
-        }
-        return listDetailJson;
-    }
+			String javascript = createJSScript(dataColumns.toString(), chartType.toString());
+			log.info(javascript);
+
+			aEvent.getRequestHandler().prependJavaScript(javascript);
+		} catch (IOException e) {
+			log.error("Unable to render chart", e);
+			error("Unable to render chart: " + e.getMessage());
+			aEvent.getRequestHandler().addChildren(getPage(), IFeedback.class);
+		}
+	}
+
+	/**
+	 * Creates the js script to render graph with the help of given data points. Sample value of dataColumns:
+	 * ['recommender1', 1.0, 2.0, 3.0 ], ['recommender2', 2.0, 3.0, 4.0]. Also creates an xaxix of a sequence
+	 * from 0 to maximumNumberOfPoints (50)
+	 * 
+	 * @param dataColumns
+	 * @param chartType 
+	 * @return
+	 * @throws IOException
+	 */
+	private String createJSScript(String dataColumns, String chartType) throws IOException {
+		int[] intArray = IntStream.range(0, maxPointsToPlot).map(i -> i).toArray();
+
+		String xaxisValues = "[ 'x' ," + substring(Arrays.toString(intArray), 1, -1) + "]";
+		String data = toJsonString(dataColumns).substring(1, dataColumns.toString().length());
+
+		
+		// bind data to chart container
+		String javascript = "var chart=c3.generate({bindto:'#" + chartContainer.getMarkupId()
+				+ "',data:{ x:'x', columns:[" + xaxisValues + " ," + data + "],types:{"+chartType +"}}});;";
+		return javascript;
+	}
+
+	/**
+	 * Fetches a number of latest evaluation scores from the database and save it in
+	 * the map corresponding to each recommender for which the scores have been
+	 * logged in the database
+	 */
+	private HashMap<String, List<Double>> getLatestScores(RenderAnnotationsEvent aEvent) {
+		// we want to plot RecommenderEvaluationResultEvent for the learning curve. The
+		// value of the event
+		String eventType = "RecommenderEvaluationResultEvent";
+
+		List<LoggedEvent> loggedEvents = eventRepo.listLoggedEvents(aModel.getObject().getProject(),
+				aModel.getObject().getUser().getUsername(), eventType, maxPointsToPlot);
+
+		//we want to show the latest record on the right side of the graph
+		Collections.reverse(loggedEvents);
+
+		HashMap<String, List<Double>> recommenderScoreMap = new HashMap<>();
+
+		// iterate over the logged events to extract the scores and map
+		// it against its corresponding recommender.
+		for (LoggedEvent loggedEvent : loggedEvents) {
+			String detailJson = loggedEvent.getDetails();
+			try {
+				Details detail = fromJsonString(Details.class, detailJson);
+				List<Double> list = recommenderScoreMap.get(detail.tool);
+				if (list==null) {
+					list = new ArrayList<Double>();
+
+					addScoreToMap(recommenderScoreMap, detail.score, detail.tool, list);
+					continue;
+				}
+
+				addScoreToMap(recommenderScoreMap, detail.score, detail.tool, list);
+			
+			} catch (IOException e) {
+				log.error("Invalid logged Event detail. Skipping record with logged event id: " + loggedEvent.getId(),
+						e);
+
+				error("Invalid logged Event detail. Skipping record with logged event id: " + loggedEvent.getId());
+
+				aEvent.getRequestHandler().addChildren(getPage(), IFeedback.class);
+			}
+		}
+		return recommenderScoreMap;
+	}
+
+	/**
+	 * adds the score to the given map, if the value of the score is valid/finite
+	 * @param recommenderScoreMap
+	 * @param score
+	 * @param tool
+	 * @param list
+	 */
+	private void addScoreToMap(HashMap<String, List<Double>> recommenderScoreMap, Double score, String tool, List<Double> list) {
+		//sometimes score values NaN. Can result into error while rendering the graph on UI
+		if (!Double.isFinite(score)) {
+			return;
+		}
+
+		list.add(score);
+		recommenderScoreMap.put(tool, list);
+	}
 }
