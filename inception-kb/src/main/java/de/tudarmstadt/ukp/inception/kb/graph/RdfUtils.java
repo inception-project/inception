b4a023251e2b7ce37380666085dcbd5fa4bb190d
Marcel de Boer <marceldeboer@web.de>
2018-12-30 14:34:31 +0100

    #299 - support sub property labels
    - remove unused methods in RdfUtils

--- inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/graph/RdfUtils.java
+++ inception-kb/src/main/java/de/tudarmstadt/ukp/inception/kb/graph/RdfUtils.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 import org.eclipse.rdf4j.common.iteration.ConvertingIteration;
 import org.eclipse.rdf4j.common.iteration.ExceptionConvertingIteration;
 import org.eclipse.rdf4j.common.iteration.Iteration;
-import org.eclipse.rdf4j.common.iteration.Iterations;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.Statement;
@@ -39,22 +38,9 @@ import org.eclipse.rdf4j.repository.RepositoryResult;
 import org.eclipse.rdf4j.rio.ntriples.NTriplesUtil;
 
 import de.tudarmstadt.ukp.inception.kb.IriConstants;
-import de.tudarmstadt.ukp.inception.kb.SPARQLQueryStore;
 
 public class RdfUtils
 {
-    public static Optional<Value> readFirstValue(RepositoryConnection conn, Resource subj, IRI pred,
-            Resource... contexts)
-    {
-        Optional<Statement> statement = readFirst(conn, subj, pred, null);
-        if (statement.isPresent()) {
-            return Optional.of(statement.get().getObject());
-        }
-        else {
-            return Optional.empty();
-        }
-    }
-
     public static Optional<Statement> readFirst(RepositoryConnection conn, Resource subj, IRI pred,
             Value obj, String language)
     {
@@ -76,21 +62,9 @@ public class RdfUtils
     }
 
     public static RepositoryResult<Statement> getStatements(RepositoryConnection conn,
-        Resource subj, IRI pred, Value obj, boolean includeInferred, int aLimit)
-    {
-        return getStatementsSparql(conn, subj, pred, obj, aLimit, includeInferred, null);
-    }
-
-    public static RepositoryResult<Statement> getStatements(RepositoryConnection conn,
         Resource subj, IRI pred, Value obj, boolean includeInferred, String language)
     {
         return getStatementsSparql(conn, subj, pred, obj, 1000, includeInferred, language);
-    }
-
-    public static RepositoryResult<Statement> getStatementsNormal(RepositoryConnection conn,
-            Resource subj, IRI pred, Value obj, boolean includeInferred)
-            throws QueryEvaluationException {
-        return conn.getStatements(subj, pred, obj, includeInferred);
     }
 
     public static RepositoryResult<Statement> getStatementsSparql(RepositoryConnection conn,
@@ -122,7 +96,7 @@ public class RdfUtils
         tupleQuery.setIncludeInferred(includeInferred);
         TupleQueryResult result = tupleQuery.evaluate();
         Iteration<Statement, QueryEvaluationException> i1 = 
-                new ConvertingIteration<BindingSet, Statement, QueryEvaluationException>(result)
+                new ConvertingIteration<>(result)
         {
             @Override
             protected Statement convert(BindingSet b) throws QueryEvaluationException
@@ -145,96 +119,7 @@ public class RdfUtils
             }
         };
         
-        return new RepositoryResult<Statement>(i2);
-    }
-    
-    public static RepositoryResult<Statement> getPropertyStatementsSparql(RepositoryConnection conn,
-            Resource subj, IRI pred, Value obj, int aLimit, boolean includeInferred,
-            String language)
-        throws QueryEvaluationException
-    {
-        
-        String filter = "";
-        if (language != null) {
-            filter = "FILTER(LANG(?o) = \"\" || LANGMATCHES(LANG(?o), \"" + NTriplesUtil
-                .escapeString(language) + "\")).";
-        }
-        String QUERY = String.join("\n",
-                SPARQLQueryStore.SPARQL_PREFIX,
-                "SELECT * WHERE { ",
-                " {?s ?p ?o .}",
-                " UNION ",
-                " {?s a ?prop .",
-                "    VALUES ?prop { rdf:Property owl:ObjectProperty owl:DatatypeProperty owl:AnnotationProperty} }",
-                filter,
-                "} LIMIT " + aLimit);
-
-        TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, QUERY);
-        if (subj != null) {
-            tupleQuery.setBinding("s", subj);
-        }
-        if (pred != null) {
-            tupleQuery.setBinding("p", pred);
-        }
-        if (obj != null) {
-            tupleQuery.setBinding("o", obj);
-        }
-        tupleQuery.setIncludeInferred(includeInferred);
-        TupleQueryResult result = tupleQuery.evaluate();
-        Iteration<Statement, QueryEvaluationException> i1 = 
-                new ConvertingIteration<BindingSet, Statement, QueryEvaluationException>(result)
-        {
-            @Override
-            protected Statement convert(BindingSet b) throws QueryEvaluationException
-            {
-                Resource s = subj == null ? (Resource) b.getValue("s") : subj;
-                IRI p = pred == null ? (IRI) b.getValue("p") : pred;
-                Value o = obj == null ? b.getValue("o") : obj;
-
-                return SimpleValueFactory.getInstance().createStatement(s, p, o);
-            }
-        };
-        
-        ExceptionConvertingIteration<Statement, RepositoryException> i2 = 
-                new ExceptionConvertingIteration<Statement, RepositoryException>(i1)
-        {
-            @Override
-            protected RepositoryException convert(Exception aE)
-            {
-                return new RepositoryException(aE);
-            }
-        };
-        
-        return new RepositoryResult<Statement>(i2);
-    }
-    
-    
-    
-    public static boolean existsStatementsWithSubject(
-            RepositoryConnection conn, Resource subj, boolean includeInferred)
-    {
-        try (RepositoryResult<Statement> stmts = getStatementsSparql(conn, subj, null, null, 1,
-                includeInferred, null)) {
-            return !Iterations.asList(stmts).isEmpty();
-        }
-    }
-    
-
-    /**
-     * Get all statements about the given subject.
-     * 
-     * @param conn
-     *            a repository connection
-     * @param subj
-     *            the subject resource
-     * @param includeInferred
-     *            whether to include inferred statements
-     * @return all statements with the given subject resource
-     */
-    public static RepositoryResult<Statement> getStatementsWithSubject(
-            RepositoryConnection conn, Resource subj, boolean includeInferred, int aLimit)
-    {
-        return getStatementsSparql(conn, subj, null, null, aLimit, includeInferred, null);
+        return new RepositoryResult<>(i2);
     }
     
     public static boolean isFromImplicitNamespace(KBHandle handle) {
