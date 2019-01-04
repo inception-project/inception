746f989dbd69c5a2d9ed0cd767d6224eca9e8d82
Marcel de Boer <marceldeboer@web.de>
2019-01-04 14:42:10 +0100

    #822 - KB resources show as URI not as label in statements
    - read KB resource to determine type and uiLabel
    - send proper selection event for the type of the resource

--- inception-ui-kb/src/main/java/de/tudarmstadt/ukp/inception/ui/kb/value/editor/IRIValuePresenter.java
+++ inception-ui-kb/src/main/java/de/tudarmstadt/ukp/inception/ui/kb/value/editor/IRIValuePresenter.java
@@ -17,44 +17,95 @@
  */
 package de.tudarmstadt.ukp.inception.ui.kb.value.editor;
 
+import java.util.Optional;
+
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.eclipse.rdf4j.model.IRI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaAjaxLink;
-import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaModel;
+import de.tudarmstadt.ukp.inception.kb.KnowledgeBaseService;
+import de.tudarmstadt.ukp.inception.kb.graph.KBConcept;
 import de.tudarmstadt.ukp.inception.kb.graph.KBHandle;
+import de.tudarmstadt.ukp.inception.kb.graph.KBInstance;
+import de.tudarmstadt.ukp.inception.kb.graph.KBObject;
 import de.tudarmstadt.ukp.inception.kb.graph.KBStatement;
+import de.tudarmstadt.ukp.inception.kb.model.KnowledgeBase;
 import de.tudarmstadt.ukp.inception.ui.kb.event.AjaxConceptSelectionEvent;
+import de.tudarmstadt.ukp.inception.ui.kb.event.AjaxInstanceSelectionEvent;
+import de.tudarmstadt.ukp.inception.ui.kb.event.AjaxPropertySelectionEvent;
+
 
 public class IRIValuePresenter
     extends ValuePresenter
 {
     private static final long serialVersionUID = -2127902473859929221L;
 
-    public IRIValuePresenter(String id, IModel<KBStatement> aModel)
+    private static final Logger LOG = LoggerFactory.getLogger(IRIValuePresenter.class);
+
+    @SpringBean KnowledgeBaseService kbService;
+
+    private IModel<KnowledgeBase> kbModel;
+
+    public IRIValuePresenter(String id, IModel<KBStatement> aModel, IModel<KnowledgeBase> aKBModel)
     {
         super(id, aModel);
+        kbModel = aKBModel;
 
-        LambdaAjaxLink link = new LambdaAjaxLink("link", this::actionIRILinkClicked);
-        link.add(new Label("label", LambdaModel.of(this::getLabel)));
+        LambdaAjaxLink link = new LambdaAjaxLink("link",
+            t -> actionIRILinkClicked(t, getKBObject()));
+        link.add(new Label("label", Model.of(getLabel(getKBObject()))));
         add(link);
     }
 
-    private String getLabel()
-    {
-        KBStatement stmt = getModelObject();
-        return ((IRI) stmt.getValue()).getLocalName();
+    Optional<KBObject> getKBObject() {
+        Object stmtValue = getModelObject().getValue();
+        Optional<KBObject> kbObject = kbService
+            .readKBIdentifier(kbModel.getObject(), stmtValue.toString());
+        if (kbObject.isPresent()) {
+            return kbObject;
+        }
+        return Optional.empty();
     }
 
-    private void actionIRILinkClicked(AjaxRequestTarget target)
+    private String getLabel(Optional<KBObject> aKbObject)
     {
-        // TODO need to know what the IRI refers to - concept, property, both???
-        KBStatement stmt = getModelObject();
-        KBHandle selectedConcept = new KBHandle(((IRI) stmt.getValue()).toString());
-        send(getPage(), Broadcast.BREADTH,
-                new AjaxConceptSelectionEvent(target, selectedConcept, true));
+        if (aKbObject.isPresent()) {
+            return aKbObject.get().getUiLabel();
+        }
+        return ((IRI) getModelObject().getValue()).getLocalName();
+    }
+
+    private void actionIRILinkClicked(AjaxRequestTarget aTarget, Optional<KBObject> aKbObject)
+    {
+        if (aKbObject.isPresent()) {
+            KBObject kbObject = aKbObject.get();
+            if (kbObject instanceof KBConcept) {
+                send(getPage(), Broadcast.BREADTH,
+                    new AjaxConceptSelectionEvent(aTarget, KBHandle.of(kbObject), true));
+            }
+            else if (kbObject instanceof KBInstance) {
+                send(getPage(), Broadcast.BREADTH,
+                    new AjaxInstanceSelectionEvent(aTarget, KBHandle.of(kbObject)));
+            }
+            else {
+                send(getPage(), Broadcast.BREADTH,
+                    new AjaxPropertySelectionEvent(aTarget, KBHandle.of(kbObject), true));
+            }
+        }
+        else {
+            // send concept selection changed event to display the resource in concept info panel
+            KBStatement stmt = getModelObject();
+            KBHandle selectedConcept = new KBHandle(((IRI) stmt.getValue()).toString());
+            send(getPage(), Broadcast.BREADTH,
+                new AjaxConceptSelectionEvent(aTarget, selectedConcept, true));
+        }
+
     }
 }
