19b51f0ca12ba48dd420bd8730b936deac499b17
Wulf Pfeiffer <wp@wpmp.me>
2019-01-12 16:00:36 +0100

    #803 - Rendering of multi range annotations
    - added unmatched spans to PdfAnnoModel
    - add user feedback for unmatched span annotations
    - refactor convertToPdfAnnoSpan method to improve readability

--- inception-pdf-editor/src/main/java/de/tudarmstadt/ukp/inception/pdfeditor/PdfAnnotationEditor.java
+++ inception-pdf-editor/src/main/java/de/tudarmstadt/ukp/inception/pdfeditor/PdfAnnotationEditor.java
@@ -18,6 +18,7 @@
 package de.tudarmstadt.ukp.inception.pdfeditor;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.uima.jcas.JCas;
@@ -108,6 +109,10 @@ public class PdfAnnotationEditor
             VDocument vdoc = render(jCas, 0, jCas.getDocumentText().length());
             PdfAnnoModel pdfAnnoModel = PdfAnnoRenderer.render(getModelObject(),
                 vdoc, jCas.getDocumentText(), annotationService, pdfExtractFile);
+            // show unmatched spans to user
+            String vIds = pdfAnnoModel.getUnmatchedSpans().stream()
+                .map(vId -> "" + vId).collect(Collectors.joining(", "));
+            error("Could not find a match for following annotation ids: " + vIds);
             return pdfAnnoModel;
         }
         return null;
