bbb2b69430ba4ab20c8981b564c020ccca9bfbae
Wulf Pfeiffer <wp@wpmp.me>
2019-02-05 18:18:58 +0100

    #839 - Creation of relations in PDF editor
    - add relation creation by drag and drop in the frontend
    - move creation of spans into an if clause (mouesdown)

--- inception-pdf-editor/src/main/js/pdfanno/src/core/src/UI/relation.js
+++ inception-pdf-editor/src/main/js/pdfanno/src/core/src/UI/relation.js
@@ -1,5 +1,8 @@
 import * as textInput from '../utils/textInput'
 import RelationAnnotation from '../annotation/relation'
+// BEGIN INCEpTION EXTENSION - #839 - Creation of rleations in PDF editor
+import { scaleDown } from './utils'
+// END INCEpTION EXTENSION
 
 /**
  * Create a new Relation annotation.
@@ -38,3 +41,67 @@ export function createRelation ({ type, anno1, anno2, text, color }) {
 
   return annotation
 }
+
+// BEGIN INCEpTION EXTENSION - #839 - Creation of rleations in PDF editor
+window.addEventListener('DOMContentLoaded', () => {
+
+  const $viewer = $('#viewer')
+
+  window.addEventListener('annotationHoverIn', (e) => {
+    hoveredAnnotation = e.detail
+  })
+
+  window.addEventListener('annotationHoverOut', () => {
+    hoveredAnnotation = null
+  })
+
+  $viewer.on('mousedown', '.anno-knob', () => {
+    mousedownFired = true
+    if (hoveredAnnotation) {
+      drawing = true
+      relationAnnotation = new RelationAnnotation()
+      relationAnnotation.rel1Annotation = hoveredAnnotation
+      relationAnnotation.direction = 'relation'
+      relationAnnotation.readOnly = false
+      relationAnnotation.setDisableHoverEvent()
+    }
+  })
+
+  $viewer.on('mousemove', (e) => {
+    if (mousedownFired && drawing) {
+      let p = scaleDown(getClientXY(e))
+      relationAnnotation.x2 = p.x
+      relationAnnotation.y2 = p.y
+      relationAnnotation.render()
+    }
+  })
+
+  $viewer.on('mouseup', () => {
+    if (mousedownFired && drawing) {
+      if (hoveredAnnotation && hoveredAnnotation !== relationAnnotation.rel1Annotation) {
+        console.log('create relation annotation here')
+      }
+      relationAnnotation.rel1Annotation.deselect()
+      relationAnnotation.destroy()
+      relationAnnotation = null
+      annoPage.getAllAnnotations().forEach(function(annotation) {
+        annotation.render()
+      })
+    }
+    drawing = false
+    mousedownFired = false
+  })
+})
+
+function getClientXY(e) {
+  let rect = document.getElementById('annoLayer').getBoundingClientRect()
+  let x = e.clientX - rect.left
+  let y = e.clientY - rect.top
+  return {x, y}
+}
+
+let relationAnnotation = null
+let hoveredAnnotation = null
+let mousedownFired = false
+let drawing = false
+// END INCEpTION EXTENSION
