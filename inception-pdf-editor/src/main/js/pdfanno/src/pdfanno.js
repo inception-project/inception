4c713474e381f097e2b7697089676f3054b1b65e
Richard Eckart de Castilho <richard.eckart@gmail.com>
2019-04-23 19:50:17 +0200

    #1089 - PDF Editor Viewport undefined
    - Slightly adjust comment format

--- inception-pdf-editor/src/main/js/pdfanno/src/pdfanno.js
+++ inception-pdf-editor/src/main/js/pdfanno/src/pdfanno.js
@@ -215,8 +215,10 @@ async function displayViewer () {
     const renderTimeout = 500
     window.pagechangeEventCounter = 0
     window.pageRender = 1;
-
 // BEGIN INCEpTION EXTENSION - #1089 - PDF Editor Viewport undefined
+/*
+    getAnnotations()
+*/
     let initAnnotations = function(e) {
       getAnnotations()
       document.removeEventListener('pagerendered', initAnnotations)
