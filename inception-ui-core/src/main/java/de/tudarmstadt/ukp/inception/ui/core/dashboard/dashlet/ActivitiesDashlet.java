c3752c4feed335fce69885cb32742a3bbc1ccda9
uwinch <winchenbach@ukp.informatik.tu-darmstadt.de>
2019-04-30 11:04:09 +0200

    #804 Easy way to resume annotation
    - loading last annotated page

--- inception-ui-core/src/main/java/de/tudarmstadt/ukp/inception/ui/core/dashboard/dashlet/ActivitiesDashlet.java
+++ inception-ui-core/src/main/java/de/tudarmstadt/ukp/inception/ui/core/dashboard/dashlet/ActivitiesDashlet.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright 2019
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.tudarmstadt.ukp.inception.ui.core.dashboard.dashlet;
+
+import static de.tudarmstadt.ukp.clarin.webanno.api.WebAnnoConst.PAGE_PARAM_DOCUMENT_ID;
+import static de.tudarmstadt.ukp.clarin.webanno.api.WebAnnoConst.PAGE_PARAM_PROJECT_ID;
 
 import java.util.List;
 
@@ -9,12 +29,15 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.StringResourceModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
 import de.tudarmstadt.ukp.clarin.webanno.model.Project;
 import de.tudarmstadt.ukp.clarin.webanno.security.UserDao;
 import de.tudarmstadt.ukp.clarin.webanno.security.model.User;
 import de.tudarmstadt.ukp.clarin.webanno.support.lambda.LambdaStatelessLink;
+import de.tudarmstadt.ukp.clarin.webanno.ui.annotation.AnnotationPage;
 import de.tudarmstadt.ukp.inception.log.EventRepository;
 import de.tudarmstadt.ukp.inception.log.model.LoggedEvent;
 
@@ -25,6 +48,7 @@ public class ActivitiesDashlet extends Dashlet_ImplBase
     
     private @SpringBean EventRepository eventRepository;
     private @SpringBean UserDao userRepository;
+    private @SpringBean DocumentService documentService;
     
     private final IModel<Project> projectModel;
     
@@ -45,7 +69,7 @@ public class ActivitiesDashlet extends Dashlet_ImplBase
             {
                 LambdaStatelessLink eventLink = new LambdaStatelessLink("eventLink",
                     () -> openDocument(aItem.getModelObject()));
-                eventLink.add(new Label("eventName", aItem.getModelObject().getEvent()));
+                eventLink.add(new Label("eventName", getEventDescription(aItem)));
                 aItem.add(eventLink);
             }
         };
@@ -53,10 +77,19 @@ public class ActivitiesDashlet extends Dashlet_ImplBase
         activitiesList.add(listview);
     }
     
-    private void openDocument(LoggedEvent aModelObject)
+    private String getEventDescription(ListItem<LoggedEvent> aItem) {
+        //return aItem.getModelObject().getEvent()
+        String documentName = documentService.getSourceDocument(projectModel.getObject().getId(), 
+                aItem.getModelObject().getDocument()).getName();
+        return String.format("Annotated in document %s", documentName);
+    }
+    
+    private void openDocument(LoggedEvent aEvent)
     {
-        // TODO Auto-generated method stub
-        System.out.println("Open document");
+        PageParameters params = new PageParameters();
+        params.add(PAGE_PARAM_PROJECT_ID, projectModel.getObject().getId());
+        params.add(PAGE_PARAM_DOCUMENT_ID, aEvent.getDocument());
+        setResponsePage(AnnotationPage.class, params);
     }
 
     private List<LoggedEvent> listActivities()
@@ -68,5 +101,7 @@ public class ActivitiesDashlet extends Dashlet_ImplBase
                 user.getUsername(), annotationEvent, 1);
         return events;
     }
+    
+
 
 }
