1441d03530ffb01343f3584522bf431927f2a2c0
uwinch <winchenbach@ukp.informatik.tu-darmstadt.de>
2019-05-27 16:57:04 +0200

    #1132 Unify training data
    - deal with cases of only one veal data point

--- inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/chart/resources/chart.js
+++ inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/chart/resources/chart.js
@@ -23,32 +23,41 @@
  */
 function updateLearningCurveDiagram(arrayOfLearningCurves) {
 	
-    var xAxixType = "";
+    var xAxixType = 'indexed';
+    var plotType = 'step';
+    var xTick = {
+    		rotate : 0,
+    		multiline : true,
+    		format : function(a) {
+    			return Math.round(1e2 * a) / 1e2;
+    		}
+    };
     
 	//make the type of x-axis "category" when we have more than one learning curves. i.e when the request is from annotation recommender side bar. It is for better visualization when the x-axis represents test data size
     if ((arrayOfLearningCurves[0][2] - arrayOfLearningCurves[0][1]) > 1) 
     	xAxixType = "category";
     
-    //draw the chart with the help of the arrayOfLearningCurves. The type of the graph is "step".
+
+    // if we just have one value, we cannot visualize a step
+	if (arrayOfLearningCurves[0] && arrayOfLearningCurves[0].length < 3) {
+		plotType = 'scatter';
+		xTick["values"] = [ 0.0, 1.0 ];
+	}
+    
+    // draw the chart with the help of the arrayOfLearningCurves. The type of
+	// the graph is "step".
     var e = c3.generate({
         bindto: "#canvas",
         data: {
             x: "x",
             columns: arrayOfLearningCurves,
-            type: "step",
+            type: plotType,
             empty:{label:{text:"No Data Available"}}
         },
         axis: {
             x: {
                 type: xAxixType,
-                tick: {
-                    rotate: 0,
-                    multiline: true,
-                    format: function(a) {
-                        return Math.round(1e2 * a) / 1e2;
-                    },
-                    fit: false
-                }
+                tick : xTick
             },
             //to round off the decimal points of the y-axis values to 4 if it is a decimal number.
             y: {
