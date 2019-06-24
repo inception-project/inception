4887585b4416da59fe428ba6e8f78e31756ced13
Michael Unterkalmsteiner <mun@bth.se>
2019-06-24 18:28:30 +0200

    Update test

--- inception-recommendation/src/test/java/de/tudarmstadt/ukp/inception/recommendation/service/RecommendationServiceImplIntegrationTest.java
+++ inception-recommendation/src/test/java/de/tudarmstadt/ukp/inception/recommendation/service/RecommendationServiceImplIntegrationTest.java
@@ -19,6 +19,7 @@
 package de.tudarmstadt.ukp.inception.recommendation.service;
 
 import static de.tudarmstadt.ukp.inception.recommendation.api.RecommendationService.FEATURE_NAME_IS_PREDICTION;
+import static de.tudarmstadt.ukp.inception.recommendation.api.RecommendationService.FEATURE_NAME_SCORE_EXPLANATION_SUFFIX;
 import static de.tudarmstadt.ukp.inception.recommendation.api.RecommendationService.FEATURE_NAME_SCORE_SUFFIX;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -182,6 +183,7 @@ public class RecommendationServiceImplIntegrationTest
         assertThat(type.getFeatures())
                 .extracting(Feature::getShortName)
                 .contains(feature.getName() + FEATURE_NAME_SCORE_SUFFIX)
+                .contains(feature.getName() + FEATURE_NAME_SCORE_EXPLANATION_SUFFIX)
                 .contains(FEATURE_NAME_IS_PREDICTION);
     }
 
