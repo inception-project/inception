767c6e358ba11129072f926de832dcae01b019bc
uwinch <winchenbach@ukp.informatik.tu-darmstadt.de>
2019-06-25 13:19:53 +0200

    #1260 Adapt DocumentOpenedEventAdapter to Webanno Change
    - added details, annotator to event adapter

--- inception-log/src/main/java/de/tudarmstadt/ukp/inception/log/adapter/DocumentOpenedEventAdapter.java
+++ inception-log/src/main/java/de/tudarmstadt/ukp/inception/log/adapter/DocumentOpenedEventAdapter.java
@@ -17,14 +17,22 @@
  */
 package de.tudarmstadt.ukp.inception.log.adapter;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.event.DocumentOpenedEvent;
+import de.tudarmstadt.ukp.clarin.webanno.model.SourceDocument;
+import de.tudarmstadt.ukp.clarin.webanno.support.JSONUtil;
 
 @Component
 public class DocumentOpenedEventAdapter
     implements EventLoggingAdapter<DocumentOpenedEvent>
 {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
     @Override
     public boolean accepts(Object aEvent)
     {
@@ -41,5 +49,60 @@ public class DocumentOpenedEventAdapter
     public long getProject(DocumentOpenedEvent aEvent)
     {
         return aEvent.getDocument().getProject().getId();
+    }
+
+    @Override
+    public String getDetails(DocumentOpenedEvent aEvent)
+    {
+        DocumentOpenedDetails details = new DocumentOpenedDetails(aEvent.getAnnotator(),
+                aEvent.getUser(), aEvent.getDocument());
+        try {
+            String json = JSONUtil.toJsonString(details); 
+            return json;
+        }
+        catch (IOException e) {
+            log.error("Unable to log event [{}]", aEvent, e);
+            return "<ERROR>";
+        }
+    }
+
+    @Override
+    public String getUser(DocumentOpenedEvent aEvent)
+    {
+        return aEvent.getUser();
+    }
+
+    @Override
+    public String getAnnotator(DocumentOpenedEvent aEvent)
+    {
+        return aEvent.getAnnotator();
+    }
+
+    private class DocumentOpenedDetails
+    {
+        private final String annotator;
+        private final String opener;
+        private final String docName;
+        
+        public DocumentOpenedDetails(String aAnnotator, String aOpener, SourceDocument aDoc) {
+            annotator = aAnnotator;
+            opener = aOpener;
+            docName = aDoc.getName();
+        }
+
+        public String getAnnotator()
+        {
+            return annotator;
+        }
+
+        public String getOpener()
+        {
+            return opener;
+        }
+
+        public String getDocName()
+        {
+            return docName;
+        }
     }
 }
