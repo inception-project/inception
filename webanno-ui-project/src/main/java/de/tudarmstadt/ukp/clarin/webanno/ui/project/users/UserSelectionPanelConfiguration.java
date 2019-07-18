a7ae29ffe76c823a88324a2ce134a5537753ca1f
DimitrisGk-iel <dgkoumas@ilsp.gr>
2019-07-18 03:34:29 +0300

    Create UserSelectionPanelConfiguration.java

---
+++ webanno-ui-project/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/project/users/UserSelectionPanelConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.clarin.webanno.ui.project.users;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = UserSelectionPanelConfiguration.PROPERTY_PREFIX)
+public class UserSelectionPanelConfiguration {
+    
+    public static final String PROPERTY_PREFIX = "userSelection";
+    
+    private boolean hideUsers;
+    
+    private int minLengthCharacters;
+
+    public boolean isHideUsers() {
+        return hideUsers;
+    }
+
+    public void setHideUsers(boolean hideUsers) {
+        this.hideUsers = hideUsers;
+    }
+
+    public int getUsersMinLengthCharacters() {
+        return minLengthCharacters;
+    }
+
+    public void setMinLengthCharacters(int minLengthCharacters) {
+        this.minLengthCharacters = minLengthCharacters;
+    }
+}
