cc31ef03e4aed6c5cfd7db66e1e57f1e6733b467
Wulf Pfeiffer <wp@wpmp.me>
2019-09-28 20:44:22 +0200

    #1218 - Structured Review Draft
    - rearrange features of span annotations in the review editor

--- inception-review-editor/src/main/java/de/tudarmstadt/ukp/inception/revieweditor/SpanAnnotationPanel.java
+++ inception-review-editor/src/main/java/de/tudarmstadt/ukp/inception/revieweditor/SpanAnnotationPanel.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FeatureStructure;
@@ -64,6 +65,7 @@ public class SpanAnnotationPanel
     
     private static final String CID_TEXT = "text";
     private static final String CID_FEATURES_CONTAINER = "featuresContainer";
+    private static final String CID_TEXT_FEATURES = "textFeatures";
     private static final String CID_FEATURES = "features";
     private static final String CID_FEATURE = "feature";
     
@@ -89,9 +91,15 @@ public class SpanAnnotationPanel
             AnnotationLayer layer = annotationService.findLayer(project, fs);
             
             List<FeatureState> features = listFeatures(fs, layer, vid);
+            List<FeatureState> textFeatures = features.stream()
+                .filter(state -> state.feature.getType().equals("uima.cas.String") 
+                    && state.feature.getTagset() == null)
+                .collect(Collectors.toList());
+            features.removeAll(textFeatures);
     
             featuresContainer = new WebMarkupContainer(CID_FEATURES_CONTAINER);
             featuresContainer.setOutputMarkupId(true);
+            featuresContainer.add(createTextFeaturesList(textFeatures));
             featuresContainer.add(createFeaturesList(features));
             // TODO: there are probably better ways to get the text of an annotation?
             featuresContainer.add(new Label(CID_TEXT, link.label));
@@ -129,9 +137,9 @@ public class SpanAnnotationPanel
         return featureStates;
     }
     
-    private ListView<FeatureState> createFeaturesList(List<FeatureState> features)
+    private ListView<FeatureState> createTextFeaturesList(List<FeatureState> features)
     {
-        return new ListView<FeatureState>(CID_FEATURES, features)
+        return new ListView<FeatureState>(CID_TEXT_FEATURES, features)
         {
             private static final long serialVersionUID = -1139622234318691941L;
             
@@ -188,6 +196,70 @@ public class SpanAnnotationPanel
                 editor.getFocusComponent().setMarkupId(
                     "" + editor.getModelObject().feature.getId());
                 
+                item.add(editor);
+            }
+        };
+    }
+    
+    private ListView<FeatureState> createFeaturesList(List<FeatureState> features)
+    {
+        return new ListView<FeatureState>(CID_FEATURES, features)
+        {
+            private static final long serialVersionUID = 16641722427333232L;
+            
+            @Override
+            protected void populateItem(ListItem<FeatureState> item)
+            {
+                // Feature editors that allow multiple values may want to update themselves,
+                // e.g. to add another slot.
+                item.setOutputMarkupId(true);
+    
+                final FeatureState featureState = item.getModelObject();
+                final FeatureEditor editor;
+                FeatureSupport<Object> featureSupport = featureSupportRegistry
+                    .getFeatureSupport(featureState.feature);
+                editor = featureSupport.createEditor(CID_FEATURE,
+                    SpanAnnotationPanel.this, null, null, item.getModel());
+                editor.setEnabled(false);
+    
+                if (!featureState.feature.getLayer().isReadonly()) {
+                    // Whenever it is updating an annotation, it updates automatically when a
+                    // component for the feature lost focus - but updating is for every component
+                    // edited LinkFeatureEditors must be excluded because the auto-update will break
+                    // the ability to add slots. Adding a slot is NOT an annotation action.
+                    //                    addAnnotateActionBehavior(editor);
+        
+                    // Add tooltip on label
+                    StringBuilder tooltipTitle = new StringBuilder();
+                    tooltipTitle.append(featureState.feature.getUiName());
+                    if (featureState.feature.getTagset() != null) {
+                        tooltipTitle.append(" (");
+                        tooltipTitle.append(featureState.feature.getTagset().getName());
+                        tooltipTitle.append(')');
+                    }
+        
+                    Component labelComponent = editor.getLabelComponent();
+                    labelComponent.add(new AttributeAppender("style", "cursor: help", ";"));
+                    labelComponent.add(new DescriptionTooltipBehavior(tooltipTitle.toString(),
+                        featureState.feature.getDescription()));
+                }
+                else {
+                    editor.getFocusComponent().setEnabled(false);
+                }
+    
+                // We need to enable the markup ID here because we use it during the AJAX behavior
+                // that automatically saves feature editors on change/blur.
+                // Check addAnnotateActionBehavior.
+                editor.setOutputMarkupId(true);
+                editor.setOutputMarkupPlaceholderTag(true);
+    
+                // Ensure that markup IDs of feature editor focus components remain constant
+                // across refreshes of the feature editor panel. This is required to restore the
+                // focus.
+                editor.getFocusComponent().setOutputMarkupId(true);
+                editor.getFocusComponent().setMarkupId(
+                    "" + editor.getModelObject().feature.getId());
+    
                 item.add(editor);
             }
         };
