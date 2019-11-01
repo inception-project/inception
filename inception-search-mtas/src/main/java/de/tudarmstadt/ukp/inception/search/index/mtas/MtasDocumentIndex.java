4fb0e65ee3e660cea3fe085f5104439e71b24ecf
Marcel de Boer <marceldeboer@web.de>
2019-11-01 10:22:09 +0100

    1054 - Dealing with many results in search sidebar
    - checkstyle
    - use LinkedHashMap for storing search results, so that results from the same document will stay together

--- inception-search-mtas/src/main/java/de/tudarmstadt/ukp/inception/search/index/mtas/MtasDocumentIndex.java
+++ inception-search-mtas/src/main/java/de/tudarmstadt/ukp/inception/search/index/mtas/MtasDocumentIndex.java
@@ -30,16 +30,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.text.BreakIterator;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.TreeMap;
+import java.util.*;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -412,7 +403,7 @@ public class MtasDocumentIndex
         SearchQueryRequest aRequest, String field, MtasSpanQuery q)
         throws IOException
     {
-        Map<String, List<SearchResult>> results = new TreeMap<>(FEATUREVALUE_COMPARATOR);
+        Map<String, List<SearchResult>> results = new LinkedHashMap<>();
 
         ListIterator<LeafReaderContext> leafReaderContextIterator = aIndexReader.leaves()
                 .listIterator();
