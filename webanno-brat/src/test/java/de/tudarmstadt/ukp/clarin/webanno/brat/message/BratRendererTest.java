cb1204cb5cc5c9fea1ac1dd7a550a63eed5dbbf2
Richard Eckart de Castilho <richard.eckart@gmail.com>
2019-11-26 11:01:30 +0100

    #1528 - Reduce database accesses during recommendation rendering
    - Fix unit test

--- webanno-brat/src/test/java/de/tudarmstadt/ukp/clarin/webanno/brat/message/BratRendererTest.java
+++ webanno-brat/src/test/java/de/tudarmstadt/ukp/clarin/webanno/brat/message/BratRendererTest.java
@@ -138,7 +138,7 @@ public class BratRendererTest
         layerRegistry.init();
         
         when(schemaService.listAnnotationLayer(any())).thenReturn(asList(posLayer));
-        when(schemaService.listAnnotationFeature(any(AnnotationLayer.class)))
+        when(schemaService.listAnnotationFeature(any(Project.class)))
                 .thenReturn(asList(posFeature));
         when(schemaService.getAdapter(any(AnnotationLayer.class))).then(_call -> {
             AnnotationLayer layer = _call.getArgument(0);
