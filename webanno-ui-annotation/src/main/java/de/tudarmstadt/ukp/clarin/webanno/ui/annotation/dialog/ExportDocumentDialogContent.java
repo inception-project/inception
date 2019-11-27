821e8080d9e7acb0dc207495a52989dd9b548b10
Richard Eckart de Castilho <richard.eckart@gmail.com>
2019-11-27 22:25:57 +0100

    #1536 - Exporting files of others
    - Export the proper annotations by using the user from the annotator state instead of using the logged-in user

--- webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/dialog/ExportDocumentDialogContent.java
+++ webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/dialog/ExportDocumentDialogContent.java
@@ -37,9 +37,9 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.resource.FileResourceStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import de.tudarmstadt.ukp.clarin.webanno.api.ImportExportService;
+import de.tudarmstadt.ukp.clarin.webanno.api.WebAnnoConst;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
 import de.tudarmstadt.ukp.clarin.webanno.api.format.FormatSupport;
 import de.tudarmstadt.ukp.clarin.webanno.model.Mode;
@@ -102,12 +102,19 @@ public class ExportDocumentDialogContent
     private FileResourceStream export()
     {
         File downloadFile = null;
-        String username = state.getObject().getMode()
+        
+        String username;
+        
+        if (state.getObject().getMode()
                 .equals(Mode.AUTOMATION)
                 && preferences.getObject().documentType
-                        .equals(SELECTEXPORT.AUTOMATED.toString()) ? "CORRECTION_USER"
-                                : SecurityContextHolder.getContext().getAuthentication()
-                                        .getName();
+                        .equals(SELECTEXPORT.AUTOMATED.toString())) {
+            username = WebAnnoConst.CORRECTION_USER;
+        }
+        else {
+            username = state.getObject().getUser().getUsername();
+        }
+        
         try {
             downloadFile = importExportService.exportAnnotationDocument(
                     state.getObject().getDocument(), username,
