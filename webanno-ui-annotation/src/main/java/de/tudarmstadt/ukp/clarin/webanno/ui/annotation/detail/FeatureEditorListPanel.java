3bd91b3b2fa043b2687e6be4a05a5ae8db7c8436
UWinch <winchenbach@ukp.informatik.tu-darmstadt.de>
2020-09-28 18:15:09 +0200

    #1785 Feature Editor wrongly shown
    - re-name for clarification

--- webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/detail/FeatureEditorListPanel.java
+++ webanno-ui-annotation/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/annotation/detail/FeatureEditorListPanel.java
@@ -92,21 +92,21 @@ public class FeatureEditorListPanel
         featureEditorContainer.setOutputMarkupPlaceholderTag(true);
         featureEditorContainer.add(featureEditorPanelContent = new FeatureEditorPanelContent("featureEditors"));
         featureEditorContainer.add(createFocusResetHelper());
-        featureEditorContainer.add(visibleWhen(() -> selectedLayerHasFeatures()));
+        featureEditorContainer.add(visibleWhen(() -> layerIsSelectedAndHasFeatures()));
         add(featureEditorContainer);
         
         noFeaturesWarning = new WebMarkupContainer("noFeaturesWarning");
         noFeaturesWarning.setOutputMarkupPlaceholderTag(true);
-        noFeaturesWarning.add(visibleWhen(() -> selectedLayerHasNoFeatures()));
+        noFeaturesWarning.add(visibleWhen(() -> layerIsSelectedButHasNoFeatures()));
         add(noFeaturesWarning);
     }
     
-    private boolean selectedLayerHasFeatures() {
+    private boolean layerIsSelectedAndHasFeatures() {
         return getModelObject().getSelection().getAnnotation().isSet() && 
                 !getModelObject().getFeatureStates().isEmpty();
     }
     
-    private boolean selectedLayerHasNoFeatures() {
+    private boolean layerIsSelectedButHasNoFeatures() {
         return getModelObject().getSelection().getAnnotation().isSet() && 
                 getModelObject().getFeatureStates().isEmpty();
     }
