945bcde69dc0840506699ecf1e94f2de922a82c4
Christoph <christoph.scheunemann@stud.tu-darmstadt.de>
2020-09-29 14:30:54 +0200

    #1813-Last-activity-link-do-not-work-anymore - Fixed #1813

--- inception-workload-dynamic/src/main/java/de/tudarmstadt/ukp/inception/workload/dynamic/annotation/DynamicWorkflowDocumentNavigationActionBarExtension.java
+++ inception-workload-dynamic/src/main/java/de/tudarmstadt/ukp/inception/workload/dynamic/annotation/DynamicWorkflowDocumentNavigationActionBarExtension.java
@@ -75,6 +75,11 @@ public class DynamicWorkflowDocumentNavigationActionBarExtension implements Acti
     @Override
     public boolean accepts (AnnotationPageBase aPage)
     {
+        // #Issue 1813 fix
+        if (aPage.getModelObject().getProject() == null) {
+            return false;
+        }
+
         return DYNAMIC_WORKLOAD_MANAGER_EXTENSION_ID.equals(workloadManagementService.
             getOrCreateWorkloadManagerConfiguration(aPage.getModelObject().getProject())
             .getType());
