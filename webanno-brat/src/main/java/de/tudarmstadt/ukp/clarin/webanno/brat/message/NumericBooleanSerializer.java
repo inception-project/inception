b1f6cf2e872e73b5482223026a3d8c5f3e846a8a
Richard Eckart de Castilho <richard.eckart@gmail.com>
2020-10-06 12:04:14 +0200

    #1697 - More explicit UI for accepting/rejecting recommender suggestons - feature for INCEpTION
    - Checkstyle fixes

--- webanno-brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/message/NumericBooleanSerializer.java
+++ webanno-brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/message/NumericBooleanSerializer.java
@@ -1,16 +1,19 @@
 package de.tudarmstadt.ukp.clarin.webanno.brat.message;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-import java.io.IOException;
-
-public class NumericBooleanSerializer extends JsonSerializer<Boolean>  {
+public class NumericBooleanSerializer
+    extends JsonSerializer<Boolean>
+{
     @Override
-    public void serialize(Boolean b, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) 
-            throws IOException {
+    public void serialize(Boolean b, JsonGenerator jsonGenerator,
+            SerializerProvider serializerProvider)
+        throws IOException
+    {
         jsonGenerator.writeNumber(b ? 1 : 0);
     }
-
 }
