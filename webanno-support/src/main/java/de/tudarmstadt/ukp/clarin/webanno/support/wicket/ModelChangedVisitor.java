f6f051eb05f2d681af3a13260a853370fcf1340d
Richard Eckart de Castilho <richard.eckart@gmail.com>
2020-10-27 20:57:36 +0100

    #1793 - Upgrade to Wicket 9
    - Fixed JavaDoc

--- webanno-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/wicket/ModelChangedVisitor.java
+++ webanno-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/wicket/ModelChangedVisitor.java
@@ -28,7 +28,7 @@ import org.apache.wicket.util.visit.IVisitor;
 
 /**
  * In contrast to {@link Component#sameInnermostModel}, this visitor can not only handle
- * {@link IWrapModel} but also {@link IChainingModel}, e.g. {@link CompoundPropertyModel}
+ * {@link IWrapModel} but also {@link ChainingModel}, e.g. {@link CompoundPropertyModel}
  * which is used in forms.
  */
 public class ModelChangedVisitor
