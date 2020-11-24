8669e2e2d8a4bee9c5291fe9fa86e7cbec37d384
Richard Eckart de Castilho <richard.eckart@gmail.com>
2020-11-24 17:14:29 +0100

    #1814 - Format code
    - Format webanno-plugin-api and webanno-plugin-manager

--- webanno-plugin-api/src/main/java/de/tudarmstadt/ukp/clarin/webanno/plugin/api/ExportedComponent.java
+++ webanno-plugin-api/src/main/java/de/tudarmstadt/ukp/clarin/webanno/plugin/api/ExportedComponent.java
@@ -23,7 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ ElementType.METHOD, ElementType.TYPE })
 public @interface ExportedComponent
 {
 }
