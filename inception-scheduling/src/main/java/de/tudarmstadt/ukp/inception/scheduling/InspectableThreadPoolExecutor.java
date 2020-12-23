d32345b8a29ae532b8960b31cba93501ab73bf90
Richard Eckart de Castilho <richard.eckart@gmail.com>
2020-12-23 11:51:17 +0100

    #1905 - Equivalent tasks run in parallel
    - If an equivalent task has already been passed to the thread pool, put a new task into a separate queue outside the threadpool

--- inception-scheduling/src/main/java/de/tudarmstadt/ukp/inception/scheduling/InspectableThreadPoolExecutor.java
+++ inception-scheduling/src/main/java/de/tudarmstadt/ukp/inception/scheduling/InspectableThreadPoolExecutor.java
@@ -17,6 +17,8 @@
  */
 package de.tudarmstadt.ukp.inception.scheduling;
 
+import static java.lang.Thread.MIN_PRIORITY;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -26,15 +28,14 @@ import java.util.function.BiConsumer;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 public class InspectableThreadPoolExecutor
-        extends ThreadPoolExecutor
+    extends ThreadPoolExecutor
 {
     private final BiConsumer<Thread, Runnable> beforeExecuteCallback;
     private final BiConsumer<Runnable, Throwable> afterExecuteCallback;
 
-    public InspectableThreadPoolExecutor(int aNumberOfThreads,
-                                         int queueSize,
-                                         BiConsumer<Thread, Runnable> aBeforeExecuteCallback,
-                                         BiConsumer<Runnable, Throwable> aAfterExecuteCallback)
+    public InspectableThreadPoolExecutor(int aNumberOfThreads, int queueSize,
+            BiConsumer<Thread, Runnable> aBeforeExecuteCallback,
+            BiConsumer<Runnable, Throwable> aAfterExecuteCallback)
     {
         super(aNumberOfThreads, aNumberOfThreads, 0L, TimeUnit.MILLISECONDS,
                 new ArrayBlockingQueue<>(queueSize, true), buildThreadFactory());
@@ -61,9 +62,10 @@ public class InspectableThreadPoolExecutor
 
     private static ThreadFactory buildThreadFactory()
     {
-        return new BasicThreadFactory.Builder()
-                .daemon(true)
-                .priority(Thread.MIN_PRIORITY)
+        return new BasicThreadFactory.Builder() //
+                .daemon(true) //
+                .namingPattern("recommender-worker-%d") //
+                .priority(MIN_PRIORITY) //
                 .build();
     }
 }
