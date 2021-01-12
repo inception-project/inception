148c6bd73325f16e8af24c659dc9bca7f3b3168f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-01-13 00:27:46 +0100

    #818 - Inconsistency in SourceDocument model
    - Align source document hashCode and equals with the business primary key constraint in the DB (project / name)

--- webanno-model/src/main/java/de/tudarmstadt/ukp/clarin/webanno/model/SourceDocument.java
+++ webanno-model/src/main/java/de/tudarmstadt/ukp/clarin/webanno/model/SourceDocument.java
@@ -20,6 +20,7 @@ package de.tudarmstadt.ukp.clarin.webanno.model;
 import java.io.Serializable;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -217,11 +218,7 @@ public class SourceDocument
     @Override
     public int hashCode()
     {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+        return Objects.hash(name, project);
     }
 
     @Override
@@ -237,23 +234,7 @@ public class SourceDocument
             return false;
         }
         SourceDocument other = (SourceDocument) obj;
-        if (id == null) {
-            if (other.id != null) {
-                return false;
-            }
-        }
-        else if (!id.equals(other.id)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        }
-        else if (!name.equals(other.name)) {
-            return false;
-        }
-        return true;
+        return Objects.equals(name, other.name) && Objects.equals(project, other.project);
     }
 
     @Override
