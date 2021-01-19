d9f641bb29c570655010fd1b8ad0e4a42794d865
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-01-19 19:59:41 +0100

    #1862 - Creating Project with no permission
    - Adjust handling of permissions during import so that admins importing without checking "import permissions" gain project manager permissions on the imported project.

--- webanno-ui-project/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/project/ProjectImportPanel.java
+++ webanno-ui-project/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/project/ProjectImportPanel.java
@@ -138,11 +138,23 @@ public class ProjectImportPanel
             importPermissions = false;
         }
 
-        // If the current user is a project creator then we assume that the user is importing the
-        // project for own use, so we add the user as a project manager. We do not do this if the
-        // user is "just" an administrator but not a project creator.
-        Optional<User> manager = currentUserIsProjectCreator ? Optional.of(currentUser)
-                : Optional.empty();
+        // If the current user is an administrator and importing of permissions is *DISABLED*, we
+        // configure the current user as a project manager. But if importing of permissions is
+        // *ENABLED*, we do not set the admin up as a project manager because we would assume that
+        // the admin wants to restore a project (maybe one exported from another instance) and in
+        // that case we want to maintain the permissions the project originally had without adding
+        // the admin as a manager.
+        Optional<User> manager = Optional.empty();
+        if (currentUserIsAdministrator) {
+            if (!importPermissions) {
+                manager = Optional.of(currentUser);
+            }
+        }
+        // If the current user is NOT an admin but a project creator then we assume that the user is
+        // importing the project for own use, so we add the user as a project manager.
+        else if (currentUserIsProjectCreator) {
+            manager = Optional.of(currentUser);
+        }
 
         Project importedProject = null;
         for (FileUpload exportedProject : exportedProjects) {
