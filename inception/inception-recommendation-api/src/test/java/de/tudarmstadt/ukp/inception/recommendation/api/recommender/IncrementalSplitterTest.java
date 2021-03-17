a540f89e2e43d8078ae31bbb5db746657a23cd09
Jan-Christoph Klie <git@mrklie.com>
2021-03-17 19:23:13 +0100

    #2107 - Migrate to Junit 5
    - Fix splitter tests

--- inception/inception-recommendation-api/src/test/java/de/tudarmstadt/ukp/inception/recommendation/api/recommender/IncrementalSplitterTest.java
+++ inception/inception-recommendation-api/src/test/java/de/tudarmstadt/ukp/inception/recommendation/api/recommender/IncrementalSplitterTest.java
@@ -25,97 +25,79 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import de.tudarmstadt.ukp.inception.recommendation.api.evaluation.IncrementalSplitter;
 
-@RunWith(Enclosed.class)
 public class IncrementalSplitterTest
 {
 
-    @RunWith(Parameterized.class)
-    public static class ParameterizedTests
+    @ParameterizedTest(name = "{index}: running on data k: {0}, #train: {1}, #test: {2}")
+    @MethodSource("data")
+    public void thatSplittingWorks(double aK, int aTrainingSetSize, int aTestSetSize)
     {
-        private final double k;
-        private final int trainingSetSize;
-        private final int testSetSize;
+        List<String> data = asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        IncrementalSplitter splitter = new IncrementalSplitter(aK, 1, 4);
 
-        public ParameterizedTests(double aK, int aTrainingSetSize, int aTestSetSize)
-        {
-            k = aK;
-            trainingSetSize = aTrainingSetSize;
-            testSetSize = aTestSetSize;
-        }
+        int currentTrainingSetSize = 1;
 
-        @Test
-        public void thatSplittingWorks()
-        {
-            List<String> data = asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-            IncrementalSplitter splitter = new IncrementalSplitter(k, 1, 4);
+        List<String> trainingSet = new ArrayList<>();
+        List<String> testSet = new ArrayList<>();
+        while (splitter.hasNext()) {
+            splitter.next();
 
-            int currentTrainingSetSize = 1;
+            trainingSet.clear();
+            testSet.clear();
 
-            List<String> trainingSet = new ArrayList<>();
-            List<String> testSet = new ArrayList<>();
-            while (splitter.hasNext()) {
-                splitter.next();
-
-                trainingSet.clear();
-                testSet.clear();
-
-                for (String s : data) {
-                    switch (splitter.getTargetSet(s)) {
-                    case TRAIN:
-                        trainingSet.add(s);
-                        break;
-                    case TEST:
-                        testSet.add(s);
-                        break;
-                    default:
-                        // Do nothing
-                        break;
-                    }
+            for (String s : data) {
+                switch (splitter.getTargetSet(s)) {
+                case TRAIN:
+                    trainingSet.add(s);
+                    break;
+                case TEST:
+                    testSet.add(s);
+                    break;
+                default:
+                    // Do nothing
+                    break;
                 }
-
-                assertThat(trainingSet).as("Training set has correct size")
-                        .hasSize(currentTrainingSetSize);
-                assertThat(testSet).as("Test set has correct size").hasSize(testSetSize);
-
-                currentTrainingSetSize++;
             }
 
-            assertThat(trainingSet).as("Training set has correct size").hasSize(trainingSetSize);
-            assertThat(testSet).as("Test set has correct size").hasSize(testSetSize);
+            assertThat(trainingSet).as("Training set has correct size")
+                    .hasSize(currentTrainingSetSize);
+            assertThat(testSet).as("Test set has correct size").hasSize(aTestSetSize);
+
+            currentTrainingSetSize++;
         }
 
-        @Parameterized.Parameters
-        public static Collection<Object[]> data()
-        {
-            // k, trainingSetSize, testSetSize
-            return Arrays.asList(new Object[][] { //
-                    { 0.1, 2, 8 }, //
-                    { 0.2, 2, 8 }, //
-                    { 0.3, 3, 7 }, //
-                    { 0.4, 4, 6 }, //
-                    { 0.5, 5, 5 }, //
-                    { 0.6, 6, 4 }, //
-                    { 0.7, 7, 3 }, //
-                    { 0.8, 8, 2 }, //
-                    { 0.9, 8, 2 } });
-        }
+        assertThat(trainingSet).as("Training set has correct size").hasSize(aTrainingSetSize);
+        assertThat(testSet).as("Test set has correct size").hasSize(aTestSetSize);
     }
 
-    @RunWith(JUnit4.class)
-    public static class NonParameterizedTests
+    public static Collection<Object[]> data()
     {
-        @Test(expected = IllegalArgumentException.class)
-        public void thatPercentageHasToBePercentage()
-        {
-            IncrementalSplitter splitter = new IncrementalSplitter(42.1337, 1, 10);
-        }
+        // k, trainingSetSize, testSetSize
+        return Arrays.asList(new Object[][] { //
+                { 0.1, 2, 8 }, //
+                { 0.2, 2, 8 }, //
+                { 0.3, 3, 7 }, //
+                { 0.4, 4, 6 }, //
+                { 0.5, 5, 5 }, //
+                { 0.6, 6, 4 }, //
+                { 0.7, 7, 3 }, //
+                { 0.8, 8, 2 }, //
+                { 0.9, 8, 2 } });
     }
+
+    @Test
+    public void thatPercentageHasToBePercentage()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            IncrementalSplitter splitter = new IncrementalSplitter(42.1337, 1, 10);
+        });
+    }
+
 }
--- inception/inception-recommendation-api/src/test/java/de/tudarmstadt/ukp/inception/recommendation/api/recommender/PercentageBasedSplitterTest.java
+++ inception/inception-recommendation-api/src/test/java/de/tudarmstadt/ukp/inception/recommendation/api/recommender/PercentageBasedSplitterTest.java
@@ -25,82 +25,65 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import de.tudarmstadt.ukp.inception.recommendation.api.evaluation.PercentageBasedSplitter;
 
-@RunWith(Enclosed.class)
 public class PercentageBasedSplitterTest
 {
 
-    @RunWith(Parameterized.class)
-    public static class ParameterizedTests
+    @ParameterizedTest(name = "{index}: running on data k: {0}, #train: {1}, #test: {2}")
+    @MethodSource("data")
+    public void thatSplittingWorks(double aK, int aTrainingSetSize, int aTestSetSize)
     {
-        private final double k;
-        private final int trainingSetSize;
-        private final int testSetSize;
+        List<String> data = asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        PercentageBasedSplitter splitter = new PercentageBasedSplitter(aK, 4);
+        List<String> trainingSet = new ArrayList<>();
+        List<String> testSet = new ArrayList<>();
 
-        public ParameterizedTests(double aK, int aTrainingSetSize, int aTestSetSize)
-        {
-            k = aK;
-            trainingSetSize = aTrainingSetSize;
-            testSetSize = aTestSetSize;
-        }
-
-        @Test
-        public void thatSplittingWorks()
-        {
-            List<String> data = asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-            PercentageBasedSplitter splitter = new PercentageBasedSplitter(k, 4);
-            List<String> trainingSet = new ArrayList<>();
-            List<String> testSet = new ArrayList<>();
-
-            for (String s : data) {
-                switch (splitter.getTargetSet(s)) {
-                case TRAIN:
-                    trainingSet.add(s);
-                    break;
-                case TEST:
-                    testSet.add(s);
-                    break;
-                default:
-                    // Do nothing
-                    break;
-                }
+        for (String s : data) {
+            switch (splitter.getTargetSet(s)) {
+            case TRAIN:
+                trainingSet.add(s);
+                break;
+            case TEST:
+                testSet.add(s);
+                break;
+            default:
+                // Do nothing
+                break;
             }
-
-            assertThat(trainingSet).as("Training set has correct size").hasSize(trainingSetSize);
-            assertThat(testSet).as("Test set has correct size").hasSize(testSetSize);
         }
 
-        @Parameterized.Parameters
-        public static Collection<Object[]> data()
-        {
-            // k, trainingSetSize, testSetSize
-            return Arrays.asList(new Object[][] { //
-                    { 0.1, 2, 8 }, //
-                    { 0.2, 2, 8 }, //
-                    { 0.3, 3, 7 }, //
-                    { 0.4, 4, 6 }, //
-                    { 0.5, 5, 5 }, //
-                    { 0.6, 6, 4 }, //
-                    { 0.7, 7, 3 }, //
-                    { 0.8, 8, 2 }, //
-                    { 0.9, 8, 2 } });
-        }
+        assertThat(trainingSet).as("Training set has correct size").hasSize(aTrainingSetSize);
+        assertThat(testSet).as("Test set has correct size").hasSize(aTestSetSize);
     }
 
-    @RunWith(JUnit4.class)
-    public static class NonParameterizedTests
+
+    public static Collection<Object[]> data()
     {
-        @Test(expected = IllegalArgumentException.class)
-        public void thatPercentageHasToBePercentage()
-        {
+        // k, trainingSetSize, testSetSize
+        return Arrays.asList(new Object[][] { //
+                { 0.1, 2, 8 }, //
+                { 0.2, 2, 8 }, //
+                { 0.3, 3, 7 }, //
+                { 0.4, 4, 6 }, //
+                { 0.5, 5, 5 }, //
+                { 0.6, 6, 4 }, //
+                { 0.7, 7, 3 }, //
+                { 0.8, 8, 2 }, //
+                { 0.9, 8, 2 } });
+    }
+
+
+    @Test()
+    public void thatPercentageHasToBePercentage()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
             PercentageBasedSplitter splitter = new PercentageBasedSplitter(42.1337, 10);
-        }
+        });
     }
 }
