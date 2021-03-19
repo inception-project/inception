7ade1409ad98a9dc07b3b6297d1b996133305ad7
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-03-19 22:12:00 +0100

    #2115 - Better info when running in unsupported Java
    - Reduce Java target in boot module to Java 1.8
    - Display message / open window when running on a version lower than Java 11

--- inception/inception-boot-loader/src/main/java/de/tudarmstadt/ukp/inception/bootloader/ExtensibleClasspathEnabledWarLauncher.java
+++ inception/inception-boot-loader/src/main/java/de/tudarmstadt/ukp/inception/bootloader/ExtensibleClasspathEnabledWarLauncher.java
@@ -17,6 +17,7 @@
  */
 package de.tudarmstadt.ukp.inception.bootloader;
 
+import java.awt.GraphicsEnvironment;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -54,9 +55,10 @@ public class ExtensibleClasspathEnabledWarLauncher
             throw new IllegalStateException(ex);
         }
     }
-    
+
     @Override
-    protected Iterator<Archive> getClassPathArchivesIterator() throws Exception {
+    protected Iterator<Archive> getClassPathArchivesIterator() throws Exception
+    {
         return getClassPathArchives().iterator();
     }
 
@@ -67,21 +69,22 @@ public class ExtensibleClasspathEnabledWarLauncher
         List<Archive> archives = new ArrayList<>();
         archive.getNestedArchives(this::isNestedArchive, candidateArchive -> true)
                 .forEachRemaining(archives::add);
-        
+
         Path extraLibPath = Paths.get(getApplicationHome() + EXTRA_LIB);
-        
+
         if (Files.exists(extraLibPath)) {
             debug("Scanning for additional JARs in [" + extraLibPath + "]");
-            
+
             for (Path jar : Files.newDirectoryStream(extraLibPath, "*.jar")) {
                 debug("Registering JAR: [" + jar + "]");
                 archives.add(new JarFileArchive(jar.toFile()));
             }
         }
         else {
-            debug("Not scanning for additional JARs in [" + extraLibPath + "] - path does not exist");
+            debug("Not scanning for additional JARs in [" + extraLibPath
+                    + "] - path does not exist");
         }
-        
+
         return archives;
     }
 
@@ -120,9 +123,52 @@ public class ExtensibleClasspathEnabledWarLauncher
             return userHome + "/" + ".inception";
         }
     }
-    
+
+    public static boolean checkSystemRequirements()
+    {
+        String verString = System.getProperty("java.specification.version");
+        int ver = 99;
+
+        try {
+            if (verString.contains(".")) {
+                final String[] toParse = verString.split("\\.");
+                if (toParse.length >= 1) {
+                    ver = Integer.valueOf(toParse[0]);
+                }
+            }
+            else {
+                ver = Integer.valueOf(verString);
+            }
+        }
+        catch (Exception e) {
+            System.err.printf(
+                    "Unable to determine your Java version from the version string [%s]. Let's "
+                            + "assume it is compatible with Java 11 and start.%n",
+                    verString);
+        }
+
+        if (ver < 11) {
+            String message = String.format(
+                    "INCEpTION requires at least Java 11, but you are running Java %s.", verString);
+            if (!GraphicsEnvironment.isHeadless()) {
+                UIMessage.displayMessage(message);
+            }
+            else {
+                System.err.println(message);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) throws Exception
     {
-        new ExtensibleClasspathEnabledWarLauncher().launch(args);
+        boolean okToLaunch = checkSystemRequirements();
+
+        if (okToLaunch) {
+            new ExtensibleClasspathEnabledWarLauncher().launch(args);
+        }
     }
 }
