0f23ea7d2ad45b389a0f115a73aacdc34fef8ae3
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-04-01 22:10:31 +0200

    #2046 - List logged events with spring websockets
    - Remove unused imports

--- inception/inception-websocket/src/main/java/de/tudarmstadt/ukp/inception/websocket/config/WebsocketConfig.java
+++ inception/inception-websocket/src/main/java/de/tudarmstadt/ukp/inception/websocket/config/WebsocketConfig.java
@@ -17,25 +17,11 @@
  */
 package de.tudarmstadt.ukp.inception.websocket.config;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
-import de.tudarmstadt.ukp.clarin.webanno.api.ProjectService;
-import de.tudarmstadt.ukp.clarin.webanno.security.UserDao;
-import de.tudarmstadt.ukp.inception.log.EventRepository;
-import de.tudarmstadt.ukp.inception.log.adapter.EventLoggingAdapter;
-import de.tudarmstadt.ukp.inception.websocket.LoggedEventMessageController;
-import de.tudarmstadt.ukp.inception.websocket.LoggedEventMessageControllerImpl;
 
 @Configuration
 @EnableWebSocketMessageBroker
