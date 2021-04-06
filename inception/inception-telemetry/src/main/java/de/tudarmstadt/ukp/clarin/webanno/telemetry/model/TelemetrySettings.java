96f3d97e6c06e61a52627299198050b229c25eb8
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-04-06 11:29:52 +0200

    #2163 - Created timestamp not properly set on some tables
    - Fix condition

--- inception/inception-telemetry/src/main/java/de/tudarmstadt/ukp/clarin/webanno/telemetry/model/TelemetrySettings.java
+++ inception/inception-telemetry/src/main/java/de/tudarmstadt/ukp/clarin/webanno/telemetry/model/TelemetrySettings.java
@@ -120,7 +120,7 @@ public class TelemetrySettings
     {
         // When we import data, we set the fields via setters and don't want these to be
         // overwritten by this event handler.
-        if (created != null) {
+        if (created == null) {
             created = new Date();
             updated = created;
         }
