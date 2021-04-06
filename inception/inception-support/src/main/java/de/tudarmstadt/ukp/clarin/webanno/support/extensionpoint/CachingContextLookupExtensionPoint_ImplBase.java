abd3f443419fc2e089c940feda196c5358584488
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-04-06 11:17:17 +0200

    #2158 - Change findExtension into a method that returns an optional value
    - Use a Caffeine cache instead of a simple map

--- inception/inception-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/extensionpoint/CachingContextLookupExtensionPoint_ImplBase.java
+++ inception/inception-support/src/main/java/de/tudarmstadt/ukp/clarin/webanno/support/extensionpoint/CachingContextLookupExtensionPoint_ImplBase.java
@@ -17,18 +17,20 @@
  */
 package de.tudarmstadt.ukp.clarin.webanno.support.extensionpoint;
 
-import java.util.HashMap;
+import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 
 import org.apache.commons.lang3.Validate;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 public abstract class CachingContextLookupExtensionPoint_ImplBase<C, E extends Extension<C>>
     extends ExtensionPoint_ImplBase<C, E>
     implements ContextLookupExtensionPoint<C, E>
 {
-    private final Map<Object, E> cache = new HashMap<>();
+    private final Cache<Object, E> cache;
 
     private final Function<C, Object> keyExtractor;
 
@@ -37,12 +39,30 @@ public abstract class CachingContextLookupExtensionPoint_ImplBase<C, E extends E
     {
         super(aExtensions);
         keyExtractor = aKeyExtractor;
+        cache = makeCache();
+    }
+
+    protected Cache<Object, E> makeCache()
+    {
+        return Caffeine.newBuilder() //
+                .expireAfterAccess(Duration.ofHours(1)) //
+                .build();
+    }
+
+    protected E findFirstAcceptingExtension(C aContext)
+    {
+        for (E s : getExtensions()) {
+            if (s.accepts(aContext)) {
+                return s;
+            }
+        }
+        return null;
     }
 
     @Override
     public synchronized <X extends E> X findExtension(C aContext)
     {
-        Validate.notNull(aContext, "Extension lookup key must be specified");
+        Validate.notNull(aContext, "Extension lookup context must be specified");
 
         // This method is called often during rendering, so we try to make it fast by caching
         // the supports by feature. Since the set of annotation features is relatively stable,
@@ -50,23 +70,13 @@ public abstract class CachingContextLookupExtensionPoint_ImplBase<C, E extends E
         // features would be deleted from the DB.
         E extension = null;
 
+        // Store feature in the cache, but only when it has an ID, i.e. it has actually been saved.
         Object keyId = keyExtractor.apply(aContext);
         if (keyId != null) {
-            extension = cache.get(keyId);
+            extension = cache.get(keyId, _keyId -> findFirstAcceptingExtension(aContext));
         }
-
-        if (extension == null) {
-            for (E s : getExtensions()) {
-                if (s.accepts(aContext)) {
-                    extension = s;
-                    if (keyId != null) {
-                        // Store feature in the cache, but only when it has an ID, i.e. it has
-                        // actually been saved.
-                        cache.put(keyId, s);
-                    }
-                    break;
-                }
-            }
+        else {
+            extension = findFirstAcceptingExtension(aContext);
         }
 
         if (extension == null) {
