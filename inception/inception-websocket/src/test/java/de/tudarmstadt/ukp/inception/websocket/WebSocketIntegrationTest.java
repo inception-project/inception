c6f32d61fa9291418ecdd0e1b95e1a5641831c32
UWinch <winchenbach@ukp.informatik.tu-darmstadt.de>
2021-05-17 09:57:51 +0200

    #2046 List logged events with spring websockets
    - adapt to changes after last merge

--- inception/inception-websocket/src/test/java/de/tudarmstadt/ukp/inception/websocket/WebSocketIntegrationTest.java
+++ inception/inception-websocket/src/test/java/de/tudarmstadt/ukp/inception/websocket/WebSocketIntegrationTest.java
@@ -47,7 +47,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
@@ -59,38 +58,14 @@ import org.springframework.security.config.annotation.web.socket.AbstractSecurit
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
-import de.tudarmstadt.ukp.clarin.webanno.api.CasStorageService;
-import de.tudarmstadt.ukp.clarin.webanno.api.DocumentImportExportService;
 import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
 import de.tudarmstadt.ukp.clarin.webanno.api.ProjectService;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.BooleanFeatureSupport;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.FeatureSupportRegistry;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.FeatureSupportRegistryImpl;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.NumberFeatureSupport;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.feature.StringFeatureSupport;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.layer.ChainLayerSupport;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.layer.LayerSupportRegistry;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.layer.LayerSupportRegistryImpl;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.layer.RelationLayerSupport;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.layer.SpanLayerSupport;
 import de.tudarmstadt.ukp.clarin.webanno.api.config.RepositoryProperties;
-import de.tudarmstadt.ukp.clarin.webanno.api.dao.AnnotationSchemaServiceImpl;
-import de.tudarmstadt.ukp.clarin.webanno.api.dao.DocumentImportExportServiceImpl;
-import de.tudarmstadt.ukp.clarin.webanno.api.dao.DocumentServiceImpl;
-import de.tudarmstadt.ukp.clarin.webanno.api.dao.docimexport.config.DocumentImportExportServiceProperties;
-import de.tudarmstadt.ukp.clarin.webanno.api.dao.docimexport.config.DocumentImportExportServicePropertiesImpl;
 import de.tudarmstadt.ukp.clarin.webanno.api.event.DocumentStateChangedEvent;
-import de.tudarmstadt.ukp.clarin.webanno.api.project.ProjectInitializer;
 import de.tudarmstadt.ukp.clarin.webanno.model.Project;
 import de.tudarmstadt.ukp.clarin.webanno.model.SourceDocument;
 import de.tudarmstadt.ukp.clarin.webanno.model.SourceDocumentState;
-import de.tudarmstadt.ukp.clarin.webanno.project.ProjectServiceImpl;
-import de.tudarmstadt.ukp.clarin.webanno.security.UserDao;
-import de.tudarmstadt.ukp.clarin.webanno.security.UserDaoImpl;
 import de.tudarmstadt.ukp.clarin.webanno.support.logging.Logging;
-import de.tudarmstadt.ukp.clarin.webanno.text.PretokenizedTextFormatSupport;
-import de.tudarmstadt.ukp.clarin.webanno.text.TextFormatSupport;
 import de.tudarmstadt.ukp.inception.log.adapter.DocumentStateChangedEventAdapter;
 import de.tudarmstadt.ukp.inception.log.adapter.EventLoggingAdapter;
 import de.tudarmstadt.ukp.inception.websocket.model.LoggedEventMessage;
@@ -226,73 +201,6 @@ public class WebSocketIntegrationTest
     "de.tudarmstadt.ukp.inception.log.model"})
     public static class WebSocketTestConfig
     {
-
-        private @Autowired ApplicationEventPublisher applicationEventPublisher;
-        private @Autowired EntityManager entityManager;
-
-        @Bean
-        public AnnotationSchemaService annotationSchemaService(
-                LayerSupportRegistry aLayerSupportRegistry,
-                FeatureSupportRegistry aFeatureSupportRegistry)
-        {
-            return new AnnotationSchemaServiceImpl(aLayerSupportRegistry, aFeatureSupportRegistry,
-                    entityManager);
-        }
-
-        @Bean
-        public LayerSupportRegistry layerSupportRegistry(
-                FeatureSupportRegistry aFeatureSupportRegistry)
-        {
-            return new LayerSupportRegistryImpl(
-                    asList(new SpanLayerSupport(aFeatureSupportRegistry, null, null),
-                            new RelationLayerSupport(aFeatureSupportRegistry, null, null),
-                            new ChainLayerSupport(aFeatureSupportRegistry, null, null)));
-        }
-
-        @Bean
-        public FeatureSupportRegistry featureSupportRegistry()
-        {
-            return new FeatureSupportRegistryImpl(asList(new NumberFeatureSupport(),
-                    new BooleanFeatureSupport(), new StringFeatureSupport()));
-        }
-
-        @Bean
-        public ProjectService projectService(UserDao aUserDao,
-                RepositoryProperties aRepositoryProperties,
-                @Lazy @Autowired(required = false) List<ProjectInitializer> aInitializerProxy)
-        {
-            return new ProjectServiceImpl(aUserDao, applicationEventPublisher,
-                    aRepositoryProperties, aInitializerProxy);
-        }
-
-        @Bean
-        public UserDao userRepository()
-        {
-            return new UserDaoImpl();
-        }
-
-        @Bean
-        public DocumentService documentService(RepositoryProperties aRepositoryProperties,
-                CasStorageService aCasStorageService,
-                DocumentImportExportService aImportExportService, ProjectService aProjectService,
-                ApplicationEventPublisher aApplicationEventPublisher)
-        {
-            return new DocumentServiceImpl(aRepositoryProperties, aCasStorageService,
-                    aImportExportService, aProjectService, aApplicationEventPublisher);
-        }
-        
-        @Bean
-        public DocumentImportExportService importExportService(
-                RepositoryProperties aRepositoryProperties,
-                AnnotationSchemaService aAnnotationSchemaService,
-                CasStorageService aCasStorageService)
-        {
-            DocumentImportExportServiceProperties properties = new DocumentImportExportServicePropertiesImpl();
-            return new DocumentImportExportServiceImpl(aRepositoryProperties,
-                    List.of(new TextFormatSupport(), new PretokenizedTextFormatSupport()),
-                    aCasStorageService, aAnnotationSchemaService, properties);
-        }
-        
         @Bean
         public List<EventLoggingAdapter<?>> eventLoggingAdapter()
         {
