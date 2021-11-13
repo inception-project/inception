e4d9c0ab6418d26e19985eee2052d4008185db21
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-11-13 13:26:29 +0100

    #2687 -Subscribe to annotation updates via WebSocket
    - Starting to pull ts maven config out into a profile

--- inception/inception-api-annotation-experimental/src/main/ts/client/AnnotationEditing.ts
+++ inception/inception-api-annotation-experimental/src/main/ts/client/AnnotationEditing.ts
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Client, Stomp, StompSubscription} from '@stomp/stompjs';
+import { Client, Stomp, StompSubscription, messageCallbackType, IFrame } from '@stomp/stompjs';
 
 /**
  * Implementation of the Interface AnnotationExperienceAPI within that package.
@@ -27,7 +27,8 @@ export class AnnotationEditing {
 
     stompClient: Client;
     webSocket: WebSocket;
-    subscription: StompSubscription;
+    initSubscription: StompSubscription;
+    updateSubscription: StompSubscription;
 
     /**
      * Constructor: creates the Annotation Experience API.
@@ -37,9 +38,8 @@ export class AnnotationEditing {
      * @param aUrl: The URL required by Websocket and the stomp broker in order to establish a connection
      * @NOTE: the connect() method will automatically be performed, there is no need to create a Websocket connection
     * manually.
-*/
-    constructor(aUrl: string)
-    {
+    */
+    constructor(aUrl: string) {
         this.connect(aUrl)
     }
 
@@ -49,129 +49,47 @@ export class AnnotationEditing {
      * @NOTE: When a connection is established, onConnect() will be called automatically so
      * there is no need to call it. Also, all subscriptions will be handled automatically.
      */
-    connect(aUrl: string)
-    {
+    connect(aUrl: string) {
         if (this.stompClient) {
-            throw "Already connected"; 
+            throw "Already connected";
         }
 
         this.stompClient = Stomp.over(() => this.webSocket = new WebSocket(aUrl));
-        this.stompClient.onConnect = () => { 
-            this.stompClient.subscribe("/user/queue/errors",  this.handleProtocolError);
+        this.stompClient.reconnectDelay = 5000;
+        this.stompClient.onConnect = () => {
+            this.stompClient.subscribe("/user/queue/errors", this.handleProtocolError);
         }
         this.stompClient.onStompError = this.handleBrokerError;
-        this.stompClient.reconnectDelay = 5000;
         this.stompClient.activate();
     }
 
-    disconnect()
-    {
+    disconnect() {
         this.stompClient.deactivate();
         this.webSocket.close();
     }
 
-    handleBrokerError(frame) {
-        console.log('Broker reported error: ' + frame.headers['message']);
-        console.log('Additional details: ' + frame.body);
+    handleBrokerError(receipt: IFrame) {
+        console.log('Broker reported error: ' + receipt.headers['message']);
+        console.log('Additional details: ' + receipt.body);
     }
 
     handleProtocolError(msg) {
         console.log(msg);
     }
 
-    subscribeToViewport(aViewportTopic: string, messageCallbackType)
-    {
-        this.stompClient.subscribe(aViewportTopic, callback);
+    subscribeToViewport(aViewportTopic: string, initCallback: messageCallbackType, updateCallback: messageCallbackType) {
+        this.unsubscribeFromViewport();
+        this.initSubscription = this.stompClient.subscribe('/app' + aViewportTopic, initCallback);
+        this.updateSubscription = this.stompClient.subscribe('/topic' + aViewportTopic, updateCallback);
     }
 
-    unsubscribeFromViewport() 
-    {
-
-    }
-
-    requestDocument(aAnnotatorName: string, aProjectId: number, aViewport: Viewport[])
-    {
-        this.stompClient.publish({
-            destination: "/app/document_request", body: JSON.stringify(
-                new DocumentRequest(aAnnotatorName, aProjectId, aViewport))
-        });
-    }
-
-    requestCreateArc(aAnnotatorName: string, aProjectId: number, aDocumentId: number, aSourceId: number, aTargetId: number, aLayer: number)
-    {
-        this.stompClient.publish({
-            destination: "/app/arc_create",
-            body: JSON.stringify(new CreateArcRequest(aAnnotatorName, aProjectId, aDocumentId, aSourceId, aTargetId, aLayer))
-        });
-    }
-
-    requestCreateSpan(aAnnotatorName: string, aProjectId: number, aDocumentId: number, aBegin: number, aEnd: number, aLayer: number)
-    {
-        this.stompClient.publish({
-            destination: "/app/span_create",
-            body: JSON.stringify(new CreateSpanRequest(aAnnotatorName, aProjectId, aDocumentId, aBegin, aEnd, aLayer))
-        });
-    }
-
-    requestDeleteAnnotation(aAnnotatorName: string, aProjectId: number, aDocumentId: number, aAnnotationId: number, aLayer: number)
-    {
-        this.stompClient.publish({
-            destination: "/app/annotation_delete", body: JSON.stringify(
-                new DeleteAnnotationRequest(aAnnotatorName, aProjectId, aDocumentId, aAnnotationId, aLayer))
-        });
-    }
-
-    requestUpdateFeature(aAnnotatorName: string, aProjectId: number, aDocumentId: number, aAnnotationId: number, aLayerId: number, aFeature: FeatureX, aValue: any)
-    {
-        this.stompClient.publish({
-            destination: "/app/features_update", body: JSON.stringify(
-                new UpdateFeatureRequest(aAnnotatorName, aProjectId, aDocumentId, aAnnotationId, aLayerId, aFeature, aValue))
-        });
-    }
-
-
-    onDocument(aMessage: DocumentMessage)
-    {
-        console.log('RECEIVED DOCUMENT' + aMessage);
-        this.notify();
-        this.annotationEditor.viewport = aMessage.viewport;
-    }
-
-    onSpanCreate(aMessage: SpanCreatedMessage)
-    {
-        console.log('RECEIVED SPAN CREATE' + aMessage);
-        this.notify();
-        this.annotationEditor.viewport[0].spans.push(new Span(aMessage.spanId, aMessage.begin, aMessage.end, aMessage.layerId, aMessage.features, aMessage.color))
-    }
-
-    onArcCreate(aMessage: ArcCreatedMessage)
-    {
-        console.log('RECEIVED ARC CREATE' + aMessage);
-        this.notify();
-        this.annotationEditor.viewport[0].arcs.push(new Arc(aMessage.arcId, aMessage.sourceId, aMessage.targetId, aMessage.layerId, aMessage.features, aMessage.color))
-    }
-
-    onAnnotationDelete(aMessage: DeleteAnnotationMessage)
-    {
-        console.log('RECEIVED DELETE ANNOTATION' + aMessage);
-        this.notify();
-        let annotationID = aMessage.annotationId;
-    }
-
-    onFeaturesUpdate(aMessage: UpdateFeatureMessage) {
-        console.log('RECEIVED UPDATE ANNOTATION' + aMessage);
-        this.notify();
-
-        let annotationID = aMessage.annotationId;
-        let feature = aMessage.feature
-        let newValue = aMessage.value;
-    }
-
-    onError(aMessage: AdviceMessage)
-    {
-        console.log('RECEIVED ERROR MESSAGE' + aMessage);
-        this.notify();
-        console.log(aMessage);
+    unsubscribeFromViewport() {
+        if (this.initSubscription) {
+            this.initSubscription.unsubscribe();
+        }
+        if (this.updateSubscription) {
+            this.updateSubscription.unsubscribe();
+        }
     }
 }
 
