11c728c500be8b85b9fa21a9aa4d0a8a70da3044
Richard Eckart de Castilho <richard.eckart@gmail.com>
2021-12-04 19:22:57 +0100

    #2732 - Upgrade RecogitoJS and convert to DIAM AJAX
    - Removed RecogitoJS fork
    - Switch to DIAM AJAX request handlers

--- inception/inception-html-recogito-editor/src/main/java/de/tudarmstadt/ukp/inception/recogitojseditor/RecogitoHtmlAnnotationEditor.java
+++ inception/inception-html-recogito-editor/src/main/java/de/tudarmstadt/ukp/inception/recogitojseditor/RecogitoHtmlAnnotationEditor.java
@@ -17,11 +17,7 @@
  */
 package de.tudarmstadt.ukp.inception.recogitojseditor;
 
-import static de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.VID.NONE_ID;
-import static de.tudarmstadt.ukp.clarin.webanno.api.annotation.util.WebAnnoCasUtil.selectByAddr;
 import static de.tudarmstadt.ukp.clarin.webanno.support.JSONUtil.toInterpretableJsonString;
-import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
-import static org.apache.commons.lang3.StringUtils.removeStart;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -29,7 +25,6 @@ import java.time.Duration;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.uima.cas.CAS;
-import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.head.CssHeaderItem;
@@ -53,15 +48,11 @@ import de.tudarmstadt.ukp.clarin.webanno.api.CasProvider;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.AnnotationEditorExtensionRegistry;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.action.AnnotationActionHandler;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.coloring.ColoringService;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.exception.AnnotationException;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.AnnotatorState;
-import de.tudarmstadt.ukp.clarin.webanno.api.annotation.model.VID;
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.rendering.model.VDocument;
-import de.tudarmstadt.ukp.clarin.webanno.support.JSONUtil;
 import de.tudarmstadt.ukp.clarin.webanno.support.wicket.WicketUtil;
+import de.tudarmstadt.ukp.inception.diam.editor.actions.EditorAjaxRequestHandlerExtensionPoint;
 import de.tudarmstadt.ukp.inception.htmleditor.HtmlAnnotationEditorImplBase;
-import de.tudarmstadt.ukp.inception.recogitojseditor.model.WebAnnotation;
-import de.tudarmstadt.ukp.inception.recogitojseditor.model.WebAnnotationTextPositionSelector;
 import de.tudarmstadt.ukp.inception.recogitojseditor.model.WebAnnotations;
 import de.tudarmstadt.ukp.inception.recogitojseditor.resources.RecogitoJsCssResourceReference;
 import de.tudarmstadt.ukp.inception.recogitojseditor.resources.RecogitoJsJavascriptResourceReference;
@@ -77,6 +68,7 @@ public class RecogitoHtmlAnnotationEditor
     private @SpringBean AnnotationSchemaService annotationService;
     private @SpringBean AnnotationEditorExtensionRegistry extensionRegistry;
     private @SpringBean ColoringService coloringService;
+    private @SpringBean EditorAjaxRequestHandlerExtensionPoint handlers;
 
     public RecogitoHtmlAnnotationEditor(String aId, IModel<AnnotatorState> aModel,
             AnnotationActionHandler aActionHandler, CasProvider aCasProvider)
@@ -105,39 +97,12 @@ public class RecogitoHtmlAnnotationEditor
     {
         String markupId = aContainer.getMarkupId();
         String callbackUrl = aAdapter.getCallbackUrl().toString();
-        StringBuilder script = new StringBuilder();
-        script.append("(function() {\n");
-        script.append("  var r = Recogito.init({\n");
-        script.append("    content: document.getElementById('" + markupId + "'),\n");
-        script.append("    disableEditor: true,\n");
-        script.append("    mode: 'pre'\n");
-        script.append("  });\n");
-        script.append("\n");
-        script.append("  r.on('createAnnotation', function(annotation) { \n");
-        script.append("    Wicket.Ajax.ajax({\n");
-        script.append("      'm' : 'POST',\n");
-        script.append("      'c' : '" + markupId + "',\n");
-        script.append("      'u' : '" + callbackUrl + "',\n");
-        script.append("      'ep' : {\n");
-        script.append("        'action': 'createAnnotation',\n");
-        script.append("        'annotation': JSON.stringify(annotation)\n");
-        script.append("      }});\n");
-        script.append("  \n");
-        script.append("  });\n");
-        script.append("  r.on('selectAnnotation', function(annotation) { \n");
-        script.append("    Wicket.Ajax.ajax({\n");
-        script.append("      'm' : 'POST',\n");
-        script.append("      'c' : '" + markupId + "',\n");
-        script.append("      'u' : '" + callbackUrl + "',\n");
-        script.append("      'ep' : {\n");
-        script.append("        'action': 'selectAnnotation',\n");
-        script.append("        'id': annotation.id\n");
-        script.append("      }});\n");
-        script.append("  \n");
-        script.append("  });\n");
-        script.append("  r.loadAnnotations('" + callbackUrl + "');\n");
-        script.append("})();");
-        return WicketUtil.wrapInTryCatch(script.toString());
+
+        StringBuilder js = new StringBuilder();
+        js.append("(function() {");
+        js.append("  recogito('" + markupId + "', '" + callbackUrl + "')");
+        js.append("})();");
+        return WicketUtil.wrapInTryCatch(js.toString());
     }
 
     @Override
@@ -167,24 +132,14 @@ public class RecogitoHtmlAnnotationEditor
             LOG.debug("[" + request.getMethod() + "]");
 
             try {
-                // Loading existing annotations
-                switch (request.getMethod()) {
-                case "GET":
+                if ("GET".equals(request.getMethod())) {
                     read(aTarget);
-                    break;
-                case "POST": {
-                    String action = getRequest().getRequestParameters().getParameterValue("action")
-                            .toString();
-                    switch (action) {
-                    case "selectAnnotation":
-                        select(aTarget);
-                        break;
-                    case "createAnnotation":
-                        create(aTarget);
-                        break;
-                    }
+                    return;
                 }
-                }
+                
+                handlers.getHandler(getRequest()) //
+                        .map(handler -> handler.handle(aTarget, getRequest())) //
+                        .orElse(null);
             }
             catch (Exception e) {
                 handleError("Error", e);
@@ -211,80 +166,9 @@ public class RecogitoHtmlAnnotationEditor
             handler.setCacheDuration(Duration.ofSeconds(1));
             handler.setContentDisposition(ContentDisposition.INLINE);
 
+            LOG.info("Sending back RecogitoJS JSON data");
+            
             getRequestCycle().scheduleRequestHandlerAfterCurrent(handler);
-        }
-
-        private void select(AjaxRequestTarget aTarget)
-            throws JsonParseException, JsonMappingException, IOException
-        {
-            VID paramId = VID.parse(removeStart(
-                    getRequest().getRequestParameters().getParameterValue("id").toString(), "#"));
-
-            try {
-                CAS cas = getCasProvider().get();
-
-                if (paramId.isSynthetic()) {
-                    extensionRegistry.fireAction(getActionHandler(), getModelObject(), aTarget, cas,
-                            paramId, "spanOpenDialog");
-                    return;
-                }
-
-                AnnotationFS fs = selectByAddr(cas, AnnotationFS.class, paramId.getId());
-
-                AnnotatorState state = getModelObject();
-                if (state.isSlotArmed()) {
-                    // When filling a slot, the current selection is *NOT* changed. The
-                    // Span annotation which owns the slot that is being filled remains
-                    // selected!
-                    getActionHandler().actionFillSlot(aTarget, cas, fs.getBegin(), fs.getEnd(),
-                            paramId);
-                }
-                else {
-                    state.getSelection().selectSpan(paramId, cas, fs.getBegin(), fs.getEnd());
-                    getActionHandler().actionSelect(aTarget);
-                }
-            }
-            catch (AnnotationException | IOException e) {
-                handleError("Unable to select span annotation", e);
-            }
-        }
-
-        private void create(AjaxRequestTarget aTarget)
-            throws JsonParseException, JsonMappingException, IOException
-        {
-            WebAnnotation anno = JSONUtil.fromJsonString(WebAnnotation.class,
-                    getRequest().getRequestParameters().getParameterValue("annotation").toString());
-
-            try {
-                WebAnnotationTextPositionSelector selector = anno.getTarget().stream() //
-                        .filter(t -> isNotEmpty(t.getSelector()))
-                        .flatMap(t -> t.getSelector().stream())
-                        .filter(s -> s instanceof WebAnnotationTextPositionSelector)
-                        .map(s -> (WebAnnotationTextPositionSelector) s).findFirst().get();
-                int begin = selector.getStart();
-                int end = selector.getEnd();
-
-                if (!(begin > -1 && end > -1)) {
-                    throw new AnnotationException(
-                            "Unable to create span annotation: No match was found");
-                }
-
-                CAS cas = getCasProvider().get();
-                AnnotatorState state = getModelObject();
-                if (state.isSlotArmed()) {
-                    // When filling a slot, the current selection is *NOT* changed. The
-                    // Span annotation which owns the slot that is being filled remains
-                    // selected!
-                    getActionHandler().actionFillSlot(aTarget, cas, begin, end, NONE_ID);
-                }
-                else {
-                    state.getSelection().selectSpan(cas, begin, end);
-                    getActionHandler().actionCreateOrUpdate(aTarget, cas);
-                }
-            }
-            catch (IOException | AnnotationException e) {
-                handleError("Unable to create span annotation", e);
-            }
         }
     }
 }
---
+++ inception/inception-html-recogito-editor/src/main/ts/recogito.ts
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt 
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Recogito } from '@recogito/recogito-js';
+
+import '@recogito/recogito-js/dist/recogito.min.css'
+import { DiamAjax } from "@inception-project/inception-diam/client/DiamAjax";
+
+function recogito(markupId: string, callbackUrl: string) {
+  var ajax = new DiamAjax(callbackUrl);
+
+  var r = new Recogito({
+    content: document.getElementById(markupId),
+    disableEditor: true,
+    mode: 'pre'
+  });
+
+  r.on('createAnnotation', annotation => { 
+    let target = annotation.target;
+    let text: string, begin: number, end: number;
+    for (let i = 0; i < target.selector.length; i ++) {
+      if (target.selector[i].type === "TextQuoteSelector") {
+        text = target.selector[i].exact;
+      }
+      if (target.selector[i].type === "TextPositionSelector") {
+        begin = target.selector[i].start;
+        end = target.selector[i].end;
+      }
+    }
+
+    ajax.createSpanAnnotation([[begin, end]], text);
+  });
+  r.on('selectAnnotation', annotation => { 
+    // The RecogitoJS annotation IDs start with a hash `#` which we need to remove
+    ajax.selectAnnotation(annotation.id.substring('1'));
+  });
+
+  r.loadAnnotations(callbackUrl);
+}
+
+export = recogito;
