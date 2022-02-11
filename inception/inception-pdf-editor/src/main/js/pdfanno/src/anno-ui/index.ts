9e33b496e6393982c5e421f6533b4e20e0ab0910
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-02-11 20:48:25 +0100

    #2846 - Trim down PDF editor code
    - Remove unused code

--- inception/inception-pdf-editor/src/main/js/pdfanno/src/anno-ui/index.ts
+++ inception/inception-pdf-editor/src/main/js/pdfanno/src/anno-ui/index.ts
@@ -1,6 +1,5 @@
 import './index.css'
 
-import * as referenceAnnoDropdown from './components/referenceAnnoDropdown'
 import * as labelInput from './components/labelInput'
 import * as searchUI from './components/searchUI'
 import * as ui from './uis'
@@ -9,7 +8,6 @@ import * as util from './utils'
 import * as core from './core'
 
 export {
-    referenceAnnoDropdown,
     labelInput,
     searchUI,
     ui,
