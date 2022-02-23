033d85998494782b528d088b77a2191bbf586ee3
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-02-23 20:43:49 +0100

    #2869 - Hugging Face Recommender Prototype
    - UI for entering an API key and for choosing a model with an auto-complete field that queries the hub
    - Very basic support for token classification tasks
    - Guarded by feature flag

---
+++ inception/inception-imls-hf/src/main/java/de/tudarmstadt/ukp/inception/recommendation/imls/hf/HfModelSearchField.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt 
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.inception.recommendation.imls.hf;
+
+import static java.util.Arrays.asList;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import com.googlecode.wicket.jquery.core.renderer.TextRenderer;
+import com.googlecode.wicket.kendo.ui.form.autocomplete.AutoCompleteTextField;
+
+import de.tudarmstadt.ukp.inception.recommendation.imls.hf.client.HfHubClient;
+import de.tudarmstadt.ukp.inception.recommendation.imls.hf.model.HfModelCard;
+
+public class HfModelSearchField
+    extends AutoCompleteTextField<HfModelCard>
+{
+    private static final long serialVersionUID = -483058932604036488L;
+
+    private @SpringBean HfHubClient hfHubClient;
+
+    protected HfModelSearchField(String aId, IModel<HfModelCard> aModel)
+    {
+        super(aId, aModel, new TextRenderer<>("modelId"));
+    }
+
+    @Override
+    protected List<HfModelCard> getChoices(String aUserInput)
+    {
+        try {
+            return asList(hfHubClient.listModels(aUserInput));
+        }
+        catch (IOException e) {
+            return Collections.emptyList();
+        }
+    }
+}
