c2454ffc27575f15c51ec96520a5c89d139af3f4
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-03-15 22:42:44 +0100

    #2893 - Annotators dropdown in dynamic workload management broken
    - Replace kendo choices with native HTML choices to avoid dropdown from closing when making a choice

--- inception/inception-workload-dynamic/src/main/java/de/tudarmstadt/ukp/inception/workload/dynamic/management/DynamicWorkloadManagementPage.java
+++ inception/inception-workload-dynamic/src/main/java/de/tudarmstadt/ukp/inception/workload/dynamic/management/DynamicWorkloadManagementPage.java
@@ -95,7 +95,6 @@ import com.googlecode.wicket.jquery.core.Options;
 import com.googlecode.wicket.jquery.ui.widget.menu.IMenuItem;
 import com.googlecode.wicket.kendo.ui.KendoDataSource;
 import com.googlecode.wicket.kendo.ui.form.datetime.AjaxDatePicker;
-import com.googlecode.wicket.kendo.ui.form.dropdown.DropDownList;
 import com.googlecode.wicket.kendo.ui.form.multiselect.lazy.MultiSelect;
 import com.googlecode.wicket.kendo.ui.renderer.ChoiceRenderer;
 
@@ -281,8 +280,7 @@ public class DynamicWorkloadManagementPage
 
         // Add StateFilters
         stateFilters = new WebMarkupContainer("stateFilters");
-        ListView<SourceDocumentState> listview = new ListView<>("stateFilter",
-                asList(SourceDocumentState.values()))
+        var listview = new ListView<>("stateFilter", asList(SourceDocumentState.values()))
         {
             private static final long serialVersionUID = -2292408105823066466L;
 
@@ -292,9 +290,8 @@ public class DynamicWorkloadManagementPage
                 LambdaAjaxLink link = new LambdaAjaxLink("stateFilterLink",
                         (_target -> actionApplyStateFilter(_target, aItem.getModelObject())));
 
-                link.add(new Label(MID_LABEL,
-                        aItem.getModel().map(SourceDocumentState::symbol).orElse(""))
-                                .setEscapeModelStrings(false));
+                link.add(new Label(MID_LABEL, aItem.getModel().map( //
+                        SourceDocumentState::symbol).orElse("")).setEscapeModelStrings(false));
                 link.add(new AttributeAppender("class", () -> dataProvider.getFilterState()
                         .getStates().contains(aItem.getModelObject()) ? "active" : "", " "));
                 aItem.add(link);
@@ -478,7 +475,7 @@ public class DynamicWorkloadManagementPage
             _target.add(settingsForm);
         }).add(visibleWhen(abandonationToggle.getModel())));
 
-        DropDownChoice<AnnotationDocumentState> abandonationState = new DropDownList<>(
+        DropDownChoice<AnnotationDocumentState> abandonationState = new DropDownChoice<>(
                 "abandonationState");
         abandonationState.setRequired(true);
         abandonationState.setNullValid(false);
@@ -488,7 +485,7 @@ public class DynamicWorkloadManagementPage
         abandonationState.add(visibleWhen(abandonationToggle.getModel()));
         settingsForm.add(abandonationState);
 
-        DropDownChoice<String> workflowChoices = new DropDownList<>("workflowType");
+        DropDownChoice<String> workflowChoices = new DropDownChoice<>("workflowType");
         workflowChoices.setChoiceRenderer(
                 new LambdaChoiceRenderer<>(id -> workflowExtensionPoint.getExtension(id)
                         .map(WorkflowExtension::getLabel).orElse("<" + id + " not available>")));
@@ -531,7 +528,7 @@ public class DynamicWorkloadManagementPage
         userSelectionForm.setOutputMarkupId(true);
 
         // Dropdown menu with all available users of the project
-        userSelection = new DropDownList<>("userSelection");
+        userSelection = new DropDownChoice<>("userSelection");
         userSelection.setChoiceRenderer(new LambdaChoiceRenderer<>(User::getUiName));
         // No default model required, as the input shall be empty by default
         userSelection.setModel(new Model<>());
@@ -637,7 +634,7 @@ public class DynamicWorkloadManagementPage
         userResetDocumentForm.add(visibleWhen(() -> !isNull(userSelection.getModelObject())));
 
         // Dropdown for all annotation documents for the user that exist in the DB
-        resetDocument = new DropDownList<>("resetDocument");
+        resetDocument = new DropDownChoice<>("resetDocument");
         resetDocument.setChoiceRenderer(new LambdaChoiceRenderer<>(AnnotationDocument::getName));
         resetDocument.setChoices(this::getCreatedDocumentsForSelectedUser);
         resetDocument.setModel(LoadableDetachableModel.of(this::getSelectedAnnotationDocument));
@@ -645,7 +642,7 @@ public class DynamicWorkloadManagementPage
         userResetDocumentForm.add(resetDocument);
 
         // Dropdown for all states that can be assign to the selected document
-        documentState = new DropDownList<>("documentState");
+        documentState = new DropDownChoice<>("documentState");
         documentState
                 .setChoiceRenderer(new LambdaChoiceRenderer<>(AnnotationDocumentState::getName));
         documentState.setChoices(this::getAnnotationDocumentStates);
