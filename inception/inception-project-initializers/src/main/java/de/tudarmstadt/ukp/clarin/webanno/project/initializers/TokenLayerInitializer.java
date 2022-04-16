0f8eb56ddb002158a217fda3350331e5d69c7455
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-04-16 18:24:50 +0200

    #1778 - Make tokenization editable
    - Make sentence layer depend on tokens because sentences have token-granularity.

--- inception/inception-project-initializers/src/main/java/de/tudarmstadt/ukp/clarin/webanno/project/initializers/TokenLayerInitializer.java
+++ inception/inception-project-initializers/src/main/java/de/tudarmstadt/ukp/clarin/webanno/project/initializers/TokenLayerInitializer.java
@@ -21,15 +21,12 @@ import static de.tudarmstadt.ukp.clarin.webanno.api.WebAnnoConst.SPAN_TYPE;
 import static de.tudarmstadt.ukp.clarin.webanno.model.AnchoringMode.CHARACTERS;
 import static de.tudarmstadt.ukp.clarin.webanno.model.OverlapMode.NO_OVERLAP;
 import static de.tudarmstadt.ukp.clarin.webanno.model.ValidationMode.NEVER;
-import static java.util.Arrays.asList;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
-import de.tudarmstadt.ukp.clarin.webanno.api.project.ProjectInitializer;
 import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationLayer;
 import de.tudarmstadt.ukp.clarin.webanno.model.Project;
 import de.tudarmstadt.ukp.clarin.webanno.project.initializers.config.ProjectInitializersAutoConfiguration;
@@ -56,14 +53,6 @@ public class TokenLayerInitializer
     public String getName()
     {
         return "Tokens";
-    }
-
-    @Override
-    public List<Class<? extends ProjectInitializer>> getDependencies()
-    {
-        return asList(
-                // Because tokens are not allowed to cross sentence boundaries
-                SentenceLayerInitializer.class);
     }
 
     @Override
