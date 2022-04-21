d07295087848eec2e5db45cd57cc04b117364f72
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-04-21 21:24:56 +0200

    #2997 - Editing PDFs no longer works
    - Remove reference to undeclared version in package.json which causes client-side code to crash and PDF annotations to not work anymore

--- inception/inception-pdf-editor/src/main/js/pdfanno/src/core/src/version.ts
+++
@@ -1,7 +0,0 @@
-const packageJson = require('../../../package.json')
-/**
- * Paper Anno Version.
- * This is overwritten at build.
- */
-export let ANNO_VERSION = packageJson.version
-export let PDFEXTRACT_VERSION = packageJson.pdfextract.version
