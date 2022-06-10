68754ff717695ac71a901c6785ade5b0b613abad
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-06-10 19:06:11 +0200

    #3070 - Modularize webapp module
    - Remove unused images
    - Remove use of deprecated API
    - Moved security config class

--- inception/inception-security/src/main/java/de/tudarmstadt/ukp/clarin/webanno/security/config/InceptionSecurityAutoConfiguration.java
+++ inception/inception-security/src/main/java/de/tudarmstadt/ukp/clarin/webanno/security/config/InceptionSecurityAutoConfiguration.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.inception.app.config;
+package de.tudarmstadt.ukp.clarin.webanno.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
