dce92b4c2ec6e49e683a4786d63e352836fb3d87
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-06-15 07:38:08 +0200

    #3096 - New PDF editor
    - Fix dependencies
    - Reduce test logging

--- inception/inception-pdf-editor2/src/test/java/de/tudarmstadt/ukp/inception/pdfeditor2/visual/VisualPDFTextStripperTest.java
+++ inception/inception-pdf-editor2/src/test/java/de/tudarmstadt/ukp/inception/pdfeditor2/visual/VisualPDFTextStripperTest.java
@@ -80,8 +80,8 @@ class VisualPDFTextStripperTest
                     float w = (d == 0 || d == 180) ? vGlyph.getExtent() : vLine.getExtent();
                     float h = (d == 0 || d == 180) ? vLine.getExtent() : vGlyph.getExtent();
 
-                    System.out.printf("%.0f %f %f -- (%f %f %f %f) -- %s%n", vLine.getDir(),
-                            vLine.getBase(), vLine.getExtent(), x, y, w, h, vGlyph);
+                    // System.out.printf("%.0f %f %f -- (%f %f %f %f) -- %s%n", vLine.getDir(),
+                    // vLine.getBase(), vLine.getExtent(), x, y, w, h, vGlyph);
 
                     // Font (screen) coordinates should be within the line boundaries
                     assertThat(vGlyph.getFontX()).isBetween(x, x + w);
