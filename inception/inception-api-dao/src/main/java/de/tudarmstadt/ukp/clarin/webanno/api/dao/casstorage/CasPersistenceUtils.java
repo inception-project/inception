ecabeceef487e966f13cde12ea3c75115a631b1b
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-06-20 19:45:50 +0200

    #3136 - Support reading compressed CASes from 24.0
    - Backport the code for detecting and reading compressed CASes from 24.x

--- inception/inception-api-dao/src/main/java/de/tudarmstadt/ukp/clarin/webanno/api/dao/casstorage/CasPersistenceUtils.java
+++ inception/inception-api-dao/src/main/java/de/tudarmstadt/ukp/clarin/webanno/api/dao/casstorage/CasPersistenceUtils.java
@@ -21,10 +21,12 @@ import static de.tudarmstadt.ukp.clarin.webanno.api.annotation.util.WebAnnoCasUt
 import static org.apache.uima.cas.impl.Serialization.deserializeCASComplete;
 import static org.apache.uima.cas.impl.Serialization.serializeCASComplete;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -37,6 +39,7 @@ import org.apache.uima.util.CasIOUtils;
 import org.apache.uima.util.TypeSystemUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xerial.snappy.SnappyFramedInputStream;
 
 import de.tudarmstadt.ukp.clarin.webanno.api.annotation.util.WebAnnoCasUtil;
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
@@ -118,23 +121,60 @@ public final class CasPersistenceUtils
         CAS realCas = getRealCas(aCas);
         // UIMA-6162 Workaround: synchronize CAS during de/serialization
         synchronized (((CASImpl) realCas).getBaseCAS()) {
-            try (ObjectInputStream is = new ObjectInputStream(new FileInputStream(aFile))) {
-                CASCompleteSerializer serializer = (CASCompleteSerializer) is.readObject();
-                deserializeCASComplete(serializer, (CASImpl) realCas);
-
-                // Workaround for UIMA adding back deleted DocumentAnnotations
-                // https://issues.apache.org/jira/browse/UIMA-6199
-                // If there is a DocumentMetaData annotation, then we can drop any of the default
-                // UIMA DocumentAnnotation instances (excluding the DocumentMetaData of course)
-                if (!aCas.select(DocumentMetaData.class.getName()).isEmpty()) {
-                    aCas.select(CAS.TYPE_NAME_DOCUMENT_ANNOTATION).filter(
-                            fs -> !DocumentMetaData.class.getName().equals(fs.getType().getName()))
-                            .forEach(aCas::removeFsFromIndexes);
-                }
-            }
-            catch (ClassNotFoundException e) {
-                throw new IOException(e);
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(aFile))) {
+                readSerializedCas(realCas, maybeUncompress(bis));
             }
         }
+    }
+
+    private static void readSerializedCas(CAS aCas, InputStream is) throws IOException
+    {
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            CASCompleteSerializer serializer = (CASCompleteSerializer) ois.readObject();
+            deserializeCASComplete(serializer, (CASImpl) aCas);
+
+            // Workaround for UIMA adding back deleted DocumentAnnotations
+            // https://issues.apache.org/jira/browse/UIMA-6199
+            // If there is a DocumentMetaData annotation, then we can drop any of the default UIMA
+            // DocumentAnnotation instances (excluding the DocumentMetaData of course)
+            if (!aCas.select(DocumentMetaData.class.getName()).isEmpty()) {
+                aCas.select(CAS.TYPE_NAME_DOCUMENT_ANNOTATION).filter(
+                        fs -> !DocumentMetaData.class.getName().equals(fs.getType().getName()))
+                        .forEach(aCas::removeFsFromIndexes);
+            }
+        }
+        catch (ClassNotFoundException e) {
+            throw new IOException(e);
+        }
+    }
+
+    private static InputStream maybeUncompress(BufferedInputStream bis) throws IOException
+    {
+        byte[] buf = new byte[32];
+        bis.mark(buf.length);
+        int bytesRead = bis.read(buf);
+        bis.reset();
+
+        InputStream is = bis;
+        if (isSnappyStream(buf, bytesRead)) {
+            is = new SnappyFramedInputStream(bis);
+        }
+        return is;
+    }
+
+    private static boolean isSnappyStream(byte[] aBuffer, int aLen)
+    {
+        byte[] snappyMagic = { (byte) 0xff, 0x06, 0x00, 0x00, 0x73, 0x4e, 0x61, 0x50, 0x70, 0x59, };
+        if (aLen < snappyMagic.length) {
+            return false;
+        }
+
+        for (int i = 0; i < snappyMagic.length; i++) {
+            if (aBuffer[i] != snappyMagic[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
