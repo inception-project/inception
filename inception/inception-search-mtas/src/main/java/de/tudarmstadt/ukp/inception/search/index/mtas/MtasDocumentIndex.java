5a191f6325d8d9c2ceee48e8950360fb3b7447b3
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-06-29 23:22:44 +0200

    #3155 - Upgrade RDF4J, Lucene, Solr, MTAS, etc.
    - When the project index is corrupt (e.g. because we upgraded Lucene...) then try deleting so it can be re-built from scratch

--- inception/inception-search-mtas/src/main/java/de/tudarmstadt/ukp/inception/search/index/mtas/MtasDocumentIndex.java
+++ inception/inception-search-mtas/src/main/java/de/tudarmstadt/ukp/inception/search/index/mtas/MtasDocumentIndex.java
@@ -71,6 +71,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -213,59 +214,74 @@ public class MtasDocumentIndex
         // new RuntimeException());
 
         try {
-            // Add the project id to the configuration
-            JSONObject jsonParserConfiguration = new JSONObject();
-            jsonParserConfiguration.put(PARAM_PROJECT_ID, project.getId());
-
-            // Tokenizer parameters
-            Map<String, String> tokenizerArguments = new HashMap<>();
-            tokenizerArguments.put(ARGUMENT_PARSER, MtasUimaParser.class.getName());
-            tokenizerArguments.put(ARGUMENT_PARSER_ARGS, jsonParserConfiguration.toString());
-
-            // Build analyzer
-            Analyzer mtasAnalyzer = CustomAnalyzer.builder()
-                    .withTokenizer(MtasTokenizerFactory.class, tokenizerArguments).build();
-
-            Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
-            analyzerPerField.put(FIELD_CONTENT, mtasAnalyzer);
-
-            PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrapper(new StandardAnalyzer(),
-                    analyzerPerField);
-
-            // Build IndexWriter
-            FileUtils.forceMkdir(getIndexDir());
-            IndexWriterConfig config = new IndexWriterConfig(analyzer);
-            config.setCodec(Codec.forName(MTAS_CODEC_NAME));
-
-            @SuppressWarnings("resource")
-            IndexWriter indexWriter = new IndexWriter(FSDirectory.open(getIndexDir().toPath()),
-                    config);
-
-            // Initialize the index
-            try {
-                indexWriter.commit();
-            }
-            catch (IOException e) {
-                try {
-                    indexWriter.close();
-                }
-                catch (IOException e1) {
-                    log.error("Error while trying to close index which could not be initalized"
-                            + " - actual exception follows", e);
-                }
-                throw e;
-            }
+            // After the index has been initialized, assign the _indexWriter - this is also used
+            // by isOpen() to check if the index writer is available.
+            _indexWriter = createIndexWriter();
+            ;
+            return _indexWriter;
+        }
+        catch (CorruptIndexException e) {
+            // If the index is corrupt, try once to delete and create from scratch
+            delete();
 
             // After the index has been initialized, assign the _indexWriter - this is also used
             // by isOpen() to check if the index writer is available.
-            _indexWriter = indexWriter;
-
+            _indexWriter = createIndexWriter();
+            ;
             return _indexWriter;
+
         }
         catch (IOException e) {
             _indexWriter = null;
             throw e;
         }
+    }
+
+    private IndexWriter createIndexWriter() throws IOException
+    {
+        // Add the project id to the configuration
+        JSONObject jsonParserConfiguration = new JSONObject();
+        jsonParserConfiguration.put(PARAM_PROJECT_ID, project.getId());
+
+        // Tokenizer parameters
+        Map<String, String> tokenizerArguments = new HashMap<>();
+        tokenizerArguments.put(ARGUMENT_PARSER, MtasUimaParser.class.getName());
+        tokenizerArguments.put(ARGUMENT_PARSER_ARGS, jsonParserConfiguration.toString());
+
+        // Build analyzer
+        Analyzer mtasAnalyzer = CustomAnalyzer.builder()
+                .withTokenizer(MtasTokenizerFactory.class, tokenizerArguments).build();
+
+        Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
+        analyzerPerField.put(FIELD_CONTENT, mtasAnalyzer);
+
+        PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrapper(new StandardAnalyzer(),
+                analyzerPerField);
+
+        // Build IndexWriter
+        FileUtils.forceMkdir(getIndexDir());
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        config.setCodec(Codec.forName(MTAS_CODEC_NAME));
+
+        @SuppressWarnings("resource")
+        IndexWriter indexWriter = new IndexWriter(FSDirectory.open(getIndexDir().toPath()), config);
+
+        // Initialize the index
+        try {
+            indexWriter.commit();
+        }
+        catch (IOException e) {
+            try {
+                indexWriter.close();
+            }
+            catch (IOException e1) {
+                log.error("Error while trying to close index which could not be initalized"
+                        + " - actual exception follows", e);
+            }
+            throw e;
+        }
+
+        return indexWriter;
     }
 
     private void ensureAllIsCommitted()
