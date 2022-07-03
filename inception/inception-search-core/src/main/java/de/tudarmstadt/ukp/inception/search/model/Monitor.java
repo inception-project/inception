0a62cc251f1320b019f07ff13e8b66cd014154f9
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-07-03 15:03:30 +0200

    #3155 - Upgrade RDF4J, Lucene, Solr, MTAS, etc.
    - Add CLI command for rebuilding the MTAS indexes all at once

--- inception/inception-search-core/src/main/java/de/tudarmstadt/ukp/inception/search/model/Monitor.java
+++ inception/inception-search-core/src/main/java/de/tudarmstadt/ukp/inception/search/model/Monitor.java
@@ -17,10 +17,24 @@
  */
 package de.tudarmstadt.ukp.inception.search.model;
 
+import static java.lang.Math.round;
+import static java.lang.System.currentTimeMillis;
+
 public class Monitor
 {
+    private final long start;
+
+    private long lastIncrement;
+    private long lastDuration;
     private int done;
     private int todo;
+
+    public Monitor()
+    {
+        start = currentTimeMillis();
+        lastIncrement = start;
+        lastDuration = 0;
+    }
 
     public synchronized void setDone(int aDone)
     {
@@ -46,5 +60,65 @@ public class Monitor
     public synchronized void incDone()
     {
         done++;
+        var now = System.currentTimeMillis();
+        lastDuration = now - lastIncrement;
+        lastIncrement = now;
     }
+
+    public long getTotalDuration()
+    {
+        return System.currentTimeMillis() - start;
+    }
+
+    public long getLastDuration()
+    {
+        return lastDuration;
+    }
+
+    public long getEstimatedDuration()
+    {
+        return round(((double) (getTotalDuration()) / done) * todo);
+    }
+
+    public boolean isDone()
+    {
+        return done >= todo;
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(done);
+        sb.append(" of ");
+        sb.append(todo);
+        if (done > 0 && todo > 0) {
+            final int perc = 100 - (int) (((todo - done) * 100) / todo);
+            sb.append(" (");
+            sb.append(perc);
+            sb.append("%  ETA ");
+            final long timeSoFar = getTotalDuration();
+            final long estTotal = round((timeSoFar / done) * todo);
+            final long timeLeft = round(estTotal - timeSoFar);
+            sb.append(milliToStringShort(timeLeft));
+            sb.append("  RUN ");
+            sb.append(milliToStringShort(timeSoFar));
+            sb.append("  AVG ");
+            sb.append(round(timeSoFar / done));
+            sb.append("  LAST ");
+            sb.append(lastDuration);
+            sb.append(")");
+        }
+        return sb.toString();
+    }
+
+    private static String milliToStringShort(final long milli)
+    {
+        final long fracs = milli % 1000;
+        final long seconds = milli / 1000;
+        final long minutes = seconds / 60;
+        final long hours = minutes / 60;
+        return String.format("%02d:%02d:%02d.%-3d", hours, (minutes % 60), (seconds % 60), fracs);
+    }
+
 }
