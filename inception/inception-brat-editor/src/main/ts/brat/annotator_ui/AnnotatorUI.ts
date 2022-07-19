9fa221103ffb6ef31be6d287dda7dedd2aa39c53
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-07-19 22:58:11 +0200

    #3209 - Reactivity lost after creating an annotation
    - Clear spanDragJustStarted flag when a span annotation is created to avoid mouse move after span creation registering as drag action

--- inception/inception-brat-editor/src/main/ts/brat/annotator_ui/AnnotatorUI.ts
+++ inception/inception-brat-editor/src/main/ts/brat/annotator_ui/AnnotatorUI.ts
@@ -851,8 +851,9 @@ export class AnnotatorUI {
         }
 
         // normal span select in standard annotation mode or reselect: show selector
-        const spanText = this.data.text.substring(selectedFrom, selectedTo);
-        this.ajax.createSpanAnnotation(this.spanOptions.offsets, spanText);
+        this.spanDragJustStarted = false
+        const spanText = this.data.text.substring(selectedFrom, selectedTo)
+        this.ajax.createSpanAnnotation(this.spanOptions.offsets, spanText)
       }
     }
   }
