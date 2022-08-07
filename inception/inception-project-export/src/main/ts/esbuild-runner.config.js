15acc3f4ba8c5354d20789d3fbb47c384648e8ee
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-08-07 21:58:40 +0200

    #3276 - Harmonize TS builds
    - Fix CSS in svelte working with mocha

--- inception/inception-project-export/src/main/ts/esbuild-runner.config.js
+++ inception/inception-project-export/src/main/ts/esbuild-runner.config.js
@@ -24,6 +24,7 @@ module.exports = {
     target: 'es6',
     plugins: [
       esbuildSvelte({
+        compilerOptions: { css: true },
         preprocess: sveltePreprocess({ sourceMap: true })
       })
     ]
