f2ed348216bfa508b41d5ff09856b11c7cc7b803
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-08-07 20:40:39 +0200

    #3276 - Harmonize TS builds
    - Remove webkit stuff from animation

--- inception/inception-project-export/src/main/ts/src/RunningExportsPanel.svelte
+++ inception/inception-project-export/src/main/ts/src/RunningExportsPanel.svelte
@@ -238,19 +238,8 @@
 }
 
 @keyframes pulse {
-    0% {
-        -webkit-transform: scale3d(1,1,1);
-        transform: scale3d(1,1,1)
-    }
-
-    50% {
-        -webkit-transform: scale3d(1.05,1.05,1.05);
-        transform: scale3d(1.05,1.05,1.05)
-    }
-
-    100% {
-        -webkit-transform: scale3d(1,1,1);
-        transform: scale3d(1,1,1)
-    }
+    0% { transform: scale3d(1,1,1) }
+    50% { transform: scale3d(1.05,1.05,1.05) }
+    100% { transform: scale3d(1,1,1) }
 }
 </style>
