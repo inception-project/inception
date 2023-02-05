29afb972393e951b73d05884b8cd3bfa50e51eb6
Richard Eckart de Castilho <richard.eckart@gmail.com>
2023-02-05 17:39:14 +0100

    #3788 - Jumping to a curation item via sidebar does not work
    - Implement scrollTo action for the curation sidebar extension
    - Change the curation sidebar extension ID from "curationEditorExtension" to "cur"
    - Adjust test
    - Move curation color literal into a constant

--- inception/inception-ui-curation/src/main/java/de/tudarmstadt/ukp/inception/ui/curation/sidebar/config/CurationSidebarAutoConfiguration.java
+++ inception/inception-ui-curation/src/main/java/de/tudarmstadt/ukp/inception/ui/curation/sidebar/config/CurationSidebarAutoConfiguration.java
@@ -31,7 +31,6 @@ import de.tudarmstadt.ukp.clarin.webanno.api.CasStorageService;
 import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
 import de.tudarmstadt.ukp.clarin.webanno.api.ProjectService;
 import de.tudarmstadt.ukp.clarin.webanno.security.UserDao;
-import de.tudarmstadt.ukp.inception.rendering.config.AnnotationEditorProperties;
 import de.tudarmstadt.ukp.inception.schema.AnnotationSchemaService;
 import de.tudarmstadt.ukp.inception.schema.layer.LayerSupportRegistry;
 import de.tudarmstadt.ukp.inception.ui.curation.sidebar.CurationEditorExtension;
@@ -60,13 +59,11 @@ public class CurationSidebarAutoConfiguration
     @Bean(CurationEditorExtension.EXTENSION_ID)
     public CurationEditorExtension curationEditorExtension(
             AnnotationSchemaService aAnnotationService, DocumentService aDocumentService,
-            AnnotationEditorProperties aAnnotationEditorProperties,
             ApplicationEventPublisher aApplicationEventPublisher, UserDao aUserRepository,
             CurationSidebarService aCurationSidebarService)
     {
         return new CurationEditorExtension(aAnnotationService, aDocumentService,
-                aAnnotationEditorProperties, aApplicationEventPublisher, aUserRepository,
-                aCurationSidebarService);
+                aApplicationEventPublisher, aUserRepository, aCurationSidebarService);
     }
 
     @Bean("curationSidebar")
