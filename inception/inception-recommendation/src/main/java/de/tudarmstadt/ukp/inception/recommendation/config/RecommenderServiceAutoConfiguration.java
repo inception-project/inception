58d1652bff506021c088468c23a1103bab0b56c1
Richard Eckart de Castilho <richard.eckart@gmail.com>
2023-02-05 19:53:13 +0100

    #3790 - Feature toggles for more sidebars
    - Global feature toggle for statistics sidebar
    - Global feature toggle for recommender sidebar
    - Update documentation

--- inception/inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/config/RecommenderServiceAutoConfiguration.java
+++ inception/inception-recommendation/src/main/java/de/tudarmstadt/ukp/inception/recommendation/config/RecommenderServiceAutoConfiguration.java
@@ -144,6 +144,8 @@ public class RecommenderServiceAutoConfiguration
         return new ProjectRecommendersMenuItem();
     }
 
+    @ConditionalOnProperty(prefix = "recommender.sidebar", name = "enabled", //
+            havingValue = "true", matchIfMissing = true)
     @Bean
     public RecommendationSidebarFactory recommendationSidebarFactory(
             RecommendationService aRecommendationService)
