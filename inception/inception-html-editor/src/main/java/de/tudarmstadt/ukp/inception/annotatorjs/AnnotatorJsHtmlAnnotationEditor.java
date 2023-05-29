90908f6af97868be76d8dc72b90aa56fbe7f47b6
Richard Eckart de Castilho <richard.eckart@gmail.com>
2023-05-29 16:00:17 +0200

    #4034 - Remove deprecated 'iframe:cas+html' view
    - Remove code

--- inception/inception-html-editor/src/main/java/de/tudarmstadt/ukp/inception/annotatorjs/AnnotatorJsHtmlAnnotationEditor.java
+++ inception/inception-html-editor/src/main/java/de/tudarmstadt/ukp/inception/annotatorjs/AnnotatorJsHtmlAnnotationEditor.java
@@ -31,11 +31,10 @@ import de.tudarmstadt.ukp.clarin.webanno.api.CasProvider;
 import de.tudarmstadt.ukp.clarin.webanno.api.DocumentService;
 import de.tudarmstadt.ukp.inception.annotatorjs.resources.AnnotatorJsCssResourceReference;
 import de.tudarmstadt.ukp.inception.annotatorjs.resources.AnnotatorJsJavascriptResourceReference;
-import de.tudarmstadt.ukp.inception.editor.AnnotationEditorFactory;
 import de.tudarmstadt.ukp.inception.editor.action.AnnotationActionHandler;
-import de.tudarmstadt.ukp.inception.editor.view.DocumentViewFactory;
 import de.tudarmstadt.ukp.inception.externaleditor.ExternalAnnotationEditorBase;
 import de.tudarmstadt.ukp.inception.externaleditor.model.AnnotationEditorProperties;
+import de.tudarmstadt.ukp.inception.externaleditor.xhtml.XHtmlXmlDocumentIFrameViewFactory;
 import de.tudarmstadt.ukp.inception.rendering.editorstate.AnnotatorState;
 
 public class AnnotatorJsHtmlAnnotationEditor
@@ -43,8 +42,8 @@ public class AnnotatorJsHtmlAnnotationEditor
 {
     private static final long serialVersionUID = -3358207848681467993L;
 
-    private @SpringBean(name = "htmlAnnotationEditorFactory") AnnotationEditorFactory editorFactory;
-    private @SpringBean(name = "xHtmlXmlDocumentIFrameViewFactory") DocumentViewFactory viewFactory;
+    private @SpringBean AnnotatorJsHtmlAnnotationEditorFactory editorFactory;
+    private @SpringBean XHtmlXmlDocumentIFrameViewFactory viewFactory;
     private @SpringBean DocumentService documentService;
     private @SpringBean ServletContext servletContext;
 
