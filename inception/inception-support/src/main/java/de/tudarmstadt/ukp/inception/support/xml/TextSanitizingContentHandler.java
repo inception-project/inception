0e1a0f4abb5a0f39a1c0ad406073cd402668492f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2023-11-24 21:43:52 +0100

    #4321 - Organize packages
    - Consolidated xml support package

--- inception/inception-support/src/main/java/de/tudarmstadt/ukp/inception/support/xml/TextSanitizingContentHandler.java
+++ inception/inception-support/src/main/java/de/tudarmstadt/ukp/inception/support/xml/TextSanitizingContentHandler.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.clarin.webanno.support.xml;
+package de.tudarmstadt.ukp.inception.support.xml;
 
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
