d5e0c5258bda21604f1c9290a90fd713fabdd9f7
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-01-22 20:39:45 +0100

    #4442 - Option to allow spaces in usernames
    - Added option
    - Added test
    - Added documentation

--- inception/inception-security/src/main/java/de/tudarmstadt/ukp/clarin/webanno/security/UserDaoImpl.java
+++ inception/inception-security/src/main/java/de/tudarmstadt/ukp/clarin/webanno/security/UserDaoImpl.java
@@ -488,8 +488,8 @@ public class UserDaoImpl
         }
 
         // Do not allow space
-        if (containsAnyCharacterMatching(aName, Character::isWhitespace)) {
-            errors.add(new ValidationError("Username cannot contain a space character") //
+        if (containsAnyCharacterMatching(aName, this::isWhitespaceIllegalInUsername)) {
+            errors.add(new ValidationError("Username cannot contain whitespace") //
                     .addKey(MSG_USERNAME_ERROR_ILLEGAL_SPACE));
             return errors;
         }
@@ -550,6 +550,15 @@ public class UserDaoImpl
         }
 
         return errors;
+    }
+
+    private boolean isWhitespaceIllegalInUsername(char ch)
+    {
+        if (securityProperties.isSpaceAllowedInUsername() && ch == ' ') {
+            return false;
+        }
+
+        return Character.isWhitespace((int) ch);
     }
 
     @Override
