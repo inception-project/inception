6e727e09c684c026dc318bda1695b172fcb57025
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-01-27 11:04:11 +0100

    #4461 - Show class in class tree when selecting a class
    - Expand tree and scroll to class if possible when a class is selected
    - Cleaning up and modernizing code a bit

--- inception/inception-ui-kb/src/main/java/de/tudarmstadt/ukp/inception/ui/kb/project/local/LocalRepositorySettingsPanel.java
+++ inception/inception-ui-kb/src/main/java/de/tudarmstadt/ukp/inception/ui/kb/project/local/LocalRepositorySettingsPanel.java
@@ -389,25 +389,25 @@ public class LocalRepositorySettingsPanel
     {
         try {
             if (selectedKnowledgeBaseProfile != null) {
-                String accessUrl = selectedKnowledgeBaseProfile.getAccess().getAccessUrl();
+                var accessUrl = selectedKnowledgeBaseProfile.getAccess().getAccessUrl();
 
-                FileUploadDownloadHelper fileUploadDownloadHelper = new FileUploadDownloadHelper(
-                        getApplication());
+                var fileUploadDownloadHelper = new FileUploadDownloadHelper(getApplication());
 
-                if (!accessUrl.startsWith(CLASSPATH_PREFIX)) {
-                    File tmpFile = fileUploadDownloadHelper
-                            .writeFileDownloadToTemporaryFile(accessUrl, getModel());
-                    getModel().getObject().putFile(selectedKnowledgeBaseProfile.getName(), tmpFile);
-                }
-                else {
+                if (accessUrl.startsWith(CLASSPATH_PREFIX)) {
                     // import from classpath
-                    File kbFile = fileUploadDownloadHelper
+                    var kbFile = fileUploadDownloadHelper
                             .writeClasspathResourceToTemporaryFile(accessUrl, getModel());
                     getModel().getObject().putFile(selectedKnowledgeBaseProfile.getName(), kbFile);
                 }
+                else if (accessUrl != null) {
+                    var tmpFile = fileUploadDownloadHelper
+                            .writeFileDownloadToTemporaryFile(accessUrl, getModel());
+                    getModel().getObject().putFile(selectedKnowledgeBaseProfile.getName(), tmpFile);
+                }
 
-                KnowledgeBase kb = getModel().getObject().getKb();
+                var kb = getModel().getObject().getKb();
                 kb.applyRootConcepts(selectedKnowledgeBaseProfile);
+                kb.applyAdditionalMatchingProperties(selectedKnowledgeBaseProfile);
                 kb.applyMapping(selectedKnowledgeBaseProfile.getMapping());
                 kb.setFullTextSearchIri(
                         selectedKnowledgeBaseProfile.getAccess().getFullTextSearchIri());
