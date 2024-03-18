b913b89e028c4f3eed0363ab75c0e024a601f722
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-03-18 22:17:35 +0100

    #4645 - Self-help info for reverse proxy problems
    - Add diagnostic info to the admin page

--- inception/inception-ui-project/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/project/detail/ProjectDetailPanel.java
+++ inception/inception-ui-project/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/project/detail/ProjectDetailPanel.java
@@ -26,13 +26,13 @@ import static java.util.Objects.isNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
+import static org.apache.wicket.RuntimeConfigurationType.DEVELOPMENT;
 
 import java.io.IOException;
 import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.feedback.IFeedback;
 import org.apache.wicket.markup.html.basic.Label;
@@ -123,8 +123,7 @@ public class ProjectDetailPanel
 
         // If we run in development mode, then also show the ID of the project
         form.add(idLabel = new Label("id"));
-        idLabel.setVisible(RuntimeConfigurationType.DEVELOPMENT
-                .equals(getApplication().getConfigurationType()));
+        idLabel.setVisible(getApplication().getConfigurationType() == DEVELOPMENT);
 
         form.add(new TextArea<String>("description").setOutputMarkupId(true));
 
