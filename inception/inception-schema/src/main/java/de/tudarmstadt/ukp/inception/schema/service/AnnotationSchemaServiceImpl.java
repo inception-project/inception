e26c26b6e21b613502c61b4ebfa51e0d92e73918
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-05-16 22:11:28 +0200

    #1319 - Cross-Layer Relations
    - Fix issue that relation layers attaching to any span layer do not cause a delete confirmation to popup when the annotator tries to delete an attaching span
    - Fix exception when trying to remove dangling relations

--- inception/inception-schema/src/main/java/de/tudarmstadt/ukp/inception/schema/service/AnnotationSchemaServiceImpl.java
+++ inception/inception-schema/src/main/java/de/tudarmstadt/ukp/inception/schema/service/AnnotationSchemaServiceImpl.java
@@ -57,6 +57,8 @@ import java.util.StringTokenizer;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.cas.CAS;
@@ -608,7 +610,7 @@ public class AnnotationSchemaServiceImpl
             // layer definitions for UIMA built-in types.
             if (superType == null) {
                 throw new NoResultException("Super-type [" + type.getSupertypeName() + "] of type ["
-                        + aName + "] not in type system - no suitable layer definition found");
+                        + aName + "] does not correspond to any project layer");
             }
 
             layer = getLayerInternal(superType.getName(), aProject);
@@ -757,21 +759,40 @@ public class AnnotationSchemaServiceImpl
     {
         Objects.requireNonNull(aLayer, "Parameter [layer] must be specified");
 
-        var query = String.join("\n",
-                "SELECT l FROM AnnotationLayer l LEFT JOIN l.attachFeature f ", //
-                "WHERE l.type        = :type AND ", //
-                "      l.project     = :project AND ", //
-                "      (l.attachType = :attachType OR f.type = :attachTypeName) ", //
-                "ORDER BY l.uiName");
+        var cb = entityManager.getCriteriaBuilder();
+        var cq = cb.createQuery(AnnotationLayer.class);
 
-        return entityManager.createQuery(query, AnnotationLayer.class)
-                .setParameter("type", RELATION_TYPE) //
-                .setParameter("attachType", aLayer) //
-                .setParameter("attachTypeName", aLayer.getName())
-                // Checking for project is necessary because type match is string-based
-                .setParameter("project", aLayer.getProject()) //
-                .setHint(CACHEABLE, true) //
-                .getResultList();
+        var layer = cq.from(AnnotationLayer.class);
+        var feature = layer.join(AnnotationLayer_.attachFeature, JoinType.LEFT);
+
+        var predicates = new ArrayList<Predicate>();
+        predicates.add(cb.equal(layer.get(AnnotationLayer_.type), RelationLayerSupport.TYPE));
+        predicates.add(cb.equal(layer.get(AnnotationLayer_.project), aLayer.getProject()));
+        var attachTypeCondition = cb.or( //
+                cb.equal(layer.get(AnnotationLayer_.attachType), aLayer), //
+                cb.isNull(layer.get(AnnotationLayer_.attachType)), //
+                cb.equal(feature.get(AnnotationFeature_.type), aLayer.getName()));
+        predicates.add(attachTypeCondition);
+
+        cq.where(predicates.toArray(new Predicate[0]));
+        cq.orderBy(cb.asc(layer.get(AnnotationLayer_.uiName)));
+
+        return entityManager.createQuery(cq).getResultList();
+
+        // var query = String.join("\n",
+        // "SELECT l FROM AnnotationLayer l LEFT JOIN l.attachFeature f ", //
+        // "WHERE l.type = :type AND ", //
+        // " l.project = :project AND ", //
+        // " (l.attachType = :attachType OR l.attachType IS NULL OR f.type = :attachTypeName) ", //
+        // "ORDER BY l.uiName");
+        //
+        // return entityManager.createQuery(query, AnnotationLayer.class)
+        // .setParameter("type", RelationLayerSupport.TYPE) //
+        // .setParameter("attachType", aLayer) //
+        // .setParameter("attachTypeName", aLayer.getName())
+        // // Checking for project is necessary because type match is string-based
+        // .setParameter("project", aLayer.getProject()) //
+        // .getResultList();
     }
 
     @Transactional
@@ -1424,7 +1445,7 @@ public class AnnotationSchemaServiceImpl
     @Transactional(noRollbackFor = NoResultException.class)
     public TypeAdapter findAdapter(Project aProject, FeatureStructure aFS)
     {
-        AnnotationLayer layer = findLayer(aProject, aFS);
+        var layer = findLayer(aProject, aFS);
         return getAdapter(layer);
     }
 
@@ -1487,61 +1508,59 @@ public class AnnotationSchemaServiceImpl
     @Transactional
     public List<AttachedAnnotation> getAttachedRels(AnnotationLayer aLayer, AnnotationFS aFs)
     {
-        CAS cas = aFs.getCAS();
-        List<AttachedAnnotation> result = new ArrayList<>();
-        for (AnnotationLayer relationLayer : listAttachedRelationLayers(aLayer)) {
-            RelationAdapter relationAdapter = (RelationAdapter) getAdapter(relationLayer);
-            Type relationType = CasUtil.getType(cas, relationLayer.getName());
-            Feature sourceFeature = relationType
-                    .getFeatureByBaseName(relationAdapter.getSourceFeatureName());
-            Feature targetFeature = relationType
-                    .getFeatureByBaseName(relationAdapter.getTargetFeatureName());
+        var cas = aFs.getCAS();
+        var result = new ArrayList<AttachedAnnotation>();
+        for (var layer : listAttachedRelationLayers(aLayer)) {
+            var adapter = (RelationAdapter) getAdapter(layer);
+            var type = adapter.getAnnotationType(cas);
+            var sourceFeature = adapter.getSourceFeature(cas);
+            var targetFeature = adapter.getTargetFeature(cas);
 
             // This code is already prepared for the day that relations can go between
             // different layers and may have different attach features for the source and
             // target layers.
-            Feature relationSourceAttachFeature = null;
-            Feature relationTargetAttachFeature = null;
-            if (relationAdapter.getAttachFeatureName() != null) {
-                relationSourceAttachFeature = sourceFeature.getRange()
-                        .getFeatureByBaseName(relationAdapter.getAttachFeatureName());
-                relationTargetAttachFeature = targetFeature.getRange()
-                        .getFeatureByBaseName(relationAdapter.getAttachFeatureName());
+            Feature sourceAttachFeature = null;
+            Feature targetAttachFeature = null;
+            if (adapter.getAttachFeatureName() != null) {
+                sourceAttachFeature = sourceFeature.getRange()
+                        .getFeatureByBaseName(adapter.getAttachFeatureName());
+                targetAttachFeature = targetFeature.getRange()
+                        .getFeatureByBaseName(adapter.getAttachFeatureName());
             }
 
-            for (AnnotationFS relationFS : CasUtil.select(cas, relationType)) {
+            for (var relationFS : CasUtil.select(cas, type)) {
                 if (!(relationFS instanceof AnnotationFS)) {
                     continue;
                 }
 
                 // Here we get the annotations that the relation is pointing to in the UI
                 AnnotationFS sourceFS;
-                if (relationSourceAttachFeature != null) {
+                if (sourceAttachFeature != null) {
                     sourceFS = (AnnotationFS) relationFS.getFeatureValue(sourceFeature)
-                            .getFeatureValue(relationSourceAttachFeature);
+                            .getFeatureValue(sourceAttachFeature);
                 }
                 else {
                     sourceFS = (AnnotationFS) relationFS.getFeatureValue(sourceFeature);
                 }
 
                 AnnotationFS targetFS;
-                if (relationTargetAttachFeature != null) {
+                if (targetAttachFeature != null) {
                     targetFS = (AnnotationFS) relationFS.getFeatureValue(targetFeature)
-                            .getFeatureValue(relationTargetAttachFeature);
+                            .getFeatureValue(targetAttachFeature);
                 }
                 else {
                     targetFS = (AnnotationFS) relationFS.getFeatureValue(targetFeature);
                 }
 
                 if (sourceFS == null || targetFS == null) {
-                    StringBuilder message = new StringBuilder();
+                    var message = new StringBuilder();
 
-                    message.append("Relation [" + relationAdapter.getLayer().getName()
-                            + "] with id [" + ICasUtil.getAddr(relationFS)
+                    message.append("Relation [" + adapter.getLayer().getName() + "] with id ["
+                            + ICasUtil.getAddr(relationFS)
                             + "] has loose ends - cannot identify attached annotations.");
-                    if (relationAdapter.getAttachFeatureName() != null) {
-                        message.append("\nRelation [" + relationAdapter.getLayer().getName()
-                                + "] attached to feature [" + relationAdapter.getAttachFeatureName()
+                    if (adapter.getAttachFeatureName() != null) {
+                        message.append("\nRelation [" + adapter.getLayer().getName()
+                                + "] attached to feature [" + adapter.getAttachFeatureName()
                                 + "].");
                     }
                     message.append("\nSource: " + sourceFS);
@@ -1550,19 +1569,17 @@ public class AnnotationSchemaServiceImpl
                     continue;
                 }
 
-                boolean isIncoming = isSame(targetFS, aFs);
-                boolean isOutgoing = isSame(sourceFS, aFs);
+                var isIncoming = isSame(targetFS, aFs);
+                var isOutgoing = isSame(sourceFS, aFs);
 
                 if (isIncoming && isOutgoing) {
-                    result.add(new AttachedAnnotation(relationLayer, relationFS, sourceFS, LOOP));
+                    result.add(new AttachedAnnotation(layer, relationFS, sourceFS, LOOP));
                 }
                 else if (isIncoming) {
-                    result.add(
-                            new AttachedAnnotation(relationLayer, relationFS, sourceFS, INCOMING));
+                    result.add(new AttachedAnnotation(layer, relationFS, sourceFS, INCOMING));
                 }
                 else if (isOutgoing) {
-                    result.add(
-                            new AttachedAnnotation(relationLayer, relationFS, targetFS, OUTGOING));
+                    result.add(new AttachedAnnotation(layer, relationFS, targetFS, OUTGOING));
                 }
             }
         }
