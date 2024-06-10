095dc88e20556e7ebcc2b9bf37d22f55559e0d37
Kévin ZGRZENDEK <kevin.zgrzendek@aphp.fr>
2024-06-10 23:02:07 +0200

    Working, basic helmchart

--- charts/inception/configuration/mariadb/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE inception DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER '{{ .Values.mariadb.auth.username }}'@'localhost' IDENTIFIED BY '{{ .Values.mariadb.auth.password }}';
-GRANT ALL PRIVILEGES ON inception.* TO '{{ .Values.mariadb.auth.username }}'@'localhost';
-FLUSH PRIVILEGES;
