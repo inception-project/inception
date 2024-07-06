8ae5b9ff03af63011a61c8d26db4f2685b39a200
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-07-06 22:11:02 +0200

    No issue: Small style fix

--- inception/inception-ui-dashboard-activity/src/main/ts/src/ActivitiesDashlet.svelte
+++ inception/inception-ui-dashboard-activity/src/main/ts/src/ActivitiesDashlet.svelte
@@ -51,7 +51,7 @@
     }
 </script>
 
-<div class="card-body">
+<div class="card-body p-0">
     {#if loading}
         <div class="mt-5 d-flex flex-column justify-content-center">
             <div class="d-flex flex-row justify-content-center">
