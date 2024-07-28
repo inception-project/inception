d318dc422060326f6f812a5d3d9a79d6c35c0832
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-07-27 22:33:09 -0300

    #4963 - Support for ChatGPT
    - Added support for calling ChatGPT through its API

---
+++ inception/inception-imls-chatgpt/src/main/java/de/tudarmstadt/ukp/inception/recommendation/imls/chatgpt/client/ChatGptClientImpl.java
@@ -0,0 +1,166 @@
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt 
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.inception.recommendation.imls.chatgpt.client;
+
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.lang3.StringUtils.appendIfMissing;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.tudarmstadt.ukp.inception.support.json.JSONUtil;
+
+public class ChatGptClientImpl
+    implements ChatGptClient
+{
+    public static final int HTTP_BAD_REQUEST = 400;
+
+    protected final HttpClient client;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public ChatGptClientImpl()
+    {
+        client = HttpClient.newBuilder().build();
+    }
+
+    public ChatGptClientImpl(HttpClient aClient)
+    {
+        client = aClient;
+    }
+
+    protected HttpResponse<InputStream> sendRequest(HttpRequest aRequest) throws IOException
+    {
+        try {
+            var response = client.send(aRequest, HttpResponse.BodyHandlers.ofInputStream());
+
+            handleError(response);
+
+            return response;
+        }
+        catch (IOException | InterruptedException e) {
+            throw new IOException("Error while sending request: " + e.getMessage(), e);
+        }
+    }
+
+    protected String getResponseBody(HttpResponse<InputStream> response) throws IOException
+    {
+        if (response.body() != null) {
+            return IOUtils.toString(response.body(), UTF_8);
+        }
+
+        return "";
+    }
+
+    protected <T> T deserializeResponse(HttpResponse<String> response, Class<T> aType)
+        throws IOException
+    {
+        try {
+            return objectMapper.readValue(response.body(), aType);
+        }
+        catch (IOException e) {
+            throw new IOException("Error while deserializing server response!", e);
+        }
+    }
+
+    protected String urlEncodeParameters(Map<String, String> aParameters)
+    {
+        if (aParameters.isEmpty()) {
+            return "";
+        }
+
+        var uriBuilder = new StringBuilder();
+        for (Entry<String, String> param : aParameters.entrySet()) {
+            if (uriBuilder.length() > 0) {
+                uriBuilder.append("&");
+            }
+            uriBuilder.append(URLEncoder.encode(param.getKey(), UTF_8));
+            uriBuilder.append('=');
+            uriBuilder.append(URLEncoder.encode(param.getValue(), UTF_8));
+        }
+
+        return uriBuilder.toString();
+    }
+
+    @Override
+    public String generate(String aUrl, ChatCompletionRequest aRequest) throws IOException
+    {
+        var request = HttpRequest.newBuilder() //
+                .uri(URI.create(appendIfMissing(aUrl, "/") + "chat/completions")) //
+                .header(HttpHeaders.CONTENT_TYPE, "application/json") //
+                .header("Authorization", "Bearer " + aRequest.getApiKey()) //
+                .POST(BodyPublishers.ofString(JSONUtil.toJsonString(aRequest), UTF_8)) //
+                .build();
+
+        var response = sendRequest(request);
+
+        handleError(response);
+
+        var result = new StringBuilder();
+        try (var is = response.body()) {
+            var completion = objectMapper.readValue(is, ChatCompletionResponse.class);
+            result.append(completion.getChoices().get(0).getMessage().getContent());
+        }
+
+        return result.toString();
+    }
+
+    public List<ChatGptModel> listModels(String aUrl, ListModelsRequest aRequest) throws IOException
+    {
+        var request = HttpRequest.newBuilder() //
+                .uri(URI.create(appendIfMissing(aUrl, "/") + "models")) //
+                .header(HttpHeaders.CONTENT_TYPE, "application/json").GET() //
+                .header("Authorization", "Bearer " + aRequest.getApiKey()) //
+                .build();
+
+        var response = sendRequest(request);
+
+        handleError(response);
+
+        try (var is = response.body()) {
+            return objectMapper.readValue(is, ChatGptModelResponse.class).getData().stream() //
+                    .sorted(Comparator.comparing(ChatGptModel::getId)) //
+                    .toList();
+        }
+    }
+
+    private void handleError(HttpResponse<InputStream> response) throws IOException
+    {
+        if (response.statusCode() >= HTTP_BAD_REQUEST) {
+            var responseBody = getResponseBody(response);
+            var msg = format("Request was not successful: [%d] - [%s]", response.statusCode(),
+                    responseBody);
+            throw new IOException(msg);
+        }
+    }
+}
