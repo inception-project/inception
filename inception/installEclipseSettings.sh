ce8b5d7d82c43f9aa08e2dc3247e375d842d11e3
Richard Eckart de Castilho <richard.eckart@gmail.com>
2025-01-14 22:08:21 +0100

    #5228 - Reduce build time
    - Enable parallel tests in most modules

--- inception/installEclipseSettings.sh
+++ inception/installEclipseSettings.sh
@@ -6,11 +6,16 @@ export JDT_CORE_PREFS="inception-build/src/main/resources/inception/eclipse/org.
 # Save actions
 export JDT_UI_PREFS="inception-build/src/main/resources/inception/eclipse/org.eclipse.jdt.ui.prefs"
 
+# JUnit configuration
+export JUNIT_PATFORM_PROPERTIES="inception-build/src/main/resources/inception/junit-platform.properties"
+
 function installPrefs {
   echo "Installing preferences into $1"
   mkdir -p $1/.settings/
+  mkdir -p $1/src/test/resources/
   cp -v $JDT_CORE_PREFS $1/.settings/
   cp -v $JDT_UI_PREFS $1/.settings/
+  # cp -v $JUNIT_PATFORM_PROPERTIES $1/src/test/resources/
 }
 
 export -f installPrefs
