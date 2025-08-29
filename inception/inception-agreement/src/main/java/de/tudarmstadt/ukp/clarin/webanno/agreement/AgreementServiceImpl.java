75d8d1dad89ac4b1d8f68e91af601fddcb44de74
Richard Eckart de Castilho <richard.eckart@gmail.com>
2025-08-29 06:47:53 +0200

    #5572 - Case where annotation document does not exist mishandled
    - Added missing return statements

--- inception/inception-agreement/src/main/java/de/tudarmstadt/ukp/clarin/webanno/agreement/AgreementServiceImpl.java
+++ inception/inception-agreement/src/main/java/de/tudarmstadt/ukp/clarin/webanno/agreement/AgreementServiceImpl.java
@@ -313,7 +313,7 @@ public class AgreementServiceImpl
         }
 
         if (!documentService.existsCas(aDocument, aDataOwner)) {
-            Optional.empty();
+            return Optional.empty();
         }
 
         return loadCas(aDocument, aDataOwner);
