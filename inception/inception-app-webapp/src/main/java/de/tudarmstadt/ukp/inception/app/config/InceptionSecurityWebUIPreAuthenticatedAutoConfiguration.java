546713d9ff599da23d0da17be070ebafee30d4f2
Richard Eckart de Castilho <richard.eckart@gmail.com>
2026-02-10 11:44:22 +0100

    #5875 - preAuth mode and proxy result in CSRF validation error
    - Port CRSF configuration from built-in to pre-auth configuration
    - Upgrade to modern API

--- inception/inception-app-webapp/src/main/java/de/tudarmstadt/ukp/inception/app/config/InceptionSecurityWebUIPreAuthenticatedAutoConfiguration.java
+++ inception/inception-app-webapp/src/main/java/de/tudarmstadt/ukp/inception/app/config/InceptionSecurityWebUIPreAuthenticatedAutoConfiguration.java
@@ -26,6 +26,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +34,8 @@ import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 import de.tudarmstadt.ukp.clarin.webanno.security.UserDao;
 import de.tudarmstadt.ukp.clarin.webanno.security.preauth.ShibbolethRequestHeaderAuthenticationFilter;
@@ -49,33 +52,45 @@ public class InceptionSecurityWebUIPreAuthenticatedAutoConfiguration
             ShibbolethRequestHeaderAuthenticationFilter aFilter, SessionRegistry aSessionRegistry)
         throws Exception
     {
-        // @formatter:off
-        aHttp
-            .rememberMe()
-            .and()
-            .addFilterBefore(aFilter, RequestHeaderAuthenticationFilter.class);
-        
-        var authorizations = aHttp.authorizeHttpRequests();
-        accessToStaticResources(authorizations);
-        accessToRemoteApiAndSwagger(authorizations);
-        accessToApplication(authorizations);
-        authorizations.anyRequest().denyAll();
+        aHttp.csrf(csrf -> {
+            // Instead of disabling the Spring CSRF filter, we just disable the CSRF validation for
+            // the Wicket UI (Wicket has its own CSRF mechanism). This way, Spring will still
+            // populate the CSRF token attribute in the request which we will need later when we
+            // need to provide the token to the JavaScript code in the browser to make callbacks to
+            // Spring MVC controllers.
+            csrf.requireCsrfProtectionMatcher(
+                    new NegatedRequestMatcher(AnyRequestMatcher.INSTANCE));
+        });
+        aHttp.headers(headers -> {
+            headers.frameOptions(frameOptions -> {
+                frameOptions.sameOrigin();
+            });
+        });
 
-        aHttp.exceptionHandling()
-                .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
-            .and()
-                .headers().frameOptions().sameOrigin()
-            .and()
-            .sessionManagement() 
+        aHttp.rememberMe(Customizer.withDefaults()) //
+                .addFilterBefore(aFilter, RequestHeaderAuthenticationFilter.class);
+
+        aHttp.authorizeHttpRequests(authz -> {
+            accessToStaticResources(authz);
+            accessToRemoteApiAndSwagger(authz);
+            accessToApplication(authz);
+            authz.anyRequest().denyAll();
+        });
+
+        aHttp.exceptionHandling(
+                exception -> exception.authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
+
+        aHttp.sessionManagement(session -> session
                 // Configuring an unlimited session per-user maximum as a side-effect registers
                 // the ConcurrentSessionFilter which checks for valid sessions in the session
                 // registry. This allows us to indirectly invalidate a server session by marking
                 // its Spring-security registration as invalid and have Spring Security in turn
                 // mark the server session as invalid on the next request. This is used e.g. to
                 // force-sign-out users that are being deleted.
-                .maximumSessions(-1)
-                .sessionRegistry(aSessionRegistry);
-        // @formatter:on
+                .maximumSessions(-1) //
+                .sessionRegistry(aSessionRegistry));
+
         return aHttp.build();
     }
 
@@ -85,7 +100,7 @@ public class InceptionSecurityWebUIPreAuthenticatedAutoConfiguration
             AuthenticationConfiguration aAuthenticationConfiguration, UserDao aUserRepository)
         throws Exception
     {
-        ShibbolethRequestHeaderAuthenticationFilter filter = new ShibbolethRequestHeaderAuthenticationFilter();
+        var filter = new ShibbolethRequestHeaderAuthenticationFilter();
         filter.setPrincipalRequestHeader(preAuthPrincipalHeader);
         filter.setAuthenticationManager(aAuthenticationConfiguration.getAuthenticationManager());
         filter.setUserRepository(aUserRepository);
