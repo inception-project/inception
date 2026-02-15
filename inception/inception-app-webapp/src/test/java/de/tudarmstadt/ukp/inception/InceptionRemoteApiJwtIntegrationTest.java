47ce086a7974e9a9ce145cf16a43956ea87cb4fc
Richard Eckart de Castilho <richard.eckart@gmail.com>
2026-02-15 13:08:42 +0100

    #5883 - Improve test stability
    - Avoid test setup leaking

--- inception/inception-app-webapp/src/test/java/de/tudarmstadt/ukp/inception/InceptionRemoteApiJwtIntegrationTest.java
+++ inception/inception-app-webapp/src/test/java/de/tudarmstadt/ukp/inception/InceptionRemoteApiJwtIntegrationTest.java
@@ -21,6 +21,7 @@ import static com.nimbusds.jose.JOSEObjectType.JWT;
 import static de.tudarmstadt.ukp.clarin.webanno.security.model.Role.ROLE_ADMIN;
 import static de.tudarmstadt.ukp.clarin.webanno.security.model.Role.ROLE_REMOTE;
 import static de.tudarmstadt.ukp.clarin.webanno.security.model.Role.ROLE_USER;
+import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -43,7 +44,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 import com.nimbusds.jwt.SignedJWT;
 
@@ -62,37 +71,46 @@ public class InceptionRemoteApiJwtIntegrationTest
 
     static final String SUB = UUID.randomUUID().toString();
 
-    static String oldServerPort;
     static @TempDir Path appHome;
     static MockOAuth2Server oauth2Server;
     static ConfigurableApplicationContext context;
+    static String oldHeadless;
     static HttpClient client;
     static UserDao userService;
 
     @BeforeAll
     static void setup()
     {
-        oldServerPort = System.setProperty("server.port", "0");
-
         oauth2Server = new MockOAuth2Server();
         oauth2Server.start();
 
         var issuerUrl = oauth2Server.url(ISSUER_ID).toString();
 
-        setProperty("spring.main.banner-mode", "off");
+        oldHeadless = getProperty("java.awt.headless");
         setProperty("java.awt.headless", "true");
-        setProperty("database.url", "jdbc:hsqldb:mem:testdb;hsqldb.tx=mvcc");
-        setProperty("inception.home", appHome.toString());
-        setProperty("remote-api.enabled", "true");
-        setProperty("remote-api.oauth2.enabled", "true");
-        setProperty("remote-api.oauth2.realm", CLIENT_ID);
-        setProperty("remote-api.oauth2.user-name-attribute", PREFERRED_USERNAME_CLAIM);
-        setProperty("spring.security.oauth2.resourceserver.jwt.issuer-uri", issuerUrl);
-        // setProperty("logging.level.org.springframework.security", "TRACE");
 
-        context = INCEpTION.start(new String[] {}, INCEpTION.class);
+        var properties = new java.util.Properties();
+        properties.setProperty("server.port", "0");
+        properties.setProperty("spring.main.banner-mode", "off");
+        properties.setProperty("database.url", "jdbc:hsqldb:mem:testdb;hsqldb.tx=mvcc");
+        properties.setProperty("spring.jpa.hibernate.ddl-auto", "create-drop");
+        properties.setProperty("inception.home", appHome.toString());
+        properties.setProperty("remote-api.enabled", "true");
+        properties.setProperty("remote-api.oauth2.enabled", "true");
+        properties.setProperty("remote-api.oauth2.realm", CLIENT_ID);
+        properties.setProperty("remote-api.oauth2.user-name-attribute", PREFERRED_USERNAME_CLAIM);
+        properties.setProperty("spring.security.oauth2.resourceserver.jwt.issuer-uri", issuerUrl);
+        // properties.setProperty("logging.level.org.springframework.security", "TRACE");
 
-        client = HttpClient.newBuilder().build();
+        context = INCEpTION.start( //
+                new String[] {}, //
+                properties, //
+                INCEpTION.class, //
+                LongTimeoutJwtConfig.class);
+
+        client = HttpClient.newBuilder() //
+                .connectTimeout(Duration.ofSeconds(60)) //
+                .build();
 
         userService = context.getBean(UserDao.class);
 
@@ -106,16 +124,15 @@ public class InceptionRemoteApiJwtIntegrationTest
     @AfterAll
     static void teardown()
     {
-        if (oldServerPort == null) {
-            System.getProperties().remove("server.port");
-
-        }
-        else {
-            System.setProperty("server.port", oldServerPort);
-        }
         context.close();
         oauth2Server.shutdown();
         LogManager.shutdown();
+        if (oldHeadless == null) {
+            System.getProperties().remove("java.awt.headless");
+        }
+        else {
+            setProperty("java.awt.headless", oldHeadless);
+        }
     }
 
     @Test
@@ -163,5 +180,28 @@ public class InceptionRemoteApiJwtIntegrationTest
                 .setHeader("Authorization", "Bearer " + token.serialize()) //
                 .build();
         return request;
+    }
+
+    @TestConfiguration
+    public static class LongTimeoutJwtConfig
+    {
+        @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+        private String issuerUri;
+
+        @Bean
+        public JwtDecoder jwtDecoder()
+        {
+            // Force a 90-second timeout to survive Windows CI starvation on GitHub actions
+            var requestFactory = new SimpleClientHttpRequestFactory();
+            requestFactory.setConnectTimeout(90_000);
+            requestFactory.setReadTimeout(90_000);
+
+            RestOperations rest = new RestTemplate(requestFactory);
+
+            return NimbusJwtDecoder //
+                    .withIssuerLocation(issuerUri) //
+                    .restOperations(rest) //
+                    .build();
+        }
     }
 }
