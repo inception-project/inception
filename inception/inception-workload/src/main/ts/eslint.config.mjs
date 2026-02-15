75d4869edf71db7ee0552f2b014dde7e8c901a22
Richard Eckart de Castilho <richard.eckart@gmail.com>
2026-02-15 16:49:18 +0100

    #5793 - Upgrade dependencies
    - eslint-plugin-n 17.23.2 -> 17.24.0
    - rollup 4.55.1 -> 4.57.1
    - svelte 5.51.0 -> 5.51.2

---
+++ inception/inception-workload/src/main/ts/eslint.config.mjs
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// =======================================================================
+// File lives in 
+// /inception/inception-build/src/main/resources/inception/vite.config.js
+// =======================================================================
+
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import parser from "svelte-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("standard", "plugin:svelte/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.mocha,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            extraFileExtensions: [".svelte"],
+        },
+    },
+
+    rules: {
+        "no-unused-expressions": 0
+    },
+}, {
+    files: ["**/*.svelte"],
+
+    languageOptions: {
+        parser: parser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+        },
+    },
+}]);
