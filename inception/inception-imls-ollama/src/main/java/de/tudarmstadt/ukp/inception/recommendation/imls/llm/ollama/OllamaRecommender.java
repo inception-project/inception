b5304ef01eb0b97a13b1a5904c9ede2694f9a634
Richard Eckart de Castilho <richard.eckart@gmail.com>
2026-03-01 18:10:54 +0100

    #5793 - Upgrade dependencies
    * mockito 5.21.0 -> 5.22.0
    * assertj 3.27.6 -> 3.27.7
    * spring 7.0.4 -> 7.0.5
    * spring-boot 4.0.2 -> 4.0.3
    * springdoc 3.0.1 -> 3.0.2
    * swagger 2.2.42 -> 2.2.43
    * sentry 8.32.0 -> 8.33.0
    * hibernate 7.2.4.Final -> 7.2.6.Final
    * jackson 2.21.0 -> 2.21.1
    * jackson3 3.0.4 -> 3.1.0
    * snakeyaml 2.5 -> 2.6
    * imageio-tiff 3.13.0 -> 3.13.1
    * nimbus-jose-jwt 10.7 -> 10.8
    * opensaml 5.2.0 -> 5.2.1
    * font-awesome 7.1.0 -> 7.2.0

--- inception/inception-imls-ollama/src/main/java/de/tudarmstadt/ukp/inception/recommendation/imls/llm/ollama/OllamaRecommender.java
+++ inception/inception-imls-ollama/src/main/java/de/tudarmstadt/ukp/inception/recommendation/imls/llm/ollama/OllamaRecommender.java
@@ -28,8 +28,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import de.tudarmstadt.ukp.inception.recommendation.api.model.Recommender;
 import de.tudarmstadt.ukp.inception.recommendation.imls.llm.AnnotationTaskCodecExtensionPoint;
 import de.tudarmstadt.ukp.inception.recommendation.imls.llm.ChatBasedLlmRecommenderImplBase;
@@ -89,7 +87,6 @@ public class OllamaRecommender
     }
 
     private JsonNode getResponseFormat(ResponseFormat aFormat, JsonNode aSchema)
-        throws JsonProcessingException
     {
         if (aSchema != null) {
             return aSchema;
